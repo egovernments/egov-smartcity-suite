060c9a15618fce134462f10a909c03a6ea79ff53
Srivatsa Katta <vatsa.katta@gmail.com>
2015-01-19 12:49:04 +0530

    Katta - ComplaintTypeController test asserting for view name

--- source/egov-erp/egov-pgrweb/src/test/java/org/egov/pgrweb/controller/ComplaintTypeControllerTest.java
+++ source/egov-erp/egov-pgrweb/src/test/java/org/egov/pgrweb/controller/ComplaintTypeControllerTest.java
@@ -2,22 +2,25 @@ package org.egov.pgrweb.controller;
 
 import org.junit.Test;
 
+import static org.mockito.MockitoAnnotations.initMocks;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 
 public class ComplaintTypeControllerTest extends AbstractContextControllerTest<ComplaintTypeController> {
 
     @Override
     protected ComplaintTypeController initController() {
+        initMocks(this);
+
         return new ComplaintTypeController();
     }
 
     @Test
     public void shouldResolveComplaintTypeCreationPage() throws Exception {
         this.mockMvc.perform(get("/complaint-type"))
-                .andExpect(content().string("complaint-type"))
+                .andExpect(view().name("complaint-type"))
                 .andExpect(status().isOk());
     }
 }
