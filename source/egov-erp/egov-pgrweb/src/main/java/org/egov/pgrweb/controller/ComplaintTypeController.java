419240a816039533fd9cf832da440d2cad0a0a58
Srivatsa Katta <vatsa.katta@gmail.com>
2015-01-22 11:46:30 +0530

    Katta - Retrieving departments as model attribute for complaint-type page

--- source/egov-erp/egov-pgrweb/src/main/java/org/egov/pgrweb/controller/ComplaintTypeController.java
+++ source/egov-erp/egov-pgrweb/src/main/java/org/egov/pgrweb/controller/ComplaintTypeController.java
@@ -1,10 +1,28 @@
 package org.egov.pgrweb.controller;
 
+import org.egov.lib.rjbac.dept.Department;
+import org.egov.lib.rjbac.dept.ejb.api.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class ComplaintTypeController {
+
+    private DepartmentService departmentService;
+
+    @Autowired
+    public ComplaintTypeController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+    @ModelAttribute("departments")
+    public List<Department> departments() {
+        return departmentService.getAllDepartments();
+    }
 
     @RequestMapping("/complaint-type")
     public String complaintType() {
--- source/egov-erp/egov-pgrweb/src/test/java/org/egov/pgrweb/controller/ComplaintTypeControllerTest.java
+++ source/egov-erp/egov-pgrweb/src/test/java/org/egov/pgrweb/controller/ComplaintTypeControllerTest.java
@@ -1,20 +1,31 @@
 package org.egov.pgrweb.controller;
 
+import org.egov.lib.rjbac.dept.Department;
+import org.egov.lib.rjbac.dept.DepartmentImpl;
+import org.egov.lib.rjbac.dept.ejb.api.DepartmentService;
 import org.junit.Test;
+import org.mockito.Mock;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 public class ComplaintTypeControllerTest extends AbstractContextControllerTest<ComplaintTypeController> {
+
+    @Mock
+    private DepartmentService departmentService;
 
     @Override
     protected ComplaintTypeController initController() {
         initMocks(this);
 
-        return new ComplaintTypeController();
+        return new ComplaintTypeController(departmentService);
     }
 
     @Test
@@ -22,5 +33,23 @@ public class ComplaintTypeControllerTest extends AbstractContextControllerTest<C
         this.mockMvc.perform(get("/complaint-type"))
                 .andExpect(view().name("complaint-type"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldAddDepartmentsAsModelAttribute() throws Exception {
+        DepartmentImpl department1 = new DepartmentImpl();
+        department1.setDeptName("dep1");
+        DepartmentImpl department2 = new DepartmentImpl();
+        department2.setDeptName("dep2");
+
+        List<Department> departments = Arrays.asList(department1, department2);
+        when(departmentService.getAllDepartments()).thenReturn(departments);
+
+        this.mockMvc.perform(get("/complaint-type"))
+                .andExpect(view().name("complaint-type"))
+                .andExpect(model().attribute("departments", departments))
+                .andExpect(status().isOk());
+
+        verify(departmentService).getAllDepartments();
     }
 }
