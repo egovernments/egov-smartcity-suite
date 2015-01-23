246982c95a6b3134feb2daaf7e610a0c2276f6ea
sreekanth.nair <sreekanth.nair@egovernments.org>
2015-01-23 09:59:18 +0530

    PHOENIX-4 #start-progress User Type Based redirect, added TODO in class and should be picked upon User Type added to User

---
+++ source/egov-erp/egov-egi/src/main/java/org/egov/infstr/security/spring/AuthenticationSuccessHandler.java
@@ -0,0 +1,36 @@
+package org.egov.infstr.security.spring;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.egov.lib.rjbac.user.User;
+import org.egov.lib.rjbac.user.ejb.api.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+public class AuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+	private UserService userService;
+	
+	@Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException,    ServletException {          
+		
+		
+		//TODO we dont have a user type yet upon implementing user type update 
+		//this method with appropriate logic to redirect
+		/*
+		 * User user = this.userService.getUserByUserName(authentication.getName());
+		 * if(user.getUserType().equals("citizen")) {
+			setDefaultTargetUrl("/citizen/homepage.action");
+		} else {
+			setDefaultTargetUrl("/common/homepage.action");
+		}*/
+		super.onAuthenticationSuccess(request, response, authentication);
+    }
+
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+}
