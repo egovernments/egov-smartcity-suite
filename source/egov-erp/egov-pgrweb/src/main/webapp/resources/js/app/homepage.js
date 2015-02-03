02bf9b5f03351c32f343600c92aab18327f35f6b
Mohammed Aslam <mdaslamrahman@gmail.com>
2015-02-03 18:36:38 +0530

    Aslam updated UI for home page

--- source/egov-erp/egov-pgrweb/src/main/webapp/resources/js/app/homepage.js
+++ source/egov-erp/egov-pgrweb/src/main/webapp/resources/js/app/homepage.js
@@ -1,6 +1,54 @@
 $(document).ready(function()
 {
-	$('#main-menu li').click(function(){
-		 $(this).children('ul').slideToggle();
+	$('#main-menu li').click(function(e){
+		$(this).children('ul').slideToggle();
+		if($(this).hasClass('has-sub'))
+		{
+			$(this).removeClass('has-sub').addClass('has-sublevel');
+		}else if($(this).hasClass('has-sublevel')){
+			$(this).removeClass('has-sublevel').addClass('has-sub');
+		}else{
+		}
+		
 	});
+	
+	$('#main-menu li ul').click(function(e){
+		e.stopPropagation();
+	});
+	
+	$('.page-container.horizontal-menu header.navbar .navbar-right > li, .page-container.horizontal-menu header.navbar .navbar-right > li ul li').hover(
+	function() {
+		$(this).children('ul').show();
+	},
+	function() {
+		$(this).children('ul').hide();
+	}
+	);
+	
+	$('.horizontal-mobile-menu').click(function(){
+		$('.sidebar-menu').slideToggle();
+	});
+	
+	
+	$(window).resize(function(){
+		if($(window).width() > 768)
+		{
+			$('.sidebar-menu').css('display','table-cell');
+			}else{
+			$('.sidebar-menu').css('display','none');
+		}
+		
+	});
+	
+	$('.sidebar-collapse-icon').click(function(){
+		if($('.sidebar-menu').hasClass('collapsed-menu'))
+		{
+			//$('.sidebar-menu').removeClass('collapsed-menu');
+		}else{
+			//$('.sidebar-menu').addClass('collapsed-menu');
+			//$('.sidebar-user-info').hide();
+		}
+		
+	});
+	
 });
