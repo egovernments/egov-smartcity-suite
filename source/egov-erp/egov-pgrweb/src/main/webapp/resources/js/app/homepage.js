f14d22f2c7132c105be14d3de5f8bc2503fdfe13
Mohammed Aslam <mdaslamrahman@gmail.com>
2015-02-04 17:51:37 +0530

    collapsed side bar for left navigation menu

--- source/egov-erp/egov-pgrweb/src/main/webapp/resources/js/app/homepage.js
+++ source/egov-erp/egov-pgrweb/src/main/webapp/resources/js/app/homepage.js
@@ -5,9 +5,9 @@ $(document).ready(function()
 		if($(this).hasClass('has-sub'))
 		{
 			$(this).removeClass('has-sub').addClass('has-sublevel');
-		}else if($(this).hasClass('has-sublevel')){
+			}else if($(this).hasClass('has-sublevel')){
 			$(this).removeClass('has-sublevel').addClass('has-sub');
-		}else{
+			}else{
 		}
 		
 	});
@@ -43,12 +43,61 @@ $(document).ready(function()
 	$('.sidebar-collapse-icon').click(function(){
 		if($('.sidebar-menu').hasClass('collapsed-menu'))
 		{
-			//$('.sidebar-menu').removeClass('collapsed-menu');
-		}else{
-			//$('.sidebar-menu').addClass('collapsed-menu');
-			//$('.sidebar-user-info').hide();
+			$('#main-menu').removeClass('sidebar-collapsed');
+			$('#main-menu li a span, #main-menu li ul li').css({'visibility':'visible'});
+			$('.sidebar-menu, .sidebar-menu-inner').removeClass('collapsed-menu');
+			$('.logo-env').removeClass('collapsed-logo');
+			$('#change-menu-logo').removeClass('entypo-right-dir').addClass('entypo-left-dir');
+			//$('#main-menu > li').addClass('has-sub');
+			$('#main-menu > li > a > span').removeClass('collapsed-span');
+			$('#main-menu > li > ul').removeClass('collapsed-ul');
+			$('#main-menu > li a span.badge').removeClass('collapsed-badge');
+			$('.sidebar-user-info').show();
+			}else{
+			/*collapsed*/
+			$('#main-menu').addClass('sidebar-collapsed');
+			$('#main-menu li a span, #main-menu ul').css({'visibility':'hidden'});
+			$('#main-menu ul').css('display','none');
+			$('.sidebar-menu, .sidebar-menu-inner').addClass('collapsed-menu');
+			$('.logo-env').addClass('collapsed-logo');
+			$('#change-menu-logo').removeClass('entypo-left-dir').addClass('entypo-right-dir');
+			$('#main-menu > li').removeClass('has-sub has-sublevel');
+			$('#main-menu > li > a > span').addClass('collapsed-span');
+			$('#main-menu > li > ul').addClass('collapsed-ul');
+			$('#main-menu > li a span.badge').addClass('collapsed-badge');
+			$('.sidebar-user-info').hide();
 		}
 		
 	});
 	
-});
+	$('.page-container .sidebar-menu #main-menu li').hover(function(e){
+		if($('#main-menu').hasClass('sidebar-collapsed'))
+		{
+			$(this).children('a').children('span').css('visibility','visible');
+			$(this).children('ul').css({'visibility':'visible', 'display':'block'});
+			$(this).children('ul').children('li').children('a').children('span').css({'visibility':'visible'});
+			}else{
+				
+		}
+	},
+	function() {
+		if($('#main-menu').hasClass('sidebar-collapsed'))
+		{
+			$(this).children('a').children('span').css('visibility','hidden');
+			$(this).children('ul').css({'visibility':'hidden', 'display':'none'});
+			}else{
+		}
+	});
+	
+	
+	$(".page-container .sidebar-menu #main-menu li ul li").mouseleave(function () {
+		if($('#main-menu').hasClass('sidebar-collapsed'))
+		{
+			$(this).children('a').children('span').css({'visibility':'visible', 'display':'block'});
+		}else{
+		}
+		
+	});
+	
+	
+	});	
