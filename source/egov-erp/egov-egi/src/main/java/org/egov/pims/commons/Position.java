d0d1764c6db91d5d151b75ba937274bbfdefb92f
vaibhav.k-egovernments.org <vaibhav.k@egovernments.org>
2015-02-10 19:48:25 +0530

    PHOENIX-6 #time 4d Migration of EIS to Master Branch(DB Scripts) - Vaibhav.K|Mani

--- source/egov-erp/egov-egi/src/main/java/org/egov/pims/commons/Position.java
+++ source/egov-erp/egov-egi/src/main/java/org/egov/pims/commons/Position.java
@@ -1,33 +1,21 @@
-/*
- * @(#)Position.java 3.0, 7 Jun, 2013 8:34:48 PM
- * Copyright 2013 eGovernments Foundation. All rights reserved. 
- * eGovernments PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
 package org.egov.pims.commons;
 
 import java.util.Date;
 
+import org.egov.lib.rjbac.user.User;
+
 public class Position {
 	
 	private Integer id;
+	private User createdBy;
+	private Date createdDate;
+	private User modifiedBy;
+	private Date modifiedDate;
 	private String name;
-	private Integer sanctionedPosts;//move todept desig newobj
-	private Integer outsourcedPosts;
-	private DesignationMaster desigId;
-	private Date efferctiveDate;//remove this
-	private DrawingOfficer drawingOfficer;
+	//private DrawingOfficer drawingOfficer;
 	private DeptDesig deptDesigId;
 	private Integer isPostOutsourced;
-	
 
-	public Integer getId() {
-		return id;
-	}
-	
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	
 	public String getName() {
 		return name;
 	}
@@ -36,46 +24,6 @@ public class Position {
 		this.name = name;
 	}
 	
-	public Integer getOutsourcedPosts() {
-		return outsourcedPosts;
-	}
-	
-	public void setOutsourcedPosts(Integer outsourcedPosts) {
-		this.outsourcedPosts = outsourcedPosts;
-	}
-	
-	public Integer getSanctionedPosts() {
-		return sanctionedPosts;
-	}
-	
-	public void setSanctionedPosts(Integer sanctionedPosts) {
-		this.sanctionedPosts = sanctionedPosts;
-	}
-	
-	public DesignationMaster getDesigId() {
-		return desigId;
-	}
-	
-	public void setDesigId(DesignationMaster desigId) {
-		this.desigId = desigId;
-	}
-	
-	public Date getEfferctiveDate() {
-		return efferctiveDate;
-	}
-	
-	public void setEfferctiveDate(Date efferctiveDate) {
-		this.efferctiveDate = efferctiveDate;
-	}
-
-	public DrawingOfficer getDrawingOfficer() {	 	
-		return drawingOfficer;
-	}
-
-	public void setDrawingOfficer(DrawingOfficer drawingOfficer) {
-		this.drawingOfficer = drawingOfficer;
-	}
-
 	public DeptDesig getDeptDesigId() {
 		return deptDesigId;
 	}
@@ -91,5 +39,45 @@ public class Position {
 	public void setIsPostOutsourced(Integer isPostOutsourced) {
 		this.isPostOutsourced = (isPostOutsourced==null?0:isPostOutsourced);
 	}
+
+	public User getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(User modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+
+	public Date getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public User getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(User createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}	
 
 }
