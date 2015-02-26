c7f65eaf9f5e73a954555526201a4046bf6258c1
parvati <parvati.hiremath@governation.com>
2015-02-26 14:48:53 +0530

    Parvati - PHOENIX-14 - Status Role Mapping master data added in sql and deleted from sample data

--- source/egov-erp/egov-database/src/main/resources/sql/00019_pgr_status-role-mapping_DML.sql
+++ source/egov-erp/egov-database/src/main/resources/sql/00019_pgr_status-role-mapping_DML.sql
@@ -1,7 +1,75 @@
+DELETE FROM eg_roles where role_name='PGR_Operator';
+
 INSERT INTO eg_roles(id_role,role_name,role_desc,role_name_local,role_desc_local,updatetime,updateuserid) values
 (nextval('seq_eg_roles'),'PGR_Operator','PGR_Operator',null,null,null,null);
 
 DELETE FROM pgr_complaintstatus_mapping;
+
+--Super User role
+
+INSERT INTO pgr_complaintstatus_mapping(id,role_id,current_status_id,orderno,show_status_id) VALUES
+(nextval('seq_pgr_complaintstatus_mapping'),(select id_role from eg_roles where role_name='SuperUser'),
+(select id from pgr_complaintstatus where name='REGISTERED'),1,(select id from pgr_complaintstatus where name='REGISTERED'));
+
+INSERT INTO pgr_complaintstatus_mapping(id,role_id,current_status_id,orderno,show_status_id) VALUES
+(nextval('seq_pgr_complaintstatus_mapping'),(select id_role from eg_roles where role_name='SuperUser'),
+(select id from pgr_complaintstatus where name='REGISTERED'),2,(select id from pgr_complaintstatus where name='FORWARDED'));
+
+INSERT INTO pgr_complaintstatus_mapping(id,role_id,current_status_id,orderno,show_status_id) VALUES
+(nextval('seq_pgr_complaintstatus_mapping'),(select id_role from eg_roles where role_name='SuperUser'),
+(select id from pgr_complaintstatus where name='REGISTERED'),3,(select id from pgr_complaintstatus where name='PROCESSING'));
+
+INSERT INTO pgr_complaintstatus_mapping(id,role_id,current_status_id,orderno,show_status_id) VALUES
+(nextval('seq_pgr_complaintstatus_mapping'),(select id_role from eg_roles where role_name='SuperUser'),
+(select id from pgr_complaintstatus where name='REGISTERED'),4,(select id from pgr_complaintstatus where name='REJECTED'));
+
+INSERT INTO pgr_complaintstatus_mapping(id,role_id,current_status_id,orderno,show_status_id) VALUES
+(nextval('seq_pgr_complaintstatus_mapping'),(select id_role from eg_roles where role_name='SuperUser'),
+(select id from pgr_complaintstatus where name='REGISTERED'),5,(select id from pgr_complaintstatus where name='COMPLETED'));
+
+INSERT INTO pgr_complaintstatus_mapping(id,role_id,current_status_id,orderno,show_status_id) VALUES
+(nextval('seq_pgr_complaintstatus_mapping'),(select id_role from eg_roles where role_name='SuperUser'),
+(select id from pgr_complaintstatus where name='FORWARDED'),1,(select id from pgr_complaintstatus where name='FORWARDED'));
+
+INSERT INTO pgr_complaintstatus_mapping(id,role_id,current_status_id,orderno,show_status_id) VALUES
+(nextval('seq_pgr_complaintstatus_mapping'),(select id_role from eg_roles where role_name='SuperUser'),
+(select id from pgr_complaintstatus where name='FORWARDED'),2,(select id from pgr_complaintstatus where name='PROCESSING'));
+
+INSERT INTO pgr_complaintstatus_mapping(id,role_id,current_status_id,orderno,show_status_id) VALUES
+(nextval('seq_pgr_complaintstatus_mapping'),(select id_role from eg_roles where role_name='SuperUser'),
+(select id from pgr_complaintstatus where name='FORWARDED'),3,(select id from pgr_complaintstatus where name='REJECTED'));
+
+INSERT INTO pgr_complaintstatus_mapping(id,role_id,current_status_id,orderno,show_status_id) VALUES
+(nextval('seq_pgr_complaintstatus_mapping'),(select id_role from eg_roles where role_name='SuperUser'),
+(select id from pgr_complaintstatus where name='FORWARDED'),4,(select id from pgr_complaintstatus where name='COMPLETED'));
+
+
+INSERT INTO pgr_complaintstatus_mapping(id,role_id,current_status_id,orderno,show_status_id) VALUES
+(nextval('seq_pgr_complaintstatus_mapping'),(select id_role from eg_roles where role_name='SuperUser'),
+(select id from pgr_complaintstatus where name='PROCESSING'),1,(select id from pgr_complaintstatus where name='PROCESSING'));
+
+
+INSERT INTO pgr_complaintstatus_mapping(id,role_id,current_status_id,orderno,show_status_id) VALUES
+(nextval('seq_pgr_complaintstatus_mapping'),(select id_role from eg_roles where role_name='SuperUser'),
+(select id from pgr_complaintstatus where name='PROCESSING'),2,(select id from pgr_complaintstatus where name='FORWARDED'));
+
+INSERT INTO pgr_complaintstatus_mapping(id,role_id,current_status_id,orderno,show_status_id) VALUES
+(nextval('seq_pgr_complaintstatus_mapping'),(select id_role from eg_roles where role_name='SuperUser'),
+(select id from pgr_complaintstatus where name='PROCESSING'),3,(select id from pgr_complaintstatus where name='REJECTED'));
+
+INSERT INTO pgr_complaintstatus_mapping(id,role_id,current_status_id,orderno,show_status_id) VALUES
+(nextval('seq_pgr_complaintstatus_mapping'),(select id_role from eg_roles where role_name='SuperUser'),
+(select id from pgr_complaintstatus where name='PROCESSING'),4,(select id from pgr_complaintstatus where name='COMPLETED'));
+
+
+INSERT INTO pgr_complaintstatus_mapping(id,role_id,current_status_id,orderno,show_status_id) VALUES
+(nextval('seq_pgr_complaintstatus_mapping'),(select id_role from eg_roles where role_name='SuperUser'),
+(select id from pgr_complaintstatus where name='REJECTED'),1,(select id from pgr_complaintstatus where name='REJECTED'));
+
+
+INSERT INTO pgr_complaintstatus_mapping(id,role_id,current_status_id,orderno,show_status_id) VALUES
+(nextval('seq_pgr_complaintstatus_mapping'),(select id_role from eg_roles where role_name='SuperUser'),
+(select id from pgr_complaintstatus where name='COMPLETED'),1,(select id from pgr_complaintstatus where name='COMPLETED'));
 
 ---GRIEVANCE_OFFICER
 
