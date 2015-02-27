b5ce767e052f26be57ed27cce9008abe9d74a40a
sreekanth.nair <sreekanth.nair@egovernments.org>
2015-02-27 13:11:24 +0530

    PHOENIX-11 Added additional validation for CRN wrt Receiving Center selected

--- source/egov-erp/egov-pgrweb/src/main/java/org/egov/pgr/web/controller/complaint/officials/OfficialsComplaintRegistrationController.java
+++ source/egov-erp/egov-pgrweb/src/main/java/org/egov/pgr/web/controller/complaint/officials/OfficialsComplaintRegistrationController.java
@@ -36,6 +36,8 @@ public class OfficialsComplaintRegistrationController extends GenericComplaintCo
 
     @RequestMapping(value = "register", method = POST)
     public String registerComplaint(@Valid @ModelAttribute final Complaint complaint, final BindingResult resultBinder, @RequestParam("files") final MultipartFile[] files) {
+        if(complaint.getReceivingCenter().isCrnRequired() && complaint.getCRN().isEmpty()) 
+            resultBinder.rejectValue("crn", "crn.mandatory.for.receivingcenter");
         if (resultBinder.hasErrors())
             return "complaint/officials/registration-form";
         complaint.setSupportDocs(addToFileStore(files));
