50be2fcbc1b593bfa45ea02c487dcb71b5cf2290
vaibhav.k-egovernments.org <vaibhav.k@egovernments.org>
2015-03-12 17:35:12 +0530

    Vaibhav.K - PHOENIX-19 Corrected the script numbering

--- source/egov-erp/egov-database/src/main/resources/sql/00030_eis_sampledata_assignment_DML.sql
+++ source/egov-erp/egov-database/src/main/resources/sql/00030_eis_sampledata_assignment_DML.sql
@@ -1,5 +1,5 @@
 UPDATE eg_emp_assignment SET is_primary='N' WHERE id IN(2,3);
-UPDATE eg_emp_assignment SET id_employee=1 WHERE id IN(1,2,3);
+UPDATE eg_emp_assignment SET id_employee=1,from_date=current_date-365,to_date=current_date+10000 WHERE id IN(1,2,3);
 
 --rollback UPDATE eg_emp_assignment SET is_primary='Y' WHERE id IN(2,3);
 --rollback UPDATE eg_emp_assignment SET id_employee=null WHERE id IN(1,2,3);
