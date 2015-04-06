3497f937f25a09a371292a9572acf0a0550703ee
venki <venki.egovernments.org>
2015-04-06 15:48:29 +0530

    PHOENIX-47 - Citizen login page

--- source/egov-erp/egov-portalweb/src/main/java/org/egov/portal/web/controller/citizen/HomeController.java
+++ source/egov-erp/egov-portalweb/src/main/java/org/egov/portal/web/controller/citizen/HomeController.java
@@ -12,7 +12,7 @@ public class HomeController {
     @RequestMapping(method = RequestMethod.GET)
     public String LoginForm() {
 
-        return "citizen-inbox";
+        return "citizen-home";
     }
 
 }
