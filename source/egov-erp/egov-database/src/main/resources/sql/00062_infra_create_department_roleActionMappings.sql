55aabf98459d4875acc39c79d9eb6a890bb9d00b
subhash <subhash.bose@egovernments.org>
2015-04-08 18:03:47 +0530

    PHOENIX-31 Create Department Master (Seperating ddl and dml statements)

---
+++ source/egov-erp/egov-database/src/main/resources/sql/00062_infra_create_department_roleActionMappings.sql
@@ -0,0 +1,9 @@
+DELETE FROM EG_ROLEACTION_MAP WHERE ACTIONID = (SELECT id FROM EG_ACTION WHERE NAME = 'Create Department');
+
+DELETE FROM EG_ACTION WHERE NAME = 'Create Department';
+
+INSERT INTO EG_ACTION(ID, NAME, ENTITYID, TASKID, UPDATEDTIME, URL, QUERYPARAMS, URLORDERID, MODULE_ID, ORDER_NUMBER, DISPLAY_NAME, IS_ENABLED, ACTION_HELP_URL, CONTEXT_ROOT)
+Values (nextval('SEQ_EG_ACTION'), 'CreateDepartmentForm', NULL, NULL, now(), '/controller/create-department', NULL, NULL, (SELECT ID_MODULE FROM EG_MODULE WHERE MODULE_DESC = 'Department'),
+0, 'Create Department', 1, NULL, 'egi');
+
+INSERT INTO EG_ROLEACTION_MAP (ROLEID, ACTIONID) values ((select id from eg_role where UPPER(name) LIKE 'SUPERUSER') ,(select id FROM eg_action  WHERE name = 'CreateDepartmentForm'));
