cd3e11fa7ea21a5aaccfd828fd08cc2caf14287a
sreekanth.nair <sreekanth.nair@egovernments.org>
2015-04-09 20:01:20 +0530

    PHOENIX-55 Officials Home Page sql change

--- source/egov-erp/egov-database/src/main/resources/sql/00068_infra_officialhomepage_DML.sql
+++ source/egov-erp/egov-database/src/main/resources/sql/00068_infra_officialhomepage_DML.sql
@@ -1,6 +1,3 @@
-INSERT INTO eg_action("id", "name", entityid, taskid, updatedtime, url, queryparams, urlorderid, module_id, order_number, display_name, 
-is_enabled, action_help_url, context_root) VALUES (nextval('seq_eg_action'), 'OfficialsHome', null, null, now(), '/controller/home', null, 
-null,(Select id_module from eg_module where module_name='egi'), null, 'OfficialsHome', 0, null, 'egi');
 
 INSERT INTO eg_action("id", "name", entityid, taskid, updatedtime, url, queryparams, urlorderid, module_id, order_number, display_name, 
 is_enabled, action_help_url, context_root) VALUES (nextval('seq_eg_action'), 'Inbox', null, null, now(), '/controller/inbox', null, 
@@ -13,9 +10,6 @@ null,(Select id_module from eg_module where module_name='egi'), null, 'Inbox Dra
 INSERT INTO eg_action("id", "name", entityid, taskid, updatedtime, url, queryparams, urlorderid, module_id, order_number, display_name, 
 is_enabled, action_help_url, context_root) VALUES (nextval('seq_eg_action'), 'InboxHistory', null, null, now(), '/controller/inbox/history', null, 
 null,(Select id_module from eg_module where module_name='egi'), null, 'Inbox History', 0, null, 'egi');
-
-insert into eg_roleaction_map (Actionid,roleid)
-values((select id from eg_action where url='/controller/home'),(Select id from eg_role where name='SuperUser'));
 
 insert into eg_roleaction_map (Actionid,roleid)
 values((select id from eg_action where url='/inbox'),(Select id from eg_role where name='SuperUser'));
