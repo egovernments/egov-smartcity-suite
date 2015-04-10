42c25a2c0835885cb448e27e32876d8b16f036f9
sreekanth.nair <sreekanth.nair@egovernments.org>
2015-04-10 15:23:16 +0530

    PHOENIX-28 Empty values updated for eg_user and eg_city_website

---
+++ source/egov-erp/egov-database/src/main/resources/sql/00072_infra_valueupdate_DML.sql
@@ -0,0 +1,4 @@
+update eg_user set name='Unknown' where name is null;
+update eg_user set name='eGovernments' where username='egovernments';
+update eg_user set version=0;
+update eg_city_website  set logo = 'chennai_logo.jpg', version=0;
