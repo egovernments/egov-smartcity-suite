9175273a2ee3c3081ad72063cc7449bec490c1f0
Mohammed Aslam <mdaslamrahman@gmail.com>
2015-04-10 14:58:38 +0530

    Aslam - Added extra input fields in sampleform.html and customized mask screen

--- source/egov-erp/egov-egiweb/src/main/webapp/resources/js/app/homepageofficial.js
+++ source/egov-erp/egov-egiweb/src/main/webapp/resources/js/app/homepageofficial.js
@@ -26,6 +26,11 @@ $(document).ready(function()
 	$("#official_inbox tbody").on('click','tr',function(event) {
 		window.open(tableContainer1.fnGetData(this,6),'_blank','width=760,height=650');
 	});
+	
+	// MASK SCREEN IMPORTANT
+	//$('.loader-class').modal('show', {backdrop: 'static'});
+	//$('.loader-class').modal('hide');
+	
 });
 /*
  * { "data":[{"id":"30#1","sender":"Unknown / Admin","date":"09/04/2015 05:22 PM","task":"Complaint","status":"Registered","details":"CRN : CRN-tj3h6","link":"/pgr/complaint-update?id\u003d48"},{"id":"23#1","sender":"Unknown / Admin","date":"31/03/2015 08:50 PM","task":"Complaint","status":"Registered","details":"CRN : CRN-Y0QBo","link":"/pgr/complaint-update?id\u003d41"},{"id":"22#1","sender":"Unknown / Admin","date":"31/03/2015 07:31 PM","task":"Complaint","status":"Registered","details":"CRN : CRN-yvyjm","link":"/pgr/complaint-update?id\u003d40"},{"id":"21#1","sender":"Unknown / Admin","date":"31/03/2015 07:24 PM","task":"Complaint","status":"Registered","details":"CRN : CRN-57xOq","link":"/pgr/complaint-update?id\u003d39"},{"id":"20#1","sender":"Unknown / Admin","date":"31/03/2015 07:17 PM","task":"Complaint","status":"Registered","details":"CRN : CRN-3zZYS","link":"/pgr/complaint-update?id\u003d38"},{"id":"19#1","sender":"Unknown / Admin","date":"31/03/2015 11:03 AM","task":"Complaint","status":"Registered","details":"CRN : CRN-6RNTy","link":"/pgr/complaint-update?id\u003d37"},{"id":"18#1","sender":"Unknown / Admin","date":"31/03/2015 10:31 AM","task":"Complaint","status":"Registered","details":"CRN : CRN-bGOMD","link":"/pgr/complaint-update?id\u003d36"},{"id":"17#1","sender":"Unknown / Admin","date":"31/03/2015 10:18 AM","task":"Complaint","status":"Registered","details":"CRN : CRN-OlTX4","link":"/pgr/complaint-update?id\u003d33"},{"id":"15#1","sender":"Unknown / Admin","date":"30/03/2015 07:34 PM","task":"Complaint","status":"Registered","details":"CRN : CRN-K5O10","link":"/pgr/complaint-update?id\u003d30"},{"id":"13#1","sender":"Unknown / Admin","date":"30/03/2015 07:34 PM","task":"Complaint","status":"Registered","details":"CRN : CRN-LmxHD","link":"/pgr/complaint-update?id\u003d26"},{"id":"14#1","sender":"Unknown / Admin","date":"30/03/2015 07:34 PM","task":"Complaint","status":"Registered","details":"CRN : CRN-XpTSH","link":"/pgr/complaint-update?id\u003d27"}]}
