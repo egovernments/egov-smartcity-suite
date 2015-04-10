b1daf80d17e391666d8d2cba6cc5b8656c0538a1
rishi <rishi@egovernments.org>
2015-04-10 13:59:54 +0530

    PHOENIX-46 : Citizen inbox.

--- source/egov-erp/egov-portalweb/src/main/java/org/egov/portal/web/controller/citizen/HomeController.java
+++ source/egov-erp/egov-portalweb/src/main/java/org/egov/portal/web/controller/citizen/HomeController.java
@@ -33,7 +33,7 @@ public class HomeController {
 
 	private String setupHomePage(final HttpServletRequest request, final HttpSession session, final ModelMap modelData) {
 		final User user = securityUtils.getCurrentUser();
-		modelData.addAttribute("citylogo", request.getContextPath() + "/resources/global/images/" + session.getAttribute("citylogo"));
+		modelData.addAttribute("citylogo", "egi/resources/global/images/" + session.getAttribute("citylogo"));
 		modelData.addAttribute("cityName", session.getAttribute("cityname"));
 		modelData.addAttribute("userName", user.getName() == null ? "Anonymous" : user.getName());
 		modelData.addAttribute("unreadMessageCount", getUnreadMessageCount());
