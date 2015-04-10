8048bd169389b1038b85c49382a7dae592e3bf69
sreekanth.nair <sreekanth.nair@egovernments.org>
2015-04-10 13:47:14 +0530

    PHOENIX-58 Build issue fix

---
+++ source/egov-erp/egov-database/src/main/resources/sql/00071_infra_officialhomepage_DML.sql
@@ -0,0 +1,8 @@
+insert into eg_roleaction_map (Actionid,roleid)
+values((select id from eg_action where url='/controller/inbox'),(Select id from eg_role where name='SuperUser'));
+
+insert into eg_roleaction_map (Actionid,roleid)
+values((select id from eg_action where url='/controller/inbox/draft'),(Select id from eg_role where name='SuperUser'));
+
+insert into eg_roleaction_map (Actionid,roleid)
+values((select id from eg_action where url='/controller/inbox/history'),(Select id from eg_role where name='SuperUser'));
