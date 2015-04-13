1e0ae113a8264edc02e1a4640025b521220ff3c5
venki <venki.egovernments.org>
2015-04-13 16:15:00 +0530

    PHOENIX-85 - Adding link for search complaint.

---
+++ source/egov-erp/egov-database/src/main/resources/sql/00087_pgr_actionmap-complaint-search_DML.sql
@@ -0,0 +1,7 @@
+INSERT INTO EG_ACTION(ID, NAME, ENTITYID, TASKID, UPDATEDTIME, URL, QUERYPARAMS, URLORDERID, MODULE_ID, ORDER_NUMBER, DISPLAY_NAME, IS_ENABLED, ACTION_HELP_URL, CONTEXT_ROOT)
+Values (nextval('SEQ_EG_ACTION'), 'SearchComplaintForm', NULL, NULL, now(), '/complaint/citizen/anonymous/search', NULL, NULL, (SELECT ID_MODULE FROM EG_MODULE WHERE MODULE_DESC = 'Complaint'),
+0, 'Search Complaint', 1, NULL, 'pgr');
+
+INSERT INTO EG_ROLEACTION_MAP (ROLEID, ACTIONID) values ((select id_role from eg_roles where UPPER(role_name) LIKE 'SUPERUSER') ,(select id FROM eg_action  WHERE name = 'SearchComplaintForm'));
+
+
