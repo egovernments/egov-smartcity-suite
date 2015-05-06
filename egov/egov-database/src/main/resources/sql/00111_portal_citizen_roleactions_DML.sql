d3668c8109c979c776849c851f49ccda26c241a9
venki <venki@egovernments.org>
2015-05-06 19:48:42 +0530

    venki - PHOENIX-167 |  Fixing script issue.

--- egov/egov-database/src/main/resources/sql/00111_portal_citizen_roleactions_DML.sql
+++ egov/egov-database/src/main/resources/sql/00111_portal_citizen_roleactions_DML.sql
@@ -2,14 +2,13 @@ DELETE FROM EG_ROLEACTION_MAP WHERE ROLEID IN (select id from eg_role where UPPE
 
 DELETE FROM eg_userrole where roleid   IN (select id from eg_role where UPPER(name) LIKE 'CITIZEN USER');
 
-delete from eg_role  where name ='Citizen User';
+DELETE FROM  eg_role  where name ='Citizen User';
 
-DELETE FROM EG_ROLEACTION_MAP WHERE ACTIONID IN ((select id FROM eg_action  WHERE name = 'ComplaintRegisteration')) AND ROLEID IN (select id from eg_role where UPPER(name) LIKE 'SUPERUSER'));
+INSERT INTO EG_USERROLE (ROLEID, USERID) values ((select id from eg_role where name LIKE 'Citizen') ,(select id FROM eg_user  WHERE  username ='citizenUser'));
 
+DELETE FROM EG_ROLEACTION_MAP WHERE ACTIONID IN (select id FROM eg_action  WHERE name = 'ComplaintRegisteration') AND ROLEID IN (select id from eg_role where UPPER(name) LIKE 'SUPERUSER');
 
 update eg_user set username = '9999999999', mobilenumber = '9999999999' where username ='citizenUser';
-
-
 
 INSERT INTO EG_ROLEACTION_MAP (ROLEID, ACTIONID) values ((select id from eg_role where name LIKE 'Citizen') ,(select id FROM eg_action  WHERE name = 'View Complaint'));
 
