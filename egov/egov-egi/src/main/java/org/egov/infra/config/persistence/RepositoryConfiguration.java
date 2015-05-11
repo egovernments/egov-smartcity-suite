3b7aad5ce498c53813c78f6f0b75c95e84e5e62d
sreekanth.nair <sreekanth.nair@egovernments.org>
2015-05-11 18:42:02 +0530

    Enabling auditing at spring data jpa level[PHOENIX-141]

--- egov/egov-egi/src/main/java/org/egov/infra/config/persistence/RepositoryConfiguration.java
+++ egov/egov-egi/src/main/java/org/egov/infra/config/persistence/RepositoryConfiguration.java
@@ -47,11 +47,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories({"org.egov.**.repository"})
+@EnableJpaRepositories(basePackages="org.egov.**.repository",repositoryFactoryBeanClass=EnversRevisionRepositoryFactoryBean.class)
 @EnableJpaAuditing
 public class RepositoryConfiguration {
  
