b44be1b607bbcc1f050ce22faacc565691eb72f2
Aslam <aslam.rahman@egovernments.org>
2015-06-17 17:22:56 +0530

    PHOENIX-32 - Added pattern validation for text field

---
+++ egov/egov-egiweb/src/main/webapp/resources/global/js/egov/patternvalidation.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+	//Add class to input field "patternvalidation" and add custom data attribute(eg: data-pattern="alphabetwithspace") 
+
+	//This will allow you to enter only alphabets with or without space. (eg: data-pattern="alphabetwithspace")
+	var regexp_alphabet = /[^a-zA-Z ]/g ; 
+
+	//This will allow you to enter only alphabets with specified special characters like dot(.), slash(/), hash(#), ampersand(&), plus(+), minus(-). If you need some additional special characters, add those characters to the corresponding regular expression. 
+	//(eg: data-pattern="alphabetwithspecialcharacters")
+	var regexp_alphabetspecialcharacters = /[^a-zA-Z_@./#&+-]*$/ ;
+
+	//This will allow you to enter only numbers. (eg: data-pattern="number")
+	var regexp_number = /[^0-9]/g ;
+
+	//This will allow you to enter alphabets and numbers with or without space. (eg: data-pattern="alphanumericwithspace")
+	var regexp_alphanumeric = /[^a-zA-Z0-9 ]/g ;
+
+	//This will allow you to enter alphabets and numbers with specified special characters like dot(.), slash(/), hash(#), ampersand(&), plus(+), minus(-). If you need some additional special characters, add those characters to the corresponding regular expression. (eg: data-pattern="alphanumericwithspecialcharacters")
+	var regexp_alphanumericspecialcharacters = /[^a-zA-Z0-9_@./#&+-]*$/ ;
+
+	//This will allow you to enter alphabets and numbers with space, hyphen(-) and underscore(_). (eg: data-pattern="alphanumericwithspace-hyphen-underscore")
+	var regexp_alphanumerichyphenunderscore = /[^a-zA-Z0-9 _-]/g ;
+
+
+	$('.patternvalidation').on("input", function(){
+
+		if($(this).data('pattern') === "alphabetwithspace"){ //alphabetwithspaces
+			if($(this).val().match(regexp_alphabet)){
+				$(this).val( $(this).val().replace(regexp_alphabet,'') );
+			}
+		}else if($(this).data('pattern') === "alphabetwithspecialcharacters"){ //alphabetwithspecialcharacters
+			if($(this).val().match(regexp_alphabetspecialcharacters)){
+				$(this).val( $(this).val().replace(regexp_alphabetspecialcharacters,'') );
+			}
+		}else if($(this).data('pattern') === "number"){ //number
+			if($(this).val().match(regexp_number)){
+				$(this).val( $(this).val().replace(regexp_number,'') );
+			}
+		}else if($(this).data('pattern') === "alphanumericwithspace"){ //alphanumericwithspaces
+			if($(this).val().match(regexp_alphanumeric)){
+				$(this).val( $(this).val().replace(regexp_alphanumeric,'') );
+			}
+		}else if($(this).data('pattern') === "alphanumericwithspecialcharacters"){ //alphanumericwithspecialcharacters
+			if($(this).val().match(regexp_alphanumericspecialcharacters)){
+				$(this).val( $(this).val().replace(regexp_alphanumericspecialcharacters,'') );
+			}
+		}else if($(this).data('pattern') === "alphanumericwithspace-hyphen-underscore"){ //alphanumericwith-hyphen-underscore
+			if($(this).val().match(regexp_alphanumerichyphenunderscore)){
+				$(this).val( $(this).val().replace(regexp_alphanumerichyphenunderscore,'') );
+			}
+		}
+
+	});
+
+});
---
+++ egov/egov-egiweb/src/main/webapp/resources/global/templates/readme.txt
@@ -0,0 +1,32 @@
+
+=== Pattern Validation ===
+	
+Add class to input field "patternvalidation" and add custom data attribute(eg: data-pattern="alphabetwithspace") 
+
+1) data-pattern="alphabetwithspace"
+
+	This will allow you to enter only alphabets with or without space.
+
+2) data-pattern="alphabetwithspecialcharacters"
+
+	This will allow you to enter only alphabets with specified special characters like dot(.), slash(/), hash(#), ampersand(&), plus(+), minus(-).
+	
+	If you need some additional special characters, add those characters to the corresponding regular expression.
+
+3) data-pattern="number"		
+
+	This will allow you to enter only numbers.
+
+4) data-pattern="alphanumericwithspace"
+
+	This will allow you to enter alphabets and numbers with or without space.
+
+5)  data-pattern="alphanumericwithspecialcharacters"
+
+	This will allow you to enter alphabets and numbers with specified special characters like dot(.), slash(/), hash(#), ampersand(&), plus(+), minus(-).
+	
+	If you need some additional special characters, add those characters to the corresponding regular expression.
+
+6) data-pattern="alphanumericwithspace-hyphen-underscore"
+
+	This will allow you to enter alphabets and numbers with space, hyphen(-) and underscore(_).
