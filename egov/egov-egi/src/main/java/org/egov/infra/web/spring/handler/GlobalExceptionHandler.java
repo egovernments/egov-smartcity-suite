10932268c3bf73d1884cd21a16c130603a7f4880
sreekanth.nair <sreekanth.nair@egovernments.org>
2015-07-01 11:48:55 +0530

    Changed error page to be redirected instead of forward since page refresh call making duplicate form submit [PHOENIX-58]

--- egov/egov-egi/src/main/java/org/egov/infra/web/spring/handler/GlobalExceptionHandler.java
+++ egov/egov-egi/src/main/java/org/egov/infra/web/spring/handler/GlobalExceptionHandler.java
@@ -1,10 +1,10 @@
-/**
- * eGov suite of products aim to improve the internal efficiency,transparency, 
+/*
+   eGov suite of products aim to improve the internal efficiency,transparency,
    accountability and the service delivery of the government  organizations.
 
     Copyright (C) <2015>  eGovernments Foundation
 
-    The updated version of eGov suite of products as by eGovernments Foundation 
+    The updated version of eGov suite of products as by eGovernments Foundation
     is available at http://www.egovernments.org
 
     This program is free software: you can redistribute it and/or modify
@@ -18,21 +18,21 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program. If not, see http://www.gnu.org/licenses/ or 
+    along with this program. If not, see http://www.gnu.org/licenses/ or
     http://www.gnu.org/licenses/gpl.html .
 
     In addition to the terms of the GPL license to be adhered to in using this
     program, the following additional terms are to be complied with:
 
-	1) All versions of this program, verbatim or modified must carry this 
+	1) All versions of this program, verbatim or modified must carry this
 	   Legal Notice.
 
-	2) Any misrepresentation of the origin of the material is prohibited. It 
-	   is required that all modified versions of this material be marked in 
+	2) Any misrepresentation of the origin of the material is prohibited. It
+	   is required that all modified versions of this material be marked in
 	   reasonable ways as different from the original version.
 
-	3) This license does not grant any rights to any user of the program 
-	   with regards to rights under trademark law for use of the trade names 
+	3) This license does not grant any rights to any user of the program
+	   with regards to rights under trademark law for use of the trade names
 	   or trademarks of eGovernments Foundation.
 
   In case of any queries, you can reach eGovernments Foundation at contact@egovernments.org.
@@ -48,16 +48,18 @@ import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.FlashMap;
+import org.springframework.web.servlet.support.RequestContextUtils;
+import org.springframework.web.servlet.view.RedirectView;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     private static final Logger LOG = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-    public static final String DEFAULT_ERROR_VIEW = "error/500";
+    public static final String DEFAULT_ERROR_VIEW = "/error/500";
 
     @ExceptionHandler(Exception.class)
-    public ModelAndView defaultErrorHandler(final HttpServletRequest req, final Exception e) throws Exception {
+    public RedirectView defaultErrorHandler(final HttpServletRequest req, final Exception e) throws Exception {
         // If the exception is annotated with @ResponseStatus rethrow it and let
         // the framework handle it.
         LOG.error("An error occurred while processing the request", e);
@@ -65,18 +67,20 @@ public class GlobalExceptionHandler {
             throw e;
         return createErrorMV(req, e);
     }
-    
+
     @ExceptionHandler(value = EGOVRuntimeException.class)
-    public ModelAndView egovErrorHandler(HttpServletRequest req, EGOVRuntimeException e) throws Exception {
-       return createErrorMV(req, e);
+    public RedirectView egovErrorHandler(final HttpServletRequest req, final EGOVRuntimeException e) throws Exception {
+        return createErrorMV(req, e);
     }
 
-    private ModelAndView createErrorMV(HttpServletRequest req, Exception e) {
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("error",e.getMessage());
-        mav.addObject("url", req.getRequestURL());
-        mav.setViewName(DEFAULT_ERROR_VIEW);
-        return mav;
+    private RedirectView createErrorMV(final HttpServletRequest request, final Exception e) {
+        final RedirectView rw = new RedirectView(DEFAULT_ERROR_VIEW, true);
+        final FlashMap outputFlashMap = RequestContextUtils.getOutputFlashMap(request);
+        if (outputFlashMap != null) {
+            outputFlashMap.put("error", e.getMessage());
+            outputFlashMap.put("url", request.getRequestURL());
+        }
+        return rw;
     }
 
 }
