e50d5b9a179014a428e5d8a04f6d67ace366fdfb
Elzan Mathew <elzan.mathew@egovernments.org>
2015-07-07 10:15:11 +0530

    Elzan- Phoenix-221 implemented drill down in compliant search

--- egov/egov-pgrweb/src/main/webapp/resources/js/app/search-complaint.js
+++ egov/egov-pgrweb/src/main/webapp/resources/js/app/search-complaint.js
@@ -42,7 +42,8 @@ var tableContainer;
 jQuery(document).ready(function ($) {
     $(":input").inputmask();
 	
- //   tableContainer1 = $("#complaintSearchResults"); 
+    tableContainer1 = $("#complaintSearchResults"); 
+    
     $(".datepicker").datepicker({
         format: "dd/mm/yyyy"
 	});
@@ -63,7 +64,7 @@ jQuery(document).ready(function ($) {
 		.done(function (searchResult) {
 			console.log(JSON.stringify(searchResult));
 			
-			$('#complaintSearchResults').dataTable({
+			tableContainer1 = $('#complaintSearchResults').dataTable({
 				destroy:true,
 				"sPaginationType": "bootstrap",
 				"sDom": "<'row'<'col-xs-12 hidden col-right'f>r>t<'row'<'col-md-6 col-xs-12'i><'col-md-3 col-xs-6'l><'col-md-3 col-xs-6 text-right'p>>",
@@ -96,18 +97,6 @@ jQuery(document).ready(function ($) {
 			});
 		})
 	});
-	
-   
- /*   $('#complaintSearchResults').click(function () {
-    alert("show me....");
-   // $("#complaintSearchResults").val(dataTable.fnGetData(this,0));
-    alert(tableContainer1);
-    alert(tableContainer1.fnGetSelectedData(this,1))
-   // var crn=resource.searchable.crn;
-    
-    window.open("/pgr/complaint/update/030715-GT-00016");
-    
-    });*/
     
     tableContainer = $("#csearch").dataTable({
 		"sPaginationType": "bootstrap",
@@ -121,8 +110,14 @@ jQuery(document).ready(function ($) {
 		}
 	});
 	
-	tableContainer.columnFilter({
+	/*tableContainer.columnFilter({
 		"sPlaceHolder": "head:after"
+	});*/
+	
+	$("#complaintSearchResults").on('click','tbody tr',function(event) {
+		//alert(tableContainer1.fnGetData(this,0));
+		var crn=tableContainer1.fnGetData(this,0);
+		window.open("/pgr/complaint/update/"+crn);
 	});
 	
     $("#when_date").change(function () {
@@ -167,84 +162,6 @@ jQuery(document).ready(function ($) {
 			$("#start_date").val("");
 			break;
 			
-			/* case "lastyear":
-				var start = ((quarter == 4) ? (d.getFullYear() - 2) : (d.getFullYear() - 1));
-				var end = ((quarter == 4) ? (d.getFullYear() - 1) : (d.getFullYear()));
-				var pyearstart = new Date(start, 3, 1);
-				var pyearend = new Date(end, 2, 31);
-				$("#start_date").datepicker("setDate", pyearstart);
-				$("#end_date").datepicker("setDate", pyearend);
-				break;
-				
-				case "thisyear":
-				var start = ((quarter == 4) ? (d.getFullYear() - 1) : (d.getFullYear()));
-				var end = ((quarter == 4) ? (d.getFullYear()) : (d.getFullYear() + 1));
-				var cyearstart = new Date(start, 3, 1);
-				var cyearend = new Date(end, 2, 31);
-				$("#start_date").datepicker("setDate", cyearstart);
-				$("#end_date").datepicker("setDate", cyearend);
-				break;
-				
-				case "lastquarter":
-				var lqy = ((quarter == 4) ? d.getFullYear() - 1 : d.getFullYear());
-				var firstDate = new Date(lqy, quarter * 3 - 3, 1);
-				$("#start_date").datepicker("setDate", firstDate);
-				$("#end_date").datepicker("setDate", new Date(firstDate.getFullYear(), firstDate.getMonth() + 3, 0));
-				break;
-				
-				case "thisquarter":
-				var month;
-				if (quarter == 4) {
-				month = 0;
-				} else if (quarter == 1) {
-				month = 3;
-				} else if (quarter == 2) {
-				month = 6;
-				} else if (quarter == 3) {
-				month = 9;
-				}
-				
-				var firstDate = new Date(d.getFullYear(), month, 1);
-				$("#start_date").datepicker("setDate", firstDate);
-				$("#end_date").datepicker("setDate", new Date(firstDate.getFullYear(), firstDate.getMonth() + 3, 0));
-				break;
-				
-				case "lastmonth":
-				var firstDay = new Date(d.getFullYear(), d.getMonth() - 1, 1);
-				var lastDay = new Date(d.getFullYear(), d.getMonth(), 0);
-				$("#start_date").datepicker("setDate", firstDay);
-				$("#end_date").datepicker("setDate", lastDay);
-				break;
-				
-				case "thismonth":
-				var firstDay = new Date(d.getFullYear(), d.getMonth(), 1);
-				var lastDay = new Date(d.getFullYear(), d.getMonth() + 1, 0);
-				$("#start_date").datepicker("setDate", firstDay);
-				$("#end_date").datepicker("setDate", lastDay);
-				break;
-				
-				case "lastweek":
-				var firstday = new Date(d.setDate((d.getDate() - d.getDay() - 7)));
-				var lastday = new Date(d.setDate(d.getDate() - d.getDay() + 6));
-				var firstDay = new Date(firstday.getFullYear(), firstday.getMonth(), firstday.getDate());
-				var lastDay = new Date(lastday.getFullYear(), lastday.getMonth(), lastday.getDate());
-				$("#start_date").datepicker("setDate", firstDay);
-				$("#end_date").datepicker("setDate", lastDay);
-				break;
-				
-				case "thisweek":
-				var firstday = new Date(d.setDate(d.getDate() - d.getDay()));
-				var lastday = new Date(d.setDate(d.getDate() - d.getDay() + 6));
-				var firstDay = new Date(firstday.getFullYear(), firstday.getMonth(), firstday.getDate());
-				var lastDay = new Date(lastday.getFullYear(), lastday.getMonth(), lastday.getDate());
-				$("#start_date").datepicker("setDate", firstDay);
-				$("#end_date").datepicker("setDate", lastDay);
-				break;
-				
-				case "today":
-				$("#start_date").datepicker("setDate", d);
-				$("#end_date").val("");
-			break; */
 		}
 	}
 	
