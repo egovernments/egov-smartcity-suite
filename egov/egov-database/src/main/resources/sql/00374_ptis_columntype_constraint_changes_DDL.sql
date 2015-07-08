06d26e45be72ef6f8bdfa16c3f23740df21e0123
sreekanth.nair <sreekanth.nair@egovernments.org>
2015-07-08 10:05:46 +0530

    Failing script fix [PHOENIX-58]

--- egov/egov-database/src/main/resources/sql/00374_ptis_columntype_constraint_changes_DDL.sql
+++ egov/egov-database/src/main/resources/sql/00374_ptis_columntype_constraint_changes_DDL.sql
@@ -1,2 +1,3 @@
 ALTER TABLE egpt_property_status_values ALTER COLUMN building_permission_no type varchar(25);
+UPDATE egpt_basic_property SET applicationno='01052015-XY-00001';
 ALTER TABLE egpt_basic_property ALTER COLUMN applicationno SET NOT NULL;
