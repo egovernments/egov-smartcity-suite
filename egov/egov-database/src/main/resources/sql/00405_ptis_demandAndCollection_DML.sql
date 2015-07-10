08dbc96df54371e6a88963e532c66d278798ac25
Nayeem <nayeem.pathan@egovernments.org>
2015-07-10 18:27:33 +0530

    PHOENIX-361 data required for property tax collection

--- egov/egov-database/src/main/resources/sql/00405_ptis_demandAndCollection_DML.sql
+++ egov/egov-database/src/main/resources/sql/00405_ptis_demandAndCollection_DML.sql
@@ -1,9 +1,9 @@
 INSERT INTO eg_bill_type(id, name, code, create_date, modified_date) VALUES(nextval('seq_eg_bill_type'), 'AUTO', 'AUTO', current_timestamp, current_timestamp);
 
+INSERT INTO egcl_servicecategory(id, name, code, isactive, version, createdby, createddate, lastmodifiedby, lastmodifieddate) VALUES(3, 'temp', 'TEMP', true, 0, 1, current_timestamp, 1, current_timestamp);
+
 INSERT INTO egcl_servicedetails (ID,NAME,SERVICEURL,ISENABLED,CALLBACKURL,SERVICETYPE,CODE,FUND,FUNDSOURCE,FUNCTIONARY, VOUCHERCREATION,SCHEME,SUBSCHEME,SERVICECATEGORY,ISVOUCHERAPPROVED,VOUCHERCUTOFFDATE,CREATED_BY,created_date, modified_by,modified_date) 
 VALUES (2,'Property Tax','/../ptis/view/viewDCBProperty!displayPropInfo.action?propertyId=',true,'/receipts/receipt-create.action', 'B','PT',1,null,null,true,null,null,3,true,to_date('11-07-12','DD-MM-RR'),1,current_timestamp,1,current_timestamp);
-
-INSERT INTO egcl_servicecategory(id, name, code, isactive, version, createdby, createddate, lastmodifiedby, lastmodifieddate) VALUES(3, 'temp', 'TEMP', true, 0, 1, current_timestamp, 1, current_timestamp);
 
 INSERT INTO EGF_INSTRUMENTTYPE (ID,TYPE,ISACTIVE,CREATEDBY,LASTMODIFIEDBY,CREATEDDATE,LASTMODIFIEDDATE) values (2,'cash','1',1,1, current_timestamp, current_timestamp);
 
