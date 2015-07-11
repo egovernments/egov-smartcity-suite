5d4fe7708134a42338d75997bc1905948d181dd1
venki <venki@egovernments.org>
2015-07-11 11:35:44 +0530

    venki - PHOENIX-121|Voucher view Screen Changes

--- egov/egov-database/src/main/resources/sql/00409_egf_voucherSearch_action.sql
+++ egov/egov-database/src/main/resources/sql/00409_egf_voucherSearch_action.sql
@@ -1,8 +1,7 @@
 Insert into eg_action(id,name,url,parentmodule,ordernumber,displayname,enabled,contextroot) 
-values(nextval('SEQ_EG_ACTION'),'voucherSearch','/voucher/voucherSearch-search.action',
-null,1,'voucherSearch',false,'EGF');
+values(nextval('SEQ_EG_ACTION'),'JournalVoucherSearch','/voucher/voucherSearch-search.action',
+null,1,'JournalVoucherSearch',false,'EGF');
 
-Insert into eg_roleaction   values((select id from eg_role where name='Super User'),(select id from eg_action where name='voucherSearch'));
-
+Insert into eg_roleaction   values((select id from eg_role where name='Super User'),(select id from eg_action where name='JournalVoucherSearch'));
 
 
