af5b9df5f2af3b505b398920b90dda10961a5901
Iffath <iffath@egovernments.org>
2015-07-16 16:55:22 +0530

    PHOENIX-120 : Collection corrected db scripts

--- egov/egov-database/src/main/resources/sql/00447_collections_eg_action.DML.sql
+++
@@ -1,7 +0,0 @@
-Insert into EG_ACTION ("id","name","url","queryparams","parentmodule","ordernumber","displayname","enabled","contextroot","version","createdby","createddate","lastmodifiedby","lastmodifieddate") values (nextval('seq_eg_action'),'SaveOnCancelReceipt','/receipts/receipt-saveOnCancel.action',null,(select ID from eg_module where NAME ='Receipt Services'),1,'SaveOnCancelReceipt','0','collection',0,1,current_timestamp,1,current_timestamp);
-
-Insert into EG_ACTION ("id","name","url","queryparams","parentmodule","ordernumber","displayname","enabled","contextroot","version","createdby","createddate","lastmodifiedby","lastmodifieddate") values (nextval('seq_eg_action'),'OnlineReceipt','/citizen/onlineReceipt-newform.action',null,(select ID from eg_module where NAME ='Receipt Services'),1,'OnlineReceipt','0','collection',0,1,current_timestamp,1,current_timestamp);
-
-
-
-
