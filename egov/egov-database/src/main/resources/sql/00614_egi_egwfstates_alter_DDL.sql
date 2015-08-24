8b72cc7baa9435286aba5e995c126085d180a150
rishi <rishi@egovernments.org>
2015-08-24 13:36:11 +0530

    PHOENIX-807 : Updated Auditable fields to not null.

---
+++ egov/egov-database/src/main/resources/sql/00614_egi_egwfstates_alter_DDL.sql
@@ -0,0 +1,4 @@
+ALTER TABLE eg_wf_states ALTER COLUMN createdby SET NOT NULL;
+ALTER TABLE eg_wf_states ALTER COLUMN createddate SET NOT NULL;
+ALTER TABLE eg_wf_states ALTER COLUMN lastmodifiedby SET NOT NULL;
+ALTER TABLE eg_wf_states ALTER COLUMN lastmodifieddate SET NOT NULL;
