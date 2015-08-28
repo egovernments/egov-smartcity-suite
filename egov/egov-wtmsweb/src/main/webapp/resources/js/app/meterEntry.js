83a31c2b195efb971ef749be9be6db198676761e
roopa <roopa.h@egovernments.org>
2015-08-28 14:46:57 +0530

    PHOENIX-850  issue fixed related current meter reading date should not allow less than execution date

--- egov/egov-wtmsweb/src/main/webapp/resources/js/app/meterEntry.js
+++ egov/egov-wtmsweb/src/main/webapp/resources/js/app/meterEntry.js
@@ -37,7 +37,7 @@
 # 
 #   In case of any queries, you can reach eGovernments Foundation at contact@egovernments.org.
 #-------------------------------------------------------------------------------*/
-$(document).ready(function(){
+
 	
 	var currentInstallmentExist = $('#currentInstallmentExist').val();
 	if (currentInstallmentExist) {
@@ -140,4 +140,4 @@ $(document).ready(function(){
 			
 		}
 	}
-});
+
