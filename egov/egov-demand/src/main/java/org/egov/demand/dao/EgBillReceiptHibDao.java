8a4a6cfc464c490ca005a8951fe6a001f49167d4
jayashree.reddy@egovernments.org <jayashree.reddy@egovernments.org>
2015-08-30 13:59:28 +0530

    PHOENIX-851 - BillReceipt obj was not getting updated while creating receipt.

--- egov/egov-demand/src/main/java/org/egov/demand/dao/EgBillReceiptHibDao.java
+++ egov/egov-demand/src/main/java/org/egov/demand/dao/EgBillReceiptHibDao.java
@@ -1,10 +1,10 @@
 /**
- * eGov suite of products aim to improve the internal efficiency,transparency, 
+ * eGov suite of products aim to improve the internal efficiency,transparency,
    accountability and the service delivery of the government  organizations.
 
     Copyright (C) <2015>  eGovernments Foundation
 
-    The updated version of eGov suite of products as by eGovernments Foundation 
+    The updated version of eGov suite of products as by eGovernments Foundation
     is available at http://www.egovernments.org
 
     This program is free software: you can redistribute it and/or modify
@@ -18,21 +18,21 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program. If not, see http://www.gnu.org/licenses/ or 
+    along with this program. If not, see http://www.gnu.org/licenses/ or
     http://www.gnu.org/licenses/gpl.html .
 
     In addition to the terms of the GPL license to be adhered to in using this
     program, the following additional terms are to be complied with:
 
-	1) All versions of this program, verbatim or modified must carry this 
+	1) All versions of this program, verbatim or modified must carry this
 	   Legal Notice.
 
-	2) Any misrepresentation of the origin of the material is prohibited. It 
-	   is required that all modified versions of this material be marked in 
+	2) Any misrepresentation of the origin of the material is prohibited. It
+	   is required that all modified versions of this material be marked in
 	   reasonable ways as different from the original version.
 
-	3) This license does not grant any rights to any user of the program 
-	   with regards to rights under trademark law for use of the trade names 
+	3) This license does not grant any rights to any user of the program
+	   with regards to rights under trademark law for use of the trade names
 	   or trademarks of eGovernments Foundation.
 
   In case of any queries, you can reach eGovernments Foundation at contact@egovernments.org.
@@ -56,56 +56,56 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class EgBillReceiptHibDao implements EgBillReceiptDao {
 
-	@PersistenceContext
-	private EntityManager entityManager;
+    @PersistenceContext
+    private EntityManager entityManager;
 
-	private Session getCurrentSession() {
-		return entityManager.unwrap(Session.class);
-	}
+    private Session getCurrentSession() {
+        return entityManager.unwrap(Session.class);
+    }
 
-	@Override
-	public BillReceipt getBillReceiptByEgBill(EgBill bill) {
-		BillReceipt billRecpt = null;
-		if (bill != null) {
-			Criteria criteria = getCurrentSession().createCriteria(BillReceipt.class).add(
-					Restrictions.eq("billId", bill));
-			if (criteria.list() != null && !criteria.list().isEmpty()) {
-				billRecpt = (BillReceipt) criteria.list().get(0);
-			}
-		}
+    @Override
+    public BillReceipt getBillReceiptByEgBill(final EgBill bill) {
+        BillReceipt billRecpt = null;
+        if (bill != null) {
+            final Criteria criteria = getCurrentSession().createCriteria(BillReceipt.class).add(
+                    Restrictions.eq("billId", bill));
+            if (criteria.list() != null && !criteria.list().isEmpty())
+                billRecpt = (BillReceipt) criteria.list().get(0);
+        }
 
-		return billRecpt;
+        return billRecpt;
 
-	}
+    }
 
-	@Override
-	public BillReceipt findById(Integer id, boolean lock) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public BillReceipt findById(final Integer id, final boolean lock) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public List<BillReceipt> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<BillReceipt> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public BillReceipt create(BillReceipt billReceipt) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public BillReceipt create(final BillReceipt billReceipt) {
+        getCurrentSession().saveOrUpdate(billReceipt);
+        getCurrentSession().flush();
+        return billReceipt;
+    }
 
-	@Override
-	public void delete(BillReceipt billReceipt) {
-		// TODO Auto-generated method stub
+    @Override
+    public void delete(final BillReceipt billReceipt) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public BillReceipt update(BillReceipt billReceipt) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public BillReceipt update(final BillReceipt billReceipt) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
