5836f1e2c68350bab3e3c0b52945afc6166c0b08
roopa <roopa.h@egovernments.org>
2015-09-01 12:50:04 +0530

    PHOENIX-905 A.E Login -> When click on "Submit" button, page throws "500"- Internal Server Error" message.

--- egov/egov-wtms/src/main/java/org/egov/wtms/masters/repository/DonationHeaderRepository.java
+++ egov/egov-wtms/src/main/java/org/egov/wtms/masters/repository/DonationHeaderRepository.java
@@ -58,7 +58,7 @@ public interface DonationHeaderRepository extends JpaRepository<DonationHeader, 
 
     List<DonationHeader> findByCategoryAndUsageType(ConnectionCategory category, UsageType usageType);
 
-    @Query("select dh from DonationHeader dh where dh.category=:category and usageType=:usageType and dh.minPipeSize.sizeInInch <= :pipeSize and dh.maxPipeSize.sizeInInch >= :pipeSize")
+    @Query("select dh from DonationHeader dh where dh.category=:category and usageType=:usageType and dh.minPipeSize.sizeInInch =:pipeSize ")
     DonationHeader findByCategoryAndUsageTypeAndPipeSize(@Param("category") ConnectionCategory category,
             @Param("usageType") UsageType usageType,
             @Param("pipeSize") double pipeSize);
