7d526c54d685fbb55aec29a496ad4354891ebbf5
Aslam <aslam.rahman@egovernments.org>
2015-09-07 16:12:32 +0530

    PHOENIX-926 - UI Updates in Advertisement Tax

---
+++ egov/egov-adtaxweb/src/main/webapp/resources/app/js/hoardingmaster.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    
+   $('.add-attachment').click(function(){
+       console.log('came');
+       $(this).parent().before('<div class="col-sm-3 add-margin"> <input type="file" class="form-control" required> </div>');
+   });
+    
+});
