7a7bc5d989845d029eca9ab45cf50b543f66124d
malathi <malathi@egovernments.org>
2015-09-11 17:52:41 +0530

    PHOENIX-915 : Trade License - Menutree display name changes

---
+++ egov/egov-database/src/main/resources/sql/00663_tl_menutree_DML.sql
@@ -0,0 +1,6 @@
+UPDATE EG_MODULE SET DISPLAYNAME='Reports' WHERE NAME='Trade License Reports' AND DISPLAYNAME='Trade License Reports' and CONTEXTROOT='tl';
+UPDATE EG_MODULE SET DISPLAYNAME='Transactions' WHERE NAME='Trade License Transactions' AND DISPLAYNAME='Trade License Transactions' and CONTEXTROOT='tl';
+UPDATE EG_MODULE SET DISPLAYNAME='Search' WHERE NAME='Search Trade' AND DISPLAYNAME='Search Trade' and CONTEXTROOT='tl';
+
+
+
