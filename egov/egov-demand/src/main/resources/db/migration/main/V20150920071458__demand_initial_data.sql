72b2948ae436cb957d6a0546836155ae0f032a5b
parvati <parvati.hiremath@egovernments.org>
2015-09-24 16:15:24 +0530

    PHOENIX-139: Flyway migration, Pending DML for PTIS

--- egov/egov-demand/src/main/resources/db/migration/main/V20150920071458__demand_initial_data.sql
+++ egov/egov-demand/src/main/resources/db/migration/main/V20150920071458__demand_initial_data.sql
@@ -1,6 +1,17 @@
+------------------START------------------
+
 INSERT INTO eg_reason_category (id, name, code, "order", modified_date)  values (nextval('seq_eg_reason_category'),'TAX','TAX',1,current_timestamp);
 INSERT INTO eg_reason_category (id, name, code, "order", modified_date)  values (nextval('seq_eg_reason_category'),'Fee','Fee',2,current_timestamp);
 INSERT INTO eg_reason_category (id, name, code, "order", modified_date)  values (nextval('seq_eg_reason_category'),'REBATE','REBATE',3,current_timestamp);
 INSERT INTO eg_reason_category (id, name, code, "order", modified_date)  values (nextval('seq_eg_reason_category'),'Advance Collection','ADVANCE',4,current_timestamp);
 INSERT INTO eg_reason_category (id, name, code, "order", modified_date)  values (nextval('seq_eg_reason_category'),'PENALTY','PENALTY',5,current_timestamp);
 INSERT INTO eg_reason_category (id, name, code, "order", modified_date)  values (nextval('seq_eg_reason_category'),'FINES','FINES',6,current_timestamp);
+
+-------------------END-------------------
+
+------------------START------------------
+
+INSERT INTO EG_BILL_TYPE(ID,NAME,CODE,CREATE_DATE,MODIFIED_DATE) VALUES(nextval('SEQ_EG_BILL_TYPE'),'MANUAL','MANUAL',NOW(),NOW());
+INSERT INTO eg_bill_type(id, name, code, create_date, modified_date) VALUES(nextval('seq_eg_bill_type'), 'AUTO', 'AUTO', current_timestamp, current_timestamp);
+
+-------------------END-------------------
--- egov/egov-ptis/src/main/resources/db/migration/main/V20150920071606__ptis_core_DDL.sql
+++ egov/egov-ptis/src/main/resources/db/migration/main/V20150920071606__ptis_core_DDL.sql
@@ -101,7 +101,7 @@ CREATE TABLE egpt_property_usage_master
   id bigint NOT NULL, -- Primary Key
   usg_name character varying(256) NOT NULL, -- name of prpperty usage
   modified_date date NOT NULL,
-  usage_factor double precision NOT NULL, -- Usage Factor Value
+  usage_factor double precision, -- Usage Factor Value
   usg_name_local character varying(512), -- Not used
   code character varying(16) NOT NULL, -- code for property usage (internal use)
   order_id bigint, -- Order no to display list
@@ -111,6 +111,7 @@ CREATE TABLE egpt_property_usage_master
   created_by bigint,
   modified_by bigint,
   created_date date,
+  ISRESIDENTIAL boolean NOT NULL DEFAULT false,
   CONSTRAINT pk_egpt_property_usage_master PRIMARY KEY (id)
 );
 COMMENT ON TABLE egpt_property_usage_master IS 'Master table for property usages';
@@ -238,7 +239,7 @@ CREATE TABLE egpt_floor_type
 (
   id bigint NOT NULL,
   version numeric DEFAULT 0,
-  name character varying(25),
+  name character varying(64),
   code character varying(10),
   createddate timestamp without time zone,
   lastmodifieddate timestamp without time zone,
@@ -262,7 +263,7 @@ CREATE TABLE egpt_wall_type
 (
   id bigint NOT NULL,
   version numeric DEFAULT 0,
-  name character varying(25),
+  name character varying(64),
   code character varying(10),
   createddate timestamp without time zone,
   lastmodifieddate timestamp without time zone,
@@ -282,7 +283,7 @@ CREATE TABLE egpt_wood_type
 (
   id bigint NOT NULL,
   version numeric DEFAULT 0,
-  name character varying(25),
+  name character varying(64),
   code character varying(10),
   createddate timestamp without time zone,
   lastmodifieddate timestamp without time zone,
@@ -327,7 +328,7 @@ CREATE TABLE egpt_roof_type
 (
   id bigint NOT NULL,
   version numeric DEFAULT 0,
-  name character varying(25),
+  name character varying(64),
   code character varying(10),
   createddate timestamp without time zone,
   lastmodifieddate timestamp without time zone,
@@ -485,6 +486,7 @@ COMMENT ON COLUMN egpt_propertyid.east_bounded IS 'Property East Bounded by';
 COMMENT ON COLUMN egpt_propertyid.west_bounded IS 'Property West Bounded by';
 -------------------END-------------------
 ------------------START------------------
+CREATE SEQUENCE SEQ_EGPT_BASIC_PROPERTY;
 CREATE TABLE egpt_basic_property
 (
   id bigint NOT NULL, -- Primary Key
@@ -716,6 +718,7 @@ CREATE TABLE egpt_document_files
 );
 -------------------END-------------------
 ------------------START------------------
+CREATE SEQUENCE SEQ_EGPT_MUTATION_MASTER;
 CREATE TABLE egpt_mutation_master
 (
   id bigint NOT NULL, -- Primary Key
@@ -1560,3 +1563,181 @@ CREATE TABLE egpt_unitareacalc_detail
 
 CREATE INDEX idx_pt_uacd_id_untax ON egpt_unitareacalc_detail USING btree (id_unittax);
 -------------------END-------------------
+
+------------------START------------------
+CREATE SEQUENCE SEQ_EGPT_NOTICE_NUMBER;
+CREATE SEQUENCE SEQ_EGPT_ASSESSMENT_NUMBER;
+-------------------END-------------------
+
+------------------START------------------
+-------------- view : EGPT_MV_C1_COLL_RECEIPTS ----------------
+CREATE VIEW  EGPT_MV_C1_COLL_RECEIPTS  AS select
+  cr.id_demand_detail,
+  cr.receipt_number,
+  cr.receipt_date,
+  cr.reason_amount,
+  reas.id_demand_reason_master as reasmid,
+  i.start_date as instlsdate,
+  i.end_date as instedate
+from
+  egdm_collected_receipts cr,
+  eg_demand_details dd,
+  eg_installment_master i,
+  eg_demand_reason reas
+where
+    cr.id_demand_detail = dd.id
+and reas.id_installment = i.id
+and dd.id_demand_reason = reas.id;
+
+-------------- view : egpt_mv_c2_rcpt_tax_coll ----------------
+create view egpt_mv_c2_rcpt_tax_coll as
+select 
+  receipt_number, 
+  sum(reason_amount) as tax_coll
+from 
+  egpt_mv_c1_coll_receipts
+where 
+  reasmid = (select id from eg_demand_reason_master where reasonmaster='General Tax' 
+		and module=(select id from eg_module where name='Property Tax'))
+and  (instlsdate between (select STARTINGDATE from financialyear where now() between STARTINGDATE and ENDINGDATE)  
+	and  (select ENDINGDATE from financialyear where now() between STARTINGDATE and ENDINGDATE))         
+group by receipt_number;
+
+-------------- view : egpt_mv_c3_rcpt_penalty_coll ----------------
+create view egpt_mv_c3_rcpt_penalty_coll as
+select 
+  receipt_number, 
+  sum(reason_amount) as penalty_coll
+from 
+  egpt_mv_c1_coll_receipts
+where 
+ reasmid = (select id from eg_demand_reason_master where reasonmaster='PENALTY_FINES' 
+		and module=(select id from eg_module where name='Property Tax'))
+and (instlsdate between (select STARTINGDATE from financialyear where now() between STARTINGDATE and ENDINGDATE)  
+	and  (select ENDINGDATE from financialyear where now() between STARTINGDATE and ENDINGDATE))  
+group by receipt_number;
+
+-------------- view : egpt_mv_c4_rcpt_librarycess_coll ----------------
+create view egpt_mv_c4_rcpt_librarycess_coll as
+select 
+  receipt_number, 
+  sum(reason_amount) as librarycess_coll
+from 
+  egpt_mv_c1_coll_receipts
+where 
+ reasmid = (select id from eg_demand_reason_master where reasonmaster='Library Cess' 
+		and module=(select id from eg_module where name='Property Tax'))
+and  (instlsdate between (select STARTINGDATE from financialyear where now() between STARTINGDATE and ENDINGDATE)  
+	and  (select ENDINGDATE from financialyear where now() between STARTINGDATE and ENDINGDATE))   
+group by receipt_number;
+
+-------------- view : egpt_mv_c5_rcpt_arreartax_coll ----------------
+create view egpt_mv_c5_rcpt_arreartax_coll as
+select 
+  receipt_number, 
+  sum(reason_amount) as arreartax_coll
+from 
+  egpt_mv_c1_coll_receipts
+where 
+ reasmid = (select id from eg_demand_reason_master where reasonmaster='General Tax' 
+		and module=(select id from eg_module where name='Property Tax'))
+and  (instlsdate not between (select STARTINGDATE from financialyear where now() between STARTINGDATE and ENDINGDATE)  
+	and  (select ENDINGDATE from financialyear where now() between STARTINGDATE and ENDINGDATE))      
+group by receipt_number;
+
+-------------- view : egpt_mv_c6_rcpt_arrearpenalty_coll ----------------
+create view egpt_mv_c6_rcpt_arrearpenalty_coll as
+select 
+  receipt_number, 
+  sum(reason_amount) as arrearpenalty_coll
+from 
+  egpt_mv_c1_coll_receipts
+where 
+ reasmid = (select id from eg_demand_reason_master where reasonmaster='PENALTY_FINES' 
+		and module=(select id from eg_module where name='Property Tax'))
+and (instlsdate not between (select STARTINGDATE from financialyear where now() between STARTINGDATE and ENDINGDATE)  
+	and  (select ENDINGDATE from financialyear where now() between STARTINGDATE and ENDINGDATE))  
+group by receipt_number;
+
+-------------- view : egpt_mv_c7_rcpt_arrearlibrarycess_coll ----------------
+create view egpt_mv_c7_rcpt_arrearlibrarycess_coll as
+select 
+  receipt_number, 
+  sum(reason_amount) as arrearlibrarycess_coll
+from 
+  egpt_mv_c1_coll_receipts
+where 
+ reasmid = (select id from eg_demand_reason_master where reasonmaster='Library Cess' 
+		and module=(select id from eg_module where name='Property Tax'))
+and  (instlsdate not between (select STARTINGDATE from financialyear where now() between STARTINGDATE and ENDINGDATE)  
+	and  (select ENDINGDATE from financialyear where now() between STARTINGDATE and ENDINGDATE))   
+group by receipt_number;
+
+-------------- view : egpt_mv_c8_unique_rcpt_dd ----------------
+create view egpt_mv_c8_unique_rcpt_dd as
+select
+  receipt_number,
+  receipt_date,
+  min(id_demand_detail) as id_dem_detail
+from
+  egpt_mv_c1_coll_receipts
+group by receipt_number, receipt_date;
+
+-------------- view : egpt_mv_c9_prop_coll_data ----------------
+create view egpt_mv_c9_prop_coll_data as
+select
+  c8.receipt_number,
+  c8.receipt_date,
+  bp.propertyid,
+  prop.id as idproperty,
+  pid.zone_num as zoneid, pid.ward_adm_id as wardid, pid.adm1 as areaid, pid.adm2 as localityid, pid.adm3 as streetid, 
+  ch.payeename, 
+  ch.collectiontype,
+  COALESCE(fit.type, 'cash', 'cash', 'bankchallan', 'bankchallan', 'card', 'card', 'online', 'online', 'atm', 'atm','banktobank','banktobank','cheque/dd') as payment_mode,
+  u.username
+from 
+  egpt_mv_c8_unique_rcpt_dd c8,
+  eg_demand_details dd,
+  egpt_ptdemand ptd,
+  egpt_property prop,
+  egpt_basic_property bp,
+  egpt_propertyid pid,
+  egcl_collectionheader ch,
+  egcl_collectioninstrument ci,
+  egf_instrumentheader fih,
+  egf_instrumenttype fit,
+  eg_user u
+where 
+  c8.id_dem_detail = dd.id 
+and dd.id_demand = ptd.id_demand
+and ptd.id_property = prop.id
+and prop.id_basic_property = bp.id
+and bp.id_propertyid = pid.id
+and (c8.receipt_number = ch.receiptnumber or c8.receipt_number = ch.manualreceiptnumber)
+and ch.id = ci.collectionheader
+and ci.instrumentheader = fih.id
+and cast(fih.instrumenttype as bigint) = fit.id
+and ch.createdby = u.id;
+
+-------------- view : egpt_mv_c10_report ----------------
+create view egpt_mv_c10_report as
+select 
+  c9.*, 
+  c2.tax_coll, 
+  c3.penalty_coll,
+  c4.librarycess_coll,
+  c5.arreartax_coll,
+  c6.arrearpenalty_coll,
+  c7.arrearlibrarycess_coll
+from
+  egpt_mv_c9_prop_coll_data c9 left outer join egpt_mv_c3_rcpt_penalty_coll c3 on c9.receipt_number = c3.receipt_number
+  left outer join egpt_mv_c5_rcpt_arreartax_coll c5 on c9.receipt_number = c5.receipt_number
+  left outer join egpt_mv_c6_rcpt_arrearpenalty_coll c6 on c9.receipt_number = c6.receipt_number
+  left outer join egpt_mv_c7_rcpt_arrearlibrarycess_coll c7 on c9.receipt_number = c7.receipt_number,
+  egpt_mv_c2_rcpt_tax_coll c2,
+  egpt_mv_c4_rcpt_librarycess_coll c4
+where
+  c9.receipt_number = c2.receipt_number
+  or c9.receipt_number = c4.receipt_number;
+  
+ -------------------END-------------------
