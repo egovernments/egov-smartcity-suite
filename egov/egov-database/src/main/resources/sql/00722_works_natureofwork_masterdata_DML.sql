6229a1288a035e1b2f7b23bad9fea3df7f18be2a
sathish.p <sathish.p@egovernments.org>
2015-09-25 11:43:30 +0530

    Sathish P - PHOENIX-1014 - Migrating Estimate screen to phoenix - nature of work master data

---
+++ egov/egov-database/src/main/resources/sql/00722_works_natureofwork_masterdata_DML.sql
@@ -0,0 +1,18 @@
+
+Insert into EGW_NATUREOFWORK (ID,NAME,EXPENDITURE_TYPE,CODE) values (NEXTVAL('SEQ_EGW_NATUREOFWORK'),'Capital Works - New Asset','CAPITAL','Capital Works');
+Insert into EGW_NATUREOFWORK (ID,NAME,EXPENDITURE_TYPE,CODE) values (NEXTVAL('SEQ_EGW_NATUREOFWORK'),'Capital Works - Improvement Works','CAPITAL','Improvement Works');
+Insert into EGW_NATUREOFWORK (ID,NAME,EXPENDITURE_TYPE,CODE) values (NEXTVAL('SEQ_EGW_NATUREOFWORK'),'Capital Works - Additional Works','CAPITAL','Improvement Works');
+Insert into EGW_NATUREOFWORK (ID,NAME,EXPENDITURE_TYPE,CODE) values (NEXTVAL('SEQ_EGW_NATUREOFWORK'),'Capital Works - Special Works','CAPITAL','Improvement Works');
+Insert into EGW_NATUREOFWORK (ID,NAME,EXPENDITURE_TYPE,CODE) values (NEXTVAL('SEQ_EGW_NATUREOFWORK'),'Deposit Works - Own Asset','CAPITAL','Deposit Works Own Asset');
+Insert into EGW_NATUREOFWORK (ID,NAME,EXPENDITURE_TYPE,CODE) values (NEXTVAL('SEQ_EGW_NATUREOFWORK'),'Deposit Works - Third Party Asset','OTHERS','Deposit Works Third Party Asset');
+Insert into EGW_NATUREOFWORK (ID,NAME,EXPENDITURE_TYPE,CODE) values (NEXTVAL('SEQ_EGW_NATUREOFWORK'),'Deposit Works - No Asset Created','OTHERS','Deposit Works No Asset Created');
+Insert into EGW_NATUREOFWORK (ID,NAME,EXPENDITURE_TYPE,CODE) values (NEXTVAL('SEQ_EGW_NATUREOFWORK'),'Deposit Works - Road Cut Restoration','OTHERS','Deposit Works No Asset Created');
+Insert into EGW_NATUREOFWORK (ID,NAME,EXPENDITURE_TYPE,CODE) values (NEXTVAL('SEQ_EGW_NATUREOFWORK'),'Repairs and maintenance Works','REVENUE','Repairs and maintenance');
+Insert into EGW_NATUREOFWORK (ID,NAME,EXPENDITURE_TYPE,CODE) values (NEXTVAL('SEQ_EGW_NATUREOFWORK'),'MLACDS - New Asset','OTHERES','MLACDS  New Asset');
+Insert into EGW_NATUREOFWORK (ID,NAME,EXPENDITURE_TYPE,CODE) values (NEXTVAL('SEQ_EGW_NATUREOFWORK'),'MLACDS - Additional Works','OTHERES','MLACDS  Additional Works');
+Insert into EGW_NATUREOFWORK (ID,NAME,EXPENDITURE_TYPE,CODE) values (NEXTVAL('SEQ_EGW_NATUREOFWORK'),'MLACDS - Improvement Works','OTHERES','MLACDS  Improvement Works');
+Insert into EGW_NATUREOFWORK (ID,NAME,EXPENDITURE_TYPE,CODE) values (NEXTVAL('SEQ_EGW_NATUREOFWORK'),'MPLADS - New Asset','OTHERES','MPLADS  New Asset');
+Insert into EGW_NATUREOFWORK (ID,NAME,EXPENDITURE_TYPE,CODE) values (NEXTVAL('SEQ_EGW_NATUREOFWORK'),'MPLADS - Additional Works','OTHERES','MPLADS  Additional Works');
+Insert into EGW_NATUREOFWORK (ID,NAME,EXPENDITURE_TYPE,CODE) values (NEXTVAL('SEQ_EGW_NATUREOFWORK'),'MPLADS - Improvement Works','OTHERES','MPLADS  Improvement Works');
+
+--rollback delete from EGW_NATUREOFWORK;
