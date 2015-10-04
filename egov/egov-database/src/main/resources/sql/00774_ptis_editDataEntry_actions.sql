cac945f1b034e850245ca8359454e2f088f0da8e
ramki <ramakrishna@egovernments.org>
2015-10-04 17:05:07 +0530

    [PHOENIX-860] - correction in action insert

--- egov/egov-database/src/main/resources/sql/00774_ptis_editDataEntry_actions.sql
+++ egov/egov-database/src/main/resources/sql/00774_ptis_editDataEntry_actions.sql
@@ -1,8 +1,8 @@
-Insert into eg_action (ID,NAME,url,queryparams,parentmodule,ordernumber,displayname,enabled,contextroot,version,createdby,createddate, lastmodifiedby, lastmodifieddate) 
-values (nextval('seq_eg_action'),'PTIS-Edit Data Entry Screen','/modify/modifyProperty-modifyDataEntry.action', null, (select ID from eg_module where NAME ='Existing property'), null, 'Edit Data Entry Screen', false, 'ptis', null, 1, current_timestamp,1,current_timestamp);
+Insert into eg_action (ID,NAME,url,queryparams,parentmodule,ordernumber,displayname,enabled,contextroot,version,createdby,createddate, lastmodifiedby, lastmodifieddate, application) 
+values (nextval('seq_eg_action'),'PTIS-Edit Data Entry Screen','/modify/modifyProperty-modifyDataEntry.action', null, (select ID from eg_module where NAME ='Existing property'), null, 'Edit Data Entry Screen', false, 'ptis', null, 1, current_timestamp,1,current_timestamp, (select ID from eg_module where NAME ='Existing property'));
 
-Insert into eg_action (ID,NAME,url,queryparams,parentmodule,ordernumber,displayname,enabled,contextroot,version,createdby,createddate, lastmodifiedby, lastmodifieddate) 
-values (nextval('seq_eg_action'),'PTIS-Save Edit Data Entry Screen','/modify/modifyProperty-saveDataEntry.action', null, (select ID from eg_module where NAME ='Existing property'), null, 'Save Edit Data Entry Screen', false, 'ptis', null, 1, current_timestamp,1,current_timestamp);
+Insert into eg_action (ID,NAME,url,queryparams,parentmodule,ordernumber,displayname,enabled,contextroot,version,createdby,createddate, lastmodifiedby, lastmodifieddate, application) 
+values (nextval('seq_eg_action'),'PTIS-Save Edit Data Entry Screen','/modify/modifyProperty-saveDataEntry.action', null, (select ID from eg_module where NAME ='Existing property'), null, 'Save Edit Data Entry Screen', false, 'ptis', null, 1, current_timestamp,1,current_timestamp, (select ID from eg_module where NAME ='Existing property'));
 
 INSERT INTO EG_ROLEACTION (ROLEID, ACTIONID) values ((select id from eg_role where name LIKE 'CSC Operator') ,(select id FROM eg_action  WHERE name = 'PTIS-Edit Data Entry Screen' and contextroot='ptis'));
 INSERT INTO EG_ROLEACTION (ROLEID, ACTIONID) values ((select id from eg_role where name LIKE 'CSC Operator') ,(select id FROM eg_action  WHERE name = 'PTIS-Save Edit Data Entry Screen' and contextroot='ptis'));
