12d4ec5308d852a8ee5399a2f2fa5c77b355fd20
jayashree.reddy@egovernments.org <jayashree.reddy@egovernments.org>
2015-10-06 16:41:06 +0530

    PHOENIX-1055 Module displayname changes.

---
+++ egov/egov-database/src/main/resources/sql/00788_pgr_module_displayname_changes_DML.sql
@@ -0,0 +1,2 @@
+update eg_module set displayname ='Grievance Redressal' where name='PGR' and parentmodule is null;
+update eg_module set displayname ='Employee management' where name='EIS' and parentmodule is null;
