35a89721d1962530d062d81675a3964b09482d2b
pradeep <pradeep@egovernments.org>
2015-10-07 00:14:30 +0530

    PHOENIX-1072 Search screen incremental update.

--- egov/egov-adtaxweb/src/main/java/org/egov/adtax/web/controller/hoarding/SearchHoardingController.java
+++ egov/egov-adtaxweb/src/main/java/org/egov/adtax/web/controller/hoarding/SearchHoardingController.java
@@ -120,12 +120,12 @@ public class SearchHoardingController extends GenericController {
     } 
 
     @RequestMapping(value = "search-for-update", method = GET)
-    public String searchHoardingForm(@ModelAttribute final HoardingSearch hoardingSearch) {
+    public String searchHoardingForm(@ModelAttribute  HoardingSearch hoardingSearch) {
         return "hoarding-search-for-update";
     }
 
     @RequestMapping(value = "search-for-update", method = POST, produces = MediaType.TEXT_PLAIN_VALUE)
-    public @ResponseBody String searchHoarding(@ModelAttribute final HoardingSearch hoardingSearch) {
+    public @ResponseBody String searchHoarding(@ModelAttribute  HoardingSearch hoardingSearch) {
         return "{ \"data\":" + new GsonBuilder().setDateFormat(applicationProperties.defaultDatePattern()).create()
                 .toJson(hoardingService.getHoardingSearchResult(hoardingSearch)) + "}";
     }
--- egov/egov-adtaxweb/src/main/webapp/resources/app/js/searchadtax.js
+++ egov/egov-adtaxweb/src/main/webapp/resources/app/js/searchadtax.js
@@ -143,10 +143,9 @@ $(document).ready(function(){
 	var datatbl = $('#search-update-result-table');
 	$('#search-update').click(function(e){
 		datatbl.dataTable({
-			"ajax": {
-	            "url": "/adtax/hoarding/search-for-update",
-	            "type": "POST"
-	        },
+			"ajax": {url:"/adtax/hoarding/search-for-update?"+$("#hoardingsearchform").serialize(),
+				type:"POST"
+			},
 			"sPaginationType": "bootstrap",
 			"sDom": "<'row'<'col-xs-12 hidden col-right'f>r>t<'row'<'col-md-5 col-xs-12'i><'col-md-3 col-xs-6'l><'col-md-4 col-xs-6 text-right'p>>",
 			"aLengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
