1b5c40e9c35d3362e0b866efce9156d1d6ba8f40
Iffath <iffath@egovernments.org>
2015-10-15 13:28:36 +0530

    PHOENIX-1212 After selecting the Collections Receipt in Mahesh Inbox oops error fixed

---
+++ egov/egov-collection/src/main/resources/db/migration/sample/V20151015121741__collection_user_counter_map_sample.sql
@@ -0,0 +1,1 @@
+Insert into eg_usercounter_map  values(nextval('seq_eg_usercounter_map'), (select id from eg_user where username ='mahesh'),(select id from eg_location where name='Zone-1'), to_date('01-04-2012','DD-MM-YYYY'), to_date('31-03-2099','DD-MM-YYYY'),1, current_timestamp);
