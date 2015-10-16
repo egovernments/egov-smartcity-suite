8dc1e65989ce381f294e3e2551536a16e12f74f6
sushant <sushant_vs@cms.co.in>
2015-10-16 15:42:16 +0530

    PHOENIX-1030 : Chair Person Master Screen having issues.

--- egov/egov-wtmsweb/src/main/webapp/resources/js/app/chairperson-details.js
+++ egov/egov-wtmsweb/src/main/webapp/resources/js/app/chairperson-details.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
 				},
 				{
 					"mData" : "chairPerson",
-					"sTitle" : "Chair Person",
+					"sTitle" : "Chair Person Name",
 				}, 
 				 {
 					"mData" : "fromDate",
