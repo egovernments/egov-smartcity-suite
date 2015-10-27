16662ba976468851e638c18d95752cc3b28ccdac
sushant <sushant_vs@cms.co.in>
2015-10-27 18:43:42 +0530

    PHOENIX-1265 :Rename Tap execution date button to Execute tap

--- egov/egov-wtms/src/main/resources/db/migration/main/V20151027161418__wtms_tapexecutiondate_change_data.sql
+++ egov/egov-wtms/src/main/resources/db/migration/main/V20151027161418__wtms_tapexecutiondate_change_data.sql
@@ -1,1 +1,1 @@
-update eg_wf_matrix set validactions = 'Execute Tap' where validactions = 'Tap Execution Date' and currentstate ='Work order generated' 
+update eg_wf_matrix set validactions = 'Execute Tap' where validactions = 'Tap Execution Date' and currentstate ='Work order generated'; 
