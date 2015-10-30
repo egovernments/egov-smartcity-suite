5722581458f1a40d32154e3c855572356b828c84
pradeep <pradeep@egovernments.org>
2015-10-30 13:36:34 +0530

    PHOENIX-1275 Inclusion of Mandatory Documents

---
+++ egov/egov-adtax/src/main/resources/db/migration/sample/V20151030131035__adtax_MandatoryDocuments.sql
@@ -0,0 +1,6 @@
+
+------------------START------------------
+INSERT INTO egadtax_hoardingdocument_type (id, name, mandatory, version) VALUES (nextval('SEQ_egadtax_document_TYPE'), 'NOC from the owner', true ,0);
+INSERT INTO egadtax_hoardingdocument_type (id, name, mandatory, version) VALUES (nextval('SEQ_egadtax_document_TYPE'), 'Structural certificate from the authorized Structural Engineer', true, 0);
+
+-----------------END------------------
