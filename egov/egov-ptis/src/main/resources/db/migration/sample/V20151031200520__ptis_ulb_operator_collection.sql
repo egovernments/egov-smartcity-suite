9497e2d7ebb2fdcb8c3c793fdc5ee4ddbc5cfe4d
subhash <subhash.bose@egovernments.org>
2015-10-31 20:21:06 +0530

    PHOENIX-1363 : ULB Operator mapping to perform collection

---
+++ egov/egov-ptis/src/main/resources/db/migration/sample/V20151031200520__ptis_ulb_operator_collection.sql
@@ -0,0 +1,12 @@
+﻿Insert into eg_usercounter_map  values(nextval('seq_eg_usercounter_map'), (select id from eg_user where username ='syed'),(select id from eg_location where name='Zone-1'), 
+to_date('01-04-2012','DD-MM-YYYY'), to_date('31-03-2099','DD-MM-YYYY'),1, current_timestamp);
+
+Insert into eg_usercounter_map  values(nextval('seq_eg_usercounter_map'), (select id from eg_user where username ='suresh'),(select id from eg_location where name='Zone-1'), 
+to_date('01-04-2012','DD-MM-YYYY'), to_date('31-03-2099','DD-MM-YYYY'),1, current_timestamp);
+
+
+insert into eg_roleaction (actionid, roleid) values ((select id from eg_action where name = 'Property Tax Collection'),(select id from eg_role where name = 'ULB Operator'));
+
+insert into eg_roleaction (actionid, roleid) values ((select id from eg_action where name = 'Mutation Fee Payment'),(select id from eg_role where name = 'ULB Operator'));
+insert into eg_roleaction (actionid, roleid) values ((select id from eg_action where name = 'Mutation Fee Payment Search'),(select id from eg_role where name = 'ULB Operator'));
+insert into eg_roleaction (actionid, roleid) values ((select id from eg_action where name = 'Mutation Fee Payment Generate'),(select id from eg_role where name = 'ULB Operator'));
