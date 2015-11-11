7b5678ce5e3086a223d3f42fd73f4d8c3d2f5554
roopa <roopa.h@egovernments.org>
2015-11-11 12:12:14 +0530

    PHOENIX-1208 Number of Connections - Report

---
+++ egov/egov-wtms/src/main/java/org/egov/wtms/masters/service/BoundaryWiseReportService.java
@@ -0,0 +1,93 @@
+/**
+ * eGov suite of products aim to improve the internal efficiency,transparency,
+   accountability and the service delivery of the government  organizations.
+
+    Copyright (C) <2015>  eGovernments Foundation
+
+    The updated version of eGov suite of products as by eGovernments Foundation
+    is available at http://www.egovernments.org
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see http://www.gnu.org/licenses/ or
+    http://www.gnu.org/licenses/gpl.html .
+
+    In addition to the terms of the GPL license to be adhered to in using this
+    program, the following additional terms are to be complied with:
+
+        1) All versions of this program, verbatim or modified must carry this
+           Legal Notice.
+
+        2) Any misrepresentation of the origin of the material is prohibited. It
+           is required that all modified versions of this material be marked in
+           reasonable ways as different from the original version.
+
+        3) This license does not grant any rights to any user of the program
+           with regards to rights under trademark law for use of the trade names
+           or trademarks of eGovernments Foundation.
+
+  In case of any queries, you can reach eGovernments Foundation at contact@egovernments.org.
+ */
+package org.egov.wtms.masters.service;
+
+import org.apache.commons.lang.StringUtils;
+import org.egov.infstr.utils.HibernateUtil;
+import org.hibernate.SQLQuery;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class BoundaryWiseReportService {
+
+    public SQLQuery getDrillDownReportQuery(final String ward, final String block) {
+
+        final StringBuffer query = new StringBuffer();
+        query.append("SELECT bndry.name as name, ");
+
+        query.append("   COUNT(CASE WHEN cs.code IN ('NEWCONNECTION') THEN 1 END) newconnection , "
+                + " COUNT(CASE WHEN cs.code IN ('ADDNLCONNECTION') THEN 1 END) addconnection, "
+                + " COUNT(CASE WHEN cs.code IN ('CHANGEOFUSE') THEN 1 END) changeofusage, "
+                + " COUNT(CASE WHEN cs.code IN ('CLOSINGCONNECTION') THEN 1 END) closeconnection, "
+                + " COUNT(CASE WHEN cs.code IN ('RECONNECTION') THEN 1 END) reconnection ");
+
+        query.append("FROM egwtr_connection con  INNER JOIN egwtr_connectiondetails cd ON con.id = cd.connection INNER JOIN egwtr_application_type cs ON cd.applicationtype=cs.id INNER JOIN egpt_basic_property bp ON con.propertyidentifier = bp.propertyid INNER JOIN EGPT_PROPERTYID ptid ON bp.ID_PROPERTYID= ptid.id INNER JOIN eg_boundary bndry ON bndry.id=ptid.WARD_ADM_ID");
+        buildWhereClause(ward, block, query);
+        buildGroupByClause(ward, block, query);
+        return setParameterForDrillDownReportQuery(query.toString(), ward, block);
+    }
+
+    private void buildGroupByClause(final String ward, final String block, final StringBuffer query) {
+        query.append("  group by bndry.name ");
+
+    }
+
+    private StringBuffer buildWhereClause(final String ward, final String block, final StringBuffer queryStr) {
+        if (StringUtils.isNotBlank(ward))
+            queryStr.append(" WHERE ptid.WARD_ADM_ID=:ward ");
+        if (StringUtils.isNotBlank(block))
+            queryStr.append(" and ptid.ADM1=:block ");
+        return queryStr;
+
+    }
+
+    private SQLQuery setParameterForDrillDownReportQuery(final String querykey, final String ward, final String block) {
+        final SQLQuery qry = HibernateUtil.getCurrentSession().createSQLQuery(querykey);
+        if (StringUtils.isNotBlank(ward))
+            qry.setLong("ward", Long.valueOf(ward));
+        if (StringUtils.isNotBlank(block))
+            qry.setLong("block", Long.valueOf(block));
+        return qry;
+
+    }
+
+}
---
+++ egov/egov-wtmsweb/src/main/java/org/egov/wtms/web/controller/reports/NumberOfConnectionReportController.java
@@ -0,0 +1,141 @@
+/**
+ * eGov suite of products aim to improve the internal efficiency,transparency,
+   accountability and the service delivery of the government  organizations.
+
+    Copyright (C) <2015>  eGovernments Foundation
+
+    The updated version of eGov suite of products as by eGovernments Foundation
+    is available at http://www.egovernments.org
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see http://www.gnu.org/licenses/ or
+    http://www.gnu.org/licenses/gpl.html .
+
+    In addition to the terms of the GPL license to be adhered to in using this
+    program, the following additional terms are to be complied with:
+
+        1) All versions of this program, verbatim or modified must carry this
+           Legal Notice.
+
+        2) Any misrepresentation of the origin of the material is prohibited. It
+           is required that all modified versions of this material be marked in
+           reasonable ways as different from the original version.
+
+        3) This license does not grant any rights to any user of the program
+           with regards to rights under trademark law for use of the trade names
+           or trademarks of eGovernments Foundation.
+
+  In case of any queries, you can reach eGovernments Foundation at contact@egovernments.org.
+ */
+package org.egov.wtms.web.controller.reports;
+
+import static org.egov.ptis.constants.PropertyTaxConstants.LOCALITY;
+import static org.egov.ptis.constants.PropertyTaxConstants.LOCATION_HIERARCHY_TYPE;
+import static org.egov.ptis.constants.PropertyTaxConstants.WARD;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.egov.infra.admin.master.entity.Boundary;
+import org.egov.infra.admin.master.service.BoundaryService;
+import org.egov.ptis.constants.PropertyTaxConstants;
+import org.egov.wtms.masters.service.BoundaryWiseReportService;
+import org.hibernate.SQLQuery;
+import org.hibernate.transform.Transformers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+@Controller
+@RequestMapping(value = "/reports")
+public class NumberOfConnectionReportController {
+
+    @Autowired
+    private final BoundaryWiseReportService boundaryWiseReportService;
+
+    @Autowired
+    public NumberOfConnectionReportController(final BoundaryWiseReportService drillDownReportService) {
+        boundaryWiseReportService = drillDownReportService;
+    }
+
+    @Autowired
+    private BoundaryService boundaryService;
+
+    @ModelAttribute("wards")
+    public List<Boundary> wards() {
+        return boundaryService.getActiveBoundariesByBndryTypeNameAndHierarchyTypeName(WARD,
+                PropertyTaxConstants.REVENUE_HIERARCHY_TYPE);
+    }
+
+    @ModelAttribute
+    public void getReportHelper(final Model model) {
+        final WaterConnectionReportResult reportHealperObj = new WaterConnectionReportResult();
+        model.addAttribute("reportHelper", reportHealperObj);
+
+    }
+
+    @ModelAttribute("localitys")
+    public List<Boundary> localitys() {
+        return boundaryService
+                .getActiveBoundariesByBndryTypeNameAndHierarchyTypeName(LOCALITY, LOCATION_HIERARCHY_TYPE);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/coonectionReport/wardWise")
+    public String searchNoOfConnectionByBoundaryForm(final Model model) {
+        model.addAttribute("currDate", new Date());
+        return "connectionReport-search";
+    }
+
+    @ExceptionHandler(Exception.class)
+    @RequestMapping(value = "/connectionReportList", method = RequestMethod.GET)
+    public @ResponseBody void springPaginationDataTablesUpdate(@RequestParam final String ward,
+            @RequestParam final String block, final HttpServletRequest request, final HttpServletResponse response)
+            throws IOException {
+
+        SQLQuery drillDownreportQuery = null;
+        String result = null;
+        drillDownreportQuery = boundaryWiseReportService.getDrillDownReportQuery(ward, block);
+        drillDownreportQuery.setResultTransformer(Transformers.aliasToBean(WaterConnectionReportResult.class));
+        final List<WaterConnectionReportResult> drillDownresult = drillDownreportQuery.list();
+        result = new StringBuilder("{ \"data\":").append(toJSON(drillDownresult)).append("}").toString();
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        IOUtils.write(result, response.getWriter());
+
+    }
+
+    private Object toJSON(final Object object) {
+
+        final GsonBuilder gsonBuilder = new GsonBuilder();
+        final Gson gson = gsonBuilder.registerTypeAdapter(WaterConnectionReportResult.class,
+                new WaterConnectionHelperAdaptor()).create();
+        final String json = gson.toJson(object);
+        return json;
+
+    }
+
+}
