c1012c610fa13730a3bf8c8e9abd1835e2445c71
roopa <roopa.h@egovernments.org>
2015-11-12 10:58:34 +0530

    PHOENIX-1502 removed alert from js

--- egov/egov-wtmsweb/src/main/webapp/resources/js/app/applicationsearch.js
+++ egov/egov-wtmsweb/src/main/webapp/resources/js/app/applicationsearch.js
@@ -139,7 +139,6 @@ jQuery(document).ready(function ($) {
 		tableContainer.$('tr.row_selected').removeClass('row_selected');
         $(this).addClass('row_selected');
         var url= tableContainer.fnGetData( this,1);
-        alert("url ="+ url);
         $('#applicationSearchRequestForm').attr('method', 'get');
 		$('#applicationSearchRequestForm').attr('action', url);
 		$('#applicationSearchRequestForm').attr('mode', 'search');
