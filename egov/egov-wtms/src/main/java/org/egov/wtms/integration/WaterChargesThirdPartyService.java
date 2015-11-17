eef0a1ff153e0c3627c41d56776ccec6039596a9
sushant <sushant_vs@cms.co.in>
2015-11-17 18:12:34 +0530

    (PHOENIX-1039) : API's for MeSeva

--- egov/egov-wtms/src/main/java/org/egov/wtms/integration/WaterChargesThirdPartyService.java
+++ egov/egov-wtms/src/main/java/org/egov/wtms/integration/WaterChargesThirdPartyService.java
@@ -17,8 +17,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@Transactional(readOnly = true)
+
 public class WaterChargesThirdPartyService {
 
     public WaterConnectionDetails waterConnectionDetails;
