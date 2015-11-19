0a326bddbe321d9cfcab3eec69b6e7cc8f4de36e
manikanta-pt <manikanta@egovernments.org>
2015-11-19 18:20:33 +0530

    Deleting as qa build is faling

--- egov/egov-collection/src/main/resources/db/migration/public/V20151118201641__collection_aggregate_view_service.sql
+++
@@ -1,7 +0,0 @@
-drop view public.receipt_aggr_view ;
-create or replace view public.receipt_aggr_view
-as 
-select date(receiptdate) as receipt_date,service.code as service,  text 'public' as ulb, sum(Totalamount) as total,count(*) as recordCount 
-from	public.egcl_collectionheader header,public.egcl_servicedetails service	where  header.servicedetails=service.id and status in
-(select id from	public.egw_status where moduletype='ReceiptHeader' and code in ('TO_BE_SUBMITTED','SUBMITTED','APPROVED','REMITTED'))
-group by receipt_date ,service
