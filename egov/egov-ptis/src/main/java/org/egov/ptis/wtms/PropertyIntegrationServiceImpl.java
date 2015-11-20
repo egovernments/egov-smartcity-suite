5e524e9ef812c8865cf61dce6e371d932295eff1
pradeep <pradeep@egovernments.org>
2015-11-20 17:15:42 +0530

    PHOENIX-1612 Unresolvable circular reference error issue fixed.

--- egov/egov-ptis/src/main/java/org/egov/ptis/wtms/PropertyIntegrationServiceImpl.java
+++ egov/egov-ptis/src/main/java/org/egov/ptis/wtms/PropertyIntegrationServiceImpl.java
@@ -42,16 +42,18 @@ package org.egov.ptis.wtms;
 import org.egov.ptis.domain.model.AssessmentDetails;
 import org.egov.ptis.domain.service.property.PropertyExternalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 @Service("propertyIntegrationServiceImpl")
 public class PropertyIntegrationServiceImpl implements PropertyIntegrationService {
 
     @Autowired
-    private PropertyExternalService propertyExternalService;
+    private ApplicationContext applicationContext;
 
     @Override
     public AssessmentDetails getAssessmentDetailsForFlag(final String asessmentNumber, final Integer flagDetail) {
+        PropertyExternalService propertyExternalService=applicationContext.getBean("propertyExternalService",PropertyExternalService.class);
         final AssessmentDetails assessmentDetails = propertyExternalService.loadAssessmentDetails(asessmentNumber,
                 flagDetail);
         return assessmentDetails;
