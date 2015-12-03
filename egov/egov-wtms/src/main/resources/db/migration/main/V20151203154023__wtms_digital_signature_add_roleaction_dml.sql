2e8ace4ca1deee9ff966f510bd87ace998e05166
ranjit <ranjit.singh@mindteck.com>
2015-12-03 15:54:09 +0530

    PHOENIX-1811 | Ranjit - Added role for roleaction map for preview ..

---
+++ egov/egov-wtms/src/main/resources/db/migration/main/V20151203154023__wtms_digital_signature_add_roleaction_dml.sql
@@ -0,0 +1,2 @@
+insert into eg_roleaction (roleid,actionid )values((select id from eg_role where name='Property Administrator'),
+   (select id from eg_action where url='/application/workorder' and contextroot='wtms'));
