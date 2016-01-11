fdba25140b3eb3436c26640c9dcfa413ea535947
Elzan Mathew <elzan.mathew@egovernments.org>
2016-01-11 17:49:36 +0530

    Phoenix-936 Added supplier accountingtype

---
+++ egov/egov-commons/src/main/resources/db/migration/main/V20160111174450__commons_supplier_type.sql
@@ -0,0 +1,2 @@
+Insert into ACCOUNTDETAILTYPE (ID,NAME,DESCRIPTION,TABLENAME,COLUMNNAME,ATTRIBUTENAME,NBROFLEVELS,ISACTIVE,CREATED,LASTMODIFIED,MODIFIEDBY ,FULL_QUALIFIED_NAME)
+ values (nextval('seq_accountdetailtype'),'Supplier','Supplier','relation','id','relation_id',1,1,current_date,current_date,null,'org.egov.commons.Relation');
