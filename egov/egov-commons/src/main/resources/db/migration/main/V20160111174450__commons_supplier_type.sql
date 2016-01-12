0e205e6192fc4825abdb6eeb0ac58b23e801f377
Elzan Mathew <elzan.mathew@egovernments.org>
2016-01-12 15:45:47 +0530

    Fixing build

--- egov/egov-commons/src/main/resources/db/migration/main/V20160111174450__commons_supplier_type.sql
+++
@@ -1,5 +0,0 @@
-SELECT setval('"seq_accountdetailtype"',
-(SELECT MAX(ID) FROM accountdetailtype ));
-
-Insert into ACCOUNTDETAILTYPE (ID,NAME,DESCRIPTION,TABLENAME,COLUMNNAME,ATTRIBUTENAME,NBROFLEVELS,ISACTIVE,CREATED,LASTMODIFIED,MODIFIEDBY ,FULL_QUALIFIED_NAME)
- values (nextval('seq_accountdetailtype'),'Supplier','Supplier','relation','id','relation_id',1,1,current_date,current_date,null,'org.egov.commons.Relation');
