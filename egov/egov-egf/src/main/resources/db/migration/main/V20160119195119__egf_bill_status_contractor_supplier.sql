c0ef2f01728d7ed8a6729c2479e88de76939eaa2
Manikanta <manikanta@egovernments.org>
2016-01-19 20:06:18 +0530

    PHOENIX-2152 Bill Payment screen changes

---
+++ egov/egov-egf/src/main/resources/db/migration/main/V20160119195119__egf_bill_status_contractor_supplier.sql
@@ -0,0 +1,19 @@
+Insert into EGW_STATUS (ID,MODULETYPE,DESCRIPTION,LASTMODIFIEDDATE,CODE,ORDER_ID) values (nextval('seq_egw_status'),'CONTRACTORBILL','Created',to_date('14-07-10','DD-MM-RR'),'CREATED',1);
+Insert into EGW_STATUS (ID,MODULETYPE,DESCRIPTION,LASTMODIFIEDDATE,CODE,ORDER_ID) values (nextval('seq_egw_status'),'CONTRACTORBILL','Checked',to_date('14-07-10','DD-MM-RR'),'CHECKED',2);
+Insert into EGW_STATUS (ID,MODULETYPE,DESCRIPTION,LASTMODIFIEDDATE,CODE,ORDER_ID) values (nextval('seq_egw_status'),'CONTRACTORBILL','Approved',to_date('14-07-10','DD-MM-RR'),'APPROVED',3);
+Insert into EGW_STATUS (ID,MODULETYPE,DESCRIPTION,LASTMODIFIEDDATE,CODE,ORDER_ID) values (nextval('seq_egw_status'),'CONTRACTORBILL','Rejected',to_date('14-07-10','DD-MM-RR'),'REJECTED',4);
+Insert into EGW_STATUS (ID,MODULETYPE,DESCRIPTION,LASTMODIFIEDDATE,CODE,ORDER_ID) values (nextval('seq_egw_status'),'CONTRACTORBILL','Re-Submitted',to_date('14-07-10','DD-MM-RR'),'RESUBMITTED',5);
+Insert into EGW_STATUS (ID,MODULETYPE,DESCRIPTION,LASTMODIFIEDDATE,CODE,ORDER_ID) values (nextval('seq_egw_status'),'CONTRACTORBILL','Cancelled',to_date('14-07-10','DD-MM-RR'),'CANCELLED',6);
+Insert into EGW_STATUS (ID,MODULETYPE,DESCRIPTION,LASTMODIFIEDDATE,CODE,ORDER_ID) values (nextval('seq_egw_status'),'CONTRACTORBILL','New',to_date('14-07-10','DD-MM-RR'),'NEW',1);
+
+
+
+
+Insert into EGW_STATUS (ID,MODULETYPE,DESCRIPTION,LASTMODIFIEDDATE,CODE,ORDER_ID) values (nextval('seq_egw_status'),'SBILL','Reversed',to_date('15-05-09','DD-MM-RR'),'Reversed',null);
+Insert into EGW_STATUS (ID,MODULETYPE,DESCRIPTION,LASTMODIFIEDDATE,CODE,ORDER_ID) values (nextval('seq_egw_status'),'SBILL','Cancelled',to_date('15-05-09','DD-MM-RR'),'Cancelled',null);
+Insert into EGW_STATUS (ID,MODULETYPE,DESCRIPTION,LASTMODIFIEDDATE,CODE,ORDER_ID) values (nextval('seq_egw_status'),'SBILL','Confirmed',to_date('15-05-09','DD-MM-RR'),'Confirmed',null);
+Insert into EGW_STATUS (ID,MODULETYPE,DESCRIPTION,LASTMODIFIEDDATE,CODE,ORDER_ID) values (nextval('seq_egw_status'),'SBILL','Payment Made',to_date('15-05-09','DD-MM-RR'),'Payment Made',null);
+Insert into EGW_STATUS (ID,MODULETYPE,DESCRIPTION,LASTMODIFIEDDATE,CODE,ORDER_ID) values (nextval('seq_egw_status'),'SBILL','Created',to_date('15-05-09','DD-MM-RR'),'Created',null);
+Insert into EGW_STATUS (ID,MODULETYPE,DESCRIPTION,LASTMODIFIEDDATE,CODE,ORDER_ID) values (nextval('seq_egw_status'),'SBILL','Approved',to_date('15-05-09','DD-MM-RR'),'Approved',null);
+Insert into EGW_STATUS (ID,MODULETYPE,DESCRIPTION,LASTMODIFIEDDATE,CODE,ORDER_ID) values (nextval('seq_egw_status'),'SBILL','Submitted',to_date('15-05-09','DD-MM-RR'),'Submitted',null);
+Insert into EGW_STATUS (ID,MODULETYPE,DESCRIPTION,LASTMODIFIEDDATE,CODE,ORDER_ID) values (nextval('seq_egw_status'),'SBILL','Rejected',to_date('15-05-09','DD-MM-RR'),'Rejected',null);
