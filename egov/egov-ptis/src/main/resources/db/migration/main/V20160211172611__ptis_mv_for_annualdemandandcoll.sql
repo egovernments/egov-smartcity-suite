807c51b4e44948eccaa42b824e5b4dc1174d84fd
parvati <parvati.hiremath@egovernments.org>
2016-02-11 17:42:43 +0530

    PHOENIX-2611 fix for script fail

--- egov/egov-ptis/src/main/resources/db/migration/main/V20160211172611__ptis_mv_for_annualdemandandcoll.sql
+++ egov/egov-ptis/src/main/resources/db/migration/main/V20160211172611__ptis_mv_for_annualdemandandcoll.sql
@@ -1,3 +1,6 @@
+drop table if exists egpt_mv_annual_demand_coll;
+drop table if exists egpt_mv_esuvidha_annual_coll;
+
 CREATE TABLE egpt_mv_annual_demand_coll(
 id_basic_property bigint NOT NULL,
 annualTax double precision,
@@ -9,5 +12,8 @@ id_basic_property bigint NOT NULL,
 coll double precision
 );
 
+ALTER TABLE egpt_mv_propertyinfo DROP COLUMN if exists annualdemand;
 ALTER TABLE egpt_mv_propertyinfo ADD COLUMN annualdemand double precision;
+
+ALTER TABLE egpt_mv_propertyinfo DROP COLUMN if exists annualcoll;
 ALTER TABLE egpt_mv_propertyinfo ADD COLUMN annualcoll double precision;
