cc1a004275fe4e2aeff511cda6fba10ba7c98d88
venki <venki@egovernments.org>
2016-02-23 19:32:37 +0530

    [PHOENIX-2638] - Cheque assignment screen changes.

---
+++ egov/egov-egf/src/main/resources/db/migration/main/V20160223193041__egf_chequeAssignment_Actions_roleActions.sql
@@ -0,0 +1,6 @@
+Insert into eg_action (id,name,url,queryparams,parentmodule,ordernumber,displayname,enabled,contextroot,version,createdby,createddate,lastmodifiedby,lastmodifieddate,application) values (nextval('SEQ_EG_ACTION'),'ChequeAssignmentSearch','/payment/chequeAssignment-search.action',null,(select id from eg_module where name='Payments'),1,'ChequeAssignmentSearch',false,'EGF',0,1,current_date,1,current_date,(select id from eg_module where name = 'EGF' and parentmodule is null));
+Insert into eg_roleaction values((select id from eg_role where name='Super User'),(select id from eg_action where name='ChequeAssignmentSearch'));
+
+Insert into eg_action (id,name,url,queryparams,parentmodule,ordernumber,displayname,enabled,contextroot,version,createdby,createddate,lastmodifiedby,lastmodifieddate,application) values (nextval('SEQ_EG_ACTION'),'ChequeAssignmentCreate','/payment/chequeAssignment-create.action',null,(select id from eg_module where name='Payments'),1,'ChequeAssignmentCreate',false,'EGF',0,1,current_date,1,current_date,(select id from eg_module where name = 'EGF' and parentmodule is null));
+Insert into eg_roleaction values((select id from eg_role where name='Super User'),(select id from eg_action where name='ChequeAssignmentCreate'));
+
