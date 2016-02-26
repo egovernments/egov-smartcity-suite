45eb7fe7cfcbd348f860e931cf1513c1802683d2
sushant <sushant_vs@cms.co.in>
2016-02-26 20:17:36 +0530

    PHOENIX-2689: Defaulters report Water charges

---
+++ egov/egov-wtms/src/main/java/org/egov/wtms/application/service/DefaultersWTReportService.java
@@ -0,0 +1,106 @@
+
+/**
+ * eGov suite of products aim to improve the internal efficiency,transparency, 
+   accountability and the service delivery of the government  organizations.
+
+    Copyright (C) <2015>  eGovernments Foundation
+
+    The updated version of eGov suite of products as by eGovernments Foundation 
+    is available at http://www.egovernments.org
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see http://www.gnu.org/licenses/ or 
+    http://www.gnu.org/licenses/gpl.html .
+
+    In addition to the terms of the GPL license to be adhered to in using this
+    program, the following additional terms are to be complied with:
+
+	1) All versions of this program, verbatim or modified must carry this 
+	   Legal Notice.
+
+	2) Any misrepresentation of the origin of the material is prohibited. It 
+	   is required that all modified versions of this material be marked in 
+	   reasonable ways as different from the original version.
+
+	3) This license does not grant any rights to any user of the program 
+	   with regards to rights under trademark law for use of the trade names 
+	   or trademarks of eGovernments Foundation.
+
+  In case of any queries, you can reach eGovernments Foundation at contact@egovernments.org.
+ */
+package org.egov.wtms.application.service;
+
+import java.math.BigDecimal;
+import java.text.ParseException;
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+
+
+
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.transform.AliasToBeanResultTransformer;
+import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class DefaultersWTReportService {
+
+ 
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public Session getCurrentSession() {
+        return entityManager.unwrap(Session.class);
+    }
+
+    public SQLQuery getDefaultersReportDetails(final String fromAmount, final String toAmount,
+            final String ward,
+            final String topDefaulters) throws ParseException {
+    	
+    	final StringBuilder queryStr = new StringBuilder(500);
+    	queryStr.append(
+                "select dcbinfo.hscno as \"hscNo\", dcbinfo.username as \"ownerName\",boundary.name as \"wardName\","
+                + "dcbinfo.houseno as \"houseNo\" , boundary.localname as \"locality\", dcbinfo.mobileno as \"mobileNumber\", "
+                + "dcbinfo.arr_balance as \"arrearsDue\" ,  dcbinfo.curr_balance as \"currentDue\" , dcbinfo.arr_balance+dcbinfo.curr_balance as \"totalDue\"  "
+                + "from egwtr_mv_dcb_view dcbinfo"
+                + " INNER JOIN eg_boundary boundary on dcbinfo.locality = boundary.id ");
+    	
+    	if ( Double.parseDouble(toAmount) == 0)
+    		queryStr.append(" where dcbinfo.arr_balance >="+ fromAmount);
+    	else 
+    		queryStr.append(" where dcbinfo.arr_balance >="+ fromAmount+"and dcbinfo.arr_balance <"+toAmount);
+    	if (ward != null && !ward.isEmpty())
+    		queryStr.append(" and boundary.name = "+"'" +ward+ "'");
+    	if(topDefaulters != null && topDefaulters.equals(" "))
+    		queryStr.append(" limit "+ topDefaulters);
+    	final SQLQuery finalQuery = entityManager.unwrap(Session.class).createSQLQuery(queryStr.toString());
+        finalQuery.setResultTransformer(new AliasToBeanResultTransformer(DefaultersReport.class));
+	  return finalQuery;
+    	
+    	
+    }
+    
+   
+}
+
+
+
+
---
+++ egov/egov-wtmsweb/src/main/webapp/resources/js/app/defaulters-report.js
@@ -0,0 +1,201 @@
+/*#-------------------------------------------------------------------------------
+# eGov suite of products aim to improve the internal efficiency,transparency, 
+#    accountability and the service delivery of the government  organizations.
+# 
+#     Copyright (C) <2015>  eGovernments Foundation
+# 
+#     The updated version of eGov suite of products as by eGovernments Foundation 
+#     is available at http://www.egovernments.org
+# 
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     any later version.
+# 
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+# 
+#     You should have received a copy of the GNU General Public License
+#     along with this program. If not, see http://www.gnu.org/licenses/ or 
+#     http://www.gnu.org/licenses/gpl.html .
+# 
+#     In addition to the terms of the GPL license to be adhered to in using this
+#     program, the following additional terms are to be complied with:
+# 
+# 	1) All versions of this program, verbatim or modified must carry this 
+# 	   Legal Notice.
+# 
+# 	2) Any misrepresentation of the origin of the material is prohibited. It 
+# 	   is required that all modified versions of this material be marked in 
+# 	   reasonable ways as different from the original version.
+# 
+# 	3) This license does not grant any rights to any user of the program 
+# 	   with regards to rights under trademark law for use of the trade names 
+# 	   or trademarks of eGovernments Foundation.
+# 
+#   In case of any queries, you can reach eGovernments Foundation at contact@egovernments.org.
+#-------------------------------------------------------------------------------*/
+jQuery(document).ready(function() {
+	$('#defaultersReport-header').hide();
+	$('#reportgeneration-header').hide();
+	$('#report-footer').hide();
+	
+	
+	jQuery('#defaultersReportSearch').click(function(e) {
+		var fromAmount = $("#fromAmount").val();
+		var toAmount = $("#toAmount").val(); 
+		var ward = $("#ward").val();
+		var topDefaulters = $("#topDefaulters").val();
+		console.log(parseInt(fromAmount)+'-'+parseInt(toAmount)+'-'+topDefaulters);
+		if ((fromAmount == undefined || parseInt(fromAmount) == 0) &&
+				(toAmount == undefined || parseInt(toAmount) == 0) && (topDefaulters == '')) {
+			bootbox.alert('Enter either From amount or  To Amount or Top Defaulters , One is mandatory');
+			return false;
+		}
+		 
+		if (parseInt(toAmount)!=0 && fromAmount > parseInt(toAmount)) {
+			bootbox.alert('From Amount should not be greater than to Amount');
+			return false;
+		}
+		loadingReport();
+	});
+	
+});
+
+function loadingReport()
+{
+		if($('form').valid()){
+			var fromAmount = $("#fromAmount").val();
+			var toAmount = $("#toAmount").val(); 
+			var ward = $("#ward").val();
+			var topDefaulters = $("#topDefaulters").val();
+			
+
+			oTable= $('#defaultersReport-table');
+			$('#defaultersReport-header').show();
+			$('#reportgeneration-header').show();
+	        $("#resultDateLabel").html(fromAmount+" - "+toAmount);	
+			oTable.dataTable({
+				"sPaginationType": "bootstrap",
+				"sDom": "<'row'<'col-xs-12 hidden col-right'f>r>t<'row'<'col-md-3 col-xs-12'i><'col-md-3 col-xs-6 col-right'l><'col-xs-12 col-md-3 col-right'<'export-data'T>><'col-md-3 col-xs-6 text-right'p>>",
+				"aLengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+				"autoWidth": false,
+				"bDestroy": true,
+				"oTableTools" : {
+					"sSwfPath" : "../../../../../../egi/resources/global/swf/copy_csv_xls_pdf.swf",
+					"aButtons" : [ 
+					               {
+						             "sExtends": "pdf",
+	                                 "sPdfMessage": "Defaulters Report",
+	                                 "sTitle": "Water Tax Defaulters Report",
+	                                 "sPdfOrientation": "landscape"
+					                },
+					                {
+							             "sExtends": "xls",
+		                                 "sPdfMessage": "Defaulters Report",
+		                                 "sTitle": "Water Tax Defaulters Report"
+						             },
+						             {
+							             "sExtends": "print",
+		                                 "sPdfMessage": "Defaulters Report",
+		                                 "sTitle": "Water Tax Defaulters Report"
+						             }],
+					
+				},
+				ajax : {
+					url : "/wtms/report/defaultersWTReport/search/result",
+					data : {
+						'ward': ward,
+						'topDefaulters':topDefaulters,
+						'fromAmount' :fromAmount,
+						'toAmount': toAmount
+					}
+				},
+				"columns" : [{"sTitle" : "S.no",},
+							  { "data" : "hscNo" , "title": "H.S.C NO"},  
+							  { "data" : "ownerName", "title": "Owner Name"},
+							  { "data" : "wardName", "title": "Revenue Ward"},
+							  { "data" : "houseNo", "title": "Door No"},
+							  { "data" : "locality", "title": "Locality"},
+							  { "data" : "mobileNumber", "title": "Mobile Number"},
+							  { "data" : "duePeriodFrom", "title": "Due Period From"},
+							  { "data" : "arrearsDue", "title": "Arears Amount"},
+							  { "data" : "currentDue", "title": "Current Amount"},
+							  { "data" : "totalDue", "title": "Total"}
+							],
+							  "aaSorting": [[2, 'desc']] ,
+							  "footerCallback" : function(row, data, start, end, display) {
+									var api = this.api(), data;
+									if (data.length == 0) {
+										jQuery('#report-footer').hide();
+									} else {
+										jQuery('#report-footer').show(); 
+									}
+									if (data.length > 0) {
+										updateTotalFooter(8, api);
+										updateTotalFooter(9, api);
+										updateTotalFooter(10, api);
+									}
+								},
+								"fnRowCallback" : function(nRow, aData, iDisplayIndex){
+					                $("td:first", nRow).html(iDisplayIndex +1);
+					               return nRow;
+					            },
+								"aoColumnDefs" : [ {
+									"aTargets" : [8,9,10],
+									"mRender" : function(data, type, full) {
+										return formatNumberInr(data);    
+									}
+								} ]		
+					});
+			e.stopPropagation();
+		}
+		
+	
+}
+
+function formatNumberInr(x) {
+	if (x) {
+		x = x.toString();
+		var afterPoint = '';
+		if (x.indexOf('.') > 0)
+			afterPoint = x.substring(x.indexOf('.'), x.length);
+		x = Math.floor(x);
+		x = x.toString();
+		var lastThree = x.substring(x.length - 3);
+		var otherNumbers = x.substring(0, x.length - 3);
+		if (otherNumbers != '')
+			lastThree = ',' + lastThree;
+		var res = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",")
+				+ lastThree + afterPoint;
+		return res;
+	}
+	return x;
+}
+
+function updateTotalFooter(colidx, api) {
+	// Remove the formatting to get integer data for summation
+	var intVal = function(i) {
+		return typeof i === 'string' ? i.replace(/[\$,]/g, '') * 1
+				: typeof i === 'number' ? i : 0;
+	};
+
+	// Total over all pages
+	total = api.column(colidx).data().reduce(function(a, b) {
+		return intVal(a) + intVal(b);
+	});
+
+	// Total over this page
+	pageTotal = api.column(colidx, {
+		page : 'current'
+	}).data().reduce(function(a, b) {
+		return intVal(a) + intVal(b);
+	}, 0);
+
+	// Update footer
+	jQuery(api.column(colidx).footer()).html(
+			formatNumberInr(pageTotal) + ' (' + formatNumberInr(total)
+					+ ')');
+}
