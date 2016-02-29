b51256501ad668f4454d5fda4f33435c57358bd4
roopa <roopa.h@egovernments.org>
2016-02-29 12:36:17 +0530

    PHOENIX-2847 updating demand and validation feematrix entry before workflow

--- egov/egov-tl/src/main/java/org/egov/tl/service/TradeLicenseService.java
+++ egov/egov-tl/src/main/java/org/egov/tl/service/TradeLicenseService.java
@@ -131,9 +131,7 @@ public class TradeLicenseService extends AbstractLicenseService<TradeLicense> {
     @Transactional
     public void updateTradeLicense(final TradeLicense license, final WorkflowBean workflowBean) {
 
-        if(null !=license && null !=license.getState() && license.getState().getValue().contains(Constants.WF_STATE_INSPECTION_APPROVED_STR)){
-           updateDemandForChangeTradeArea(license);
-         }
+       
         licensePersitenceService().persist(license);
         tradeLicenseSmsAndEmailService.sendSmsAndEmail(license, workflowBean.getWorkFlowAction());
         updateIndexService.updateTradeLicenseIndexes(license);
@@ -182,6 +180,9 @@ public class TradeLicenseService extends AbstractLicenseService<TradeLicense> {
                         .find("from org.egov.tl.entity.LicenseStatus where code='CAN'");
                 license.setStatus(activeStatus);
             }
+        if(null !=license && null !=license.getState() && license.getState().getValue().contains(Constants.WF_STATE_SANITORY_INSPECTOR_APPROVAL_PENDING)){
+            updateDemandForChangeTradeArea(license);
+          }
     }
 
     public ReportRequest prepareReportInputData(final License license) {
