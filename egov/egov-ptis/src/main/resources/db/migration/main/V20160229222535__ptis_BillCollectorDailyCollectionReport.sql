633ffa3a168d4713f2219d0eb2e07689e195b6c6
pradeep <pradeep@egovernments.org>
2016-02-29 22:41:56 +0530

    PHOENIX-2705 incremental update - Removed not null condition from zone,totalAssessments and blockid field

---
+++ egov/egov-ptis/src/main/resources/db/migration/main/V20160229222535__ptis_BillCollectorDailyCollectionReport.sql
@@ -0,0 +1,3 @@
+alter table EGPT_BILLCOLLCTOR_BLOCKDETAIL alter column zoneid drop not null;
+alter table EGPT_BILLCOLLCTOR_BLOCKDETAIL alter column totalAssessments drop not null;
+alter table EGPT_BILLCOLLCTOR_BLOCKDETAIL alter column blockid drop not null;
