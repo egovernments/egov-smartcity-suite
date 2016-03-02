49e32003b998ea3efcfbb289e21b17dcf5104de2
sathish.p <sathish.p@egovernments.org>
2016-03-02 18:36:10 +0530

    [PHOENIX-1118] refactored code for budgetHeadFormatter

--- egov/egov-egf/src/main/java/org/egov/egf/formatters/BudgetHeadFormatter.java
+++ egov/egov-egf/src/main/java/org/egov/egf/formatters/BudgetHeadFormatter.java
@@ -37,7 +37,7 @@
 
   In case of any queries, you can reach eGovernments Foundation at contact@egovernments.org.
  */
-package org.egov.formatters;
+package org.egov.egf.formatters;
 
 import java.text.ParseException;
 import java.util.Locale;
