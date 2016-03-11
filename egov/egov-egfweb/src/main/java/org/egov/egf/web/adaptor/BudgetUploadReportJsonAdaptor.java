f6745683f2322492764abefec6bd88cee9590291
venki <venki@egovernments.org>
2016-03-11 12:39:59 +0530

    [PHOENIX-3052] - Budget upload report changes.

---
+++ egov/egov-egfweb/src/main/java/org/egov/egf/web/adaptor/BudgetUploadReportJsonAdaptor.java
@@ -0,0 +1,32 @@
+package org.egov.egf.web.adaptor;
+
+import java.lang.reflect.Type;
+import java.math.BigDecimal;
+
+import org.egov.model.budget.BudgetUploadReport;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+public class BudgetUploadReportJsonAdaptor implements JsonSerializer<BudgetUploadReport>
+{
+    @Override
+    public JsonElement serialize(final BudgetUploadReport budgetUploadReport, final Type type, final JsonSerializationContext jsc)
+    { 
+        final JsonObject jsonObject = new JsonObject();
+        if (budgetUploadReport != null)
+        { 
+            jsonObject.addProperty("fundCode", budgetUploadReport.getFundCode()!=null?budgetUploadReport.getFundCode():"");
+            jsonObject.addProperty("deptCode", budgetUploadReport.getDeptCode()!=null?budgetUploadReport.getDeptCode():"");
+            jsonObject.addProperty("functionCode", budgetUploadReport.getFunctionCode()!=null?budgetUploadReport.getFunctionCode():"");
+            jsonObject.addProperty("glCode", budgetUploadReport.getGlCode()!=null?budgetUploadReport.getGlCode():"");
+            jsonObject.addProperty("approvedReAmount", budgetUploadReport.getApprovedReAmount()!=null?budgetUploadReport.getApprovedReAmount():BigDecimal.ZERO);
+            jsonObject.addProperty("planningReAmount", budgetUploadReport.getPlanningReAmount()!=null?budgetUploadReport.getPlanningReAmount():BigDecimal.ZERO);
+            jsonObject.addProperty("approvedBeAmount", budgetUploadReport.getApprovedBeAmount()!=null?budgetUploadReport.getApprovedBeAmount():BigDecimal.ZERO);
+            jsonObject.addProperty("planningBeAmount", budgetUploadReport.getPlanningBeAmount()!=null?budgetUploadReport.getPlanningBeAmount():BigDecimal.ZERO);
+        }
+        return jsonObject;
+    }
+}
