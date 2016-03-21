069d36e0c356f2aac42c5156513b4ad28f71f2a5
sathish.p <sathish.p@egovernments.org>
2016-03-21 11:33:07 +0530

    [PHOENIX-3132] flyway migration db script  issue fix

--- egov/egov-works/src/main/resources/db/migration/main/V20160319180712__works_roleaction_mapping_ward.sql
+++ egov/egov-works/src/main/resources/db/migration/main/V20160319180712__works_roleaction_mapping_ward.sql
@@ -2,7 +2,7 @@
 INSERT INTO EG_ACTION (ID,NAME,URL,QUERYPARAMS,PARENTMODULE,ORDERNUMBER,DISPLAYNAME,ENABLED,CONTEXTROOT,VERSION,CREATEDBY,CREATEDDATE,LASTMODIFIEDBY,LASTMODIFIEDDATE,APPLICATION) values (NEXTVAL('SEQ_EG_ACTION'),'WorksAjaxGetWard','/lineestimate/ajax-getward',null,(SELECT id FROM EG_MODULE WHERE name = 'WorksLineEstimate'),1,'Create Line Estimate','false','egworks',0,1,now(),1,now(),(SELECT id FROM eg_module  WHERE name = 'Works Management'));
 INSERT INTO eg_roleaction (roleid, actionid) VALUES ((SELECT id FROM eg_role WHERE name = 'Super User'),(SELECT id FROM eg_action WHERE name ='WorksAjaxGetWard' and contextroot = 'egworks'));
 
-ALTER TABLE egw_lineestimate_details ALTER COLUMN quantity TYPE double precision;
+ALTER TABLE egw_lineestimate_details ALTER COLUMN quantity TYPE double precision USING quantity::double precision;
 
 ALTER TABLE egw_lineestimate ADD COLUMN workcategory character varying(100) not null;
 
