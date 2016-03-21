2bbe1592f8d690bf5e456d7305018c3f49bc83b4
rishi <rishi@egovernments.org>
2016-03-21 17:32:12 +0530

    PHOENIX-2803 : Single entry/exit to service.

--- egov/egov-collection/src/main/java/org/egov/collection/service/ReceiptHeaderService.java
+++ egov/egov-collection/src/main/java/org/egov/collection/service/ReceiptHeaderService.java
@@ -1813,4 +1813,12 @@ public class ReceiptHeaderService extends PersistenceService<ReceiptHeader, Long
         final CollectionIndex collectionIndex = collectionsUtil.constructCollectionIndex(receiptHeader);
         collectionIndexService.pushCollectionIndex(collectionIndex);
     }
+
+    @Transactional
+    public void persistFieldReceipt(final ReceiptHeader receiptHeader) {
+        this.persist(receiptHeader);
+        LOGGER.info("Receipt Created with receipt number: " + receiptHeader.getReceiptnumber());
+        updateBillingSystemWithReceiptInfo(receiptHeader);
+        LOGGER.info("Billing system updated with receipt info");
+    }
 }
