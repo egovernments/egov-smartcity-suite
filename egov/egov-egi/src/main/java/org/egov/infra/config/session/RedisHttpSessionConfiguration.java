cecb556bee2095fee2931618abc71d9e6e6c701e
sreekanth.nair <sreekanth.nair@egovernments.org>
2016-03-23 17:37:28 +0530

    [PHOENIX-3219] Removed custom CookieHttpSessionStrategy with default one

--- egov/egov-egi/src/main/java/org/egov/infra/config/session/RedisHttpSessionConfiguration.java
+++ egov/egov-egi/src/main/java/org/egov/infra/config/session/RedisHttpSessionConfiguration.java
@@ -43,6 +43,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.CookieHttpSessionStrategy;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 
