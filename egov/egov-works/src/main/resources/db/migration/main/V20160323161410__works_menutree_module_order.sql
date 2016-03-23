2a18a72e6214dc996433642f337a750a7349a34e
sathish.p <sathish.p@egovernments.org>
2016-03-23 17:22:54 +0530

    [PHOENIX-3135] Menu tree reorder and roleaction mapping to WorksCreator role for create LOA

---
+++ egov/egov-works/src/main/resources/db/migration/main/V20160323161410__works_menutree_module_order.sql
@@ -0,0 +1,4 @@
+update eg_module set ordernumber=2 where name='WorksLineEstimate';
+update eg_module set ordernumber=3 where name='WorksAbstractEstimate';
+update eg_module set ordernumber=4 where name='WorksPackage';
+update eg_module set ordernumber=6 where name='WorksLetterOfAcceptance';
