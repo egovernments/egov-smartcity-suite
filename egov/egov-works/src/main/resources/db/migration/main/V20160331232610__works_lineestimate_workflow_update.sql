8473a3b72bcf3c2f3929b0b49fba2e2bd97b30d0
sathish.p <sathish.p@egovernments.org>
2016-03-31 23:35:47 +0530

    [PHOENIX-3195] corrected workflow matrix

--- egov/egov-works/src/main/resources/db/migration/main/V20160331232610__works_lineestimate_workflow_update.sql
+++ egov/egov-works/src/main/resources/db/migration/main/V20160331232610__works_lineestimate_workflow_update.sql
@@ -1,4 +1,4 @@
 -----------Update script to show Technical Sanctioned status in history---------------
-update eg_wf_matrix SET nextstate = 'Techincal Sanctioned' where id = (select id from eg_wf_matrix where currentstate = 'Admin Sanctioned');
+update eg_wf_matrix SET nextstate = 'Techincal Sanctioned' where id = (select id from eg_wf_matrix where currentstate = 'Admin Sanctioned') and objecttype='LineEstimate';
 
---rollback update eg_wf_matrix SET nextstate = 'END' where id = (select id from eg_wf_matrix where currentstate = 'Admin Sanctioned');
+--rollback update eg_wf_matrix SET nextstate = 'END' where id = (select id from eg_wf_matrix where currentstate = 'Admin Sanctioned') and objecttype='LineEstimate';
