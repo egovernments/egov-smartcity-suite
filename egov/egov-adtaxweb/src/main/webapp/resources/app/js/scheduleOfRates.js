6a85490e54813890152d429d96515a737bd1de30
Nagalinga.MP <nagalinga.mp@egovernments.org>
2016-03-31 13:31:29 +0530

    PHOENIX-3365 Adding export option with PDF and excel formats.

--- egov/egov-adtaxweb/src/main/webapp/resources/app/js/scheduleOfRates.js
+++ egov/egov-adtaxweb/src/main/webapp/resources/app/js/scheduleOfRates.js
@@ -121,10 +121,14 @@ jQuery(document).ready(function ($) {
 	        	}
 			},
 			"sPaginationType": "bootstrap",
-			"sDom": "<'row'<'col-xs-12 hidden col-right'f>r>t<'row'<'col-md-5 col-xs-12'i><'col-md-3 col-xs-6'l><'col-md-4 col-xs-6 text-right'p>>",
+			"sDom" : "<'row'<'col-xs-12 hidden col-right'f>r>t<'row'<'col-md-3 col-xs-12'i><'col-md-3 col-xs-6 col-right'l><'col-xs-12 col-md-3 col-right'<'export-data'T>><'col-md-3 col-xs-6 text-right'p>>",
 			"aLengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
 			"bDestroy": true,
 			"autoWidth": false,
+			"oTableTools" : {
+				"sSwfPath" : "../../../../../../egi/resources/global/swf/copy_csv_xls_pdf.swf",
+				"aButtons" : [ "xls", "pdf", "print" ]
+			},
 			"columns" : [
 			  { "data" : "financialyear", "title":"Financial Year"},
 			  { "data" : "unitfactor", "title":"Unit Rate"},
