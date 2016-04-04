6cef6d4d6c155e010b85262223d5495338a327ac
sreekanth.nair <sreekanth.nair@egovernments.org>
2016-04-04 12:35:07 +0530

    [PHOENIX-379]  Enhanced to fill city details in ThreadLocal

--- egov/egov-egi/src/main/java/org/egov/infra/scheduler/quartz/AbstractQuartzJob.java
+++ egov/egov-egi/src/main/java/org/egov/infra/scheduler/quartz/AbstractQuartzJob.java
@@ -39,11 +39,9 @@
  */
 package org.egov.infra.scheduler.quartz;
 
-import java.util.List;
-
-import javax.annotation.Resource;
-
 import org.apache.commons.lang.StringUtils;
+import org.egov.infra.admin.master.entity.City;
+import org.egov.infra.admin.master.service.CityService;
 import org.egov.infra.admin.master.service.UserService;
 import org.egov.infra.scheduler.GenericJob;
 import org.egov.infra.utils.EgovThreadLocals;
@@ -54,6 +52,9 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * An abstract base class wrapper for {@link QuartzJobBean} and implements {@link GenericJob}. A class which extends this will be
@@ -73,26 +74,28 @@ public abstract class AbstractQuartzJob extends QuartzJobBean implements Generic
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private CityService cityService;
+
     /**
      * This method will wrap up the Transaction (if isTransactional set to true) and call the executeJob implementation on
      * individual job class.
      **/
     @Override
-    protected void executeInternal(final JobExecutionContext jobCtx) throws JobExecutionException {
+    protected void executeInternal(JobExecutionContext jobCtx) throws JobExecutionException {
         try {
             MDC.put("appname", jobCtx.getJobDetail().getKey().getName());
-            if (isTransactional)
-                for (final String tenant : cities) {
+            if (this.isTransactional)
+                for (String tenant : this.cities) {
                     MDC.put("ulbcode", tenant);
-                    setTractionalSupport(tenant);
-                    setUserInThreadLocal();
-                    executeJob();
+                    this.prepareThreadLocal(tenant);
+                    this.executeJob();
                 }
             else
-                executeJob();
+                this.executeJob();
 
-        } catch (final Exception ex) {
-            LOGGER.error("Unable to complete execution Scheduler ", ex);
+        } catch (Exception ex) {
+            AbstractQuartzJob.LOGGER.error("Unable to complete execution Scheduler ", ex);
             throw new JobExecutionException("Unable to execute batch job Scheduler", ex, false);
         } finally {
             EgovThreadLocals.clearValues();
@@ -100,22 +103,27 @@ public abstract class AbstractQuartzJob extends QuartzJobBean implements Generic
         }
     }
 
-    public void setUserName(final String userName) {
+    public void setUserName(String userName) {
         if (StringUtils.isBlank(userName))
             this.userName = "egovernments";
         else
             this.userName = userName;
     }
 
-    public void setTransactional(final boolean isTransactional) {
+    public void setTransactional(boolean isTransactional) {
         this.isTransactional = isTransactional;
     }
 
-    protected void setTractionalSupport(final String tenant) {
-        EgovThreadLocals.setTenantID(tenant);
+    protected void prepareCityThreadLocal() {
+        City city = this.cityService.findAll().get(0);
+        EgovThreadLocals.setCityCode(city.getCode());
+        EgovThreadLocals.setCityName(city.getName());
+        EgovThreadLocals.setMunicipalityName(city.getPreferences().getMunicipalityName());
+        EgovThreadLocals.setDomainName(city.getDomainURL());
     }
 
-    protected void setUserInThreadLocal() {
-        EgovThreadLocals.setUserId(userService.getUserByUsername(userName).getId());
+    private void prepareThreadLocal(String tenant) {
+        EgovThreadLocals.setTenantID(tenant);
+        EgovThreadLocals.setUserId(this.userService.getUserByUsername(this.userName).getId());
     }
 }
