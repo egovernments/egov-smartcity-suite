6c37a36863d84b98366912ae1d87e597dd29fc3f
kavya <kavya.ys@egovernments.org>
2016-04-05 11:33:03 +0530

    [PHOENIX-3476]-FA Role action mapping for fund and financial year masters.

---
+++ egov/egov-egf/src/main/resources/db/migration/main/V20160405112347__egf_FA_roleaction_fund_FY.sql
@@ -0,0 +1,28 @@
+
+Insert into eg_roleaction values((select id from eg_role where name='Financial Administrator'),(select id from eg_action where name='New-CFinancialYear'));
+Insert into eg_roleaction values((select id from eg_role where name='Financial Administrator'),(select id from eg_action where name='Create-CFinancialYear'));
+Insert into eg_roleaction values((select id from eg_role where name='Financial Administrator'),(select id from eg_action where name='Update-CFinancialYear'));
+Insert into eg_roleaction values((select id from eg_role where name='Financial Administrator'),(select id from eg_action where name='View-CFinancialYear'));
+Insert into eg_roleaction values((select id from eg_role where name='Financial Administrator'),(select id from eg_action where name='Edit-CFinancialYear'));
+Insert into eg_roleaction values((select id from eg_role where name='Financial Administrator'),(select id from eg_action where name='Result-CFinancialYear'));
+Insert into eg_roleaction values((select id from eg_role where name='Financial Administrator'),(select id from eg_action where name='Search and View-CFinancialYear'));
+Insert into eg_roleaction values((select id from eg_role where name='Financial Administrator'),(select id from eg_action where name='Search and Edit-CFinancialYear'));
+Insert into eg_roleaction values((select id from eg_role where name='Financial Administrator'),(select id from eg_action where name='Search and View Result-CFinancialYear'));
+Insert into eg_roleaction values((select id from eg_role where name='Financial Administrator'),(select id from eg_action where name='Search and Edit Result-CFinancialYear'));
+
+
+
+Insert into eg_roleaction values((select id from eg_role where name='Financial Administrator'),(select id from eg_action where name='New-Fund'));
+Insert into eg_roleaction values((select id from eg_role where name='Financial Administrator'),(select id from eg_action where name='Create-Fund'));
+Insert into eg_roleaction values((select id from eg_role where name='Financial Administrator'),(select id from eg_action where name='Update-Fund'));
+Insert into eg_roleaction values((select id from eg_role where name='Financial Administrator'),(select id from eg_action where name='View-Fund'));
+Insert into eg_roleaction values((select id from eg_role where name='Financial Administrator'),(select id from eg_action where name='Edit-Fund'));
+Insert into eg_roleaction values((select id from eg_role where name='Financial Administrator'),(select id from eg_action where name='Result-Fund'));
+Insert into eg_roleaction values((select id from eg_role where name='Financial Administrator'),(select id from eg_action where name='Search and View-Fund'));
+Insert into eg_roleaction values((select id from eg_role where name='Financial Administrator'),(select id from eg_action where name='Search and Edit-Fund'));
+Insert into eg_roleaction values((select id from eg_role where name='Financial Administrator'),(select id from eg_action where name='Search and View Result-Fund'));
+Insert into eg_roleaction values((select id from eg_role where name='Financial Administrator'),(select id from eg_action where name='Search and Edit Result-Fund'));
+
+
+
+
