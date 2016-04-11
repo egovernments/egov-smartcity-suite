8a038aafef8ff98cd5d48572ffa776a16548cd6b
Rafeek <mohammad.rafeek@egovernments.org>
2016-04-11 15:59:21 +0530

    PHOENIX-3524 : Fix for Title Transfer is not processing

--- egov/egov-ptisweb/src/main/java/org/egov/ptis/actions/transfer/PropertyTransferAction.java
+++ egov/egov-ptisweb/src/main/java/org/egov/ptis/actions/transfer/PropertyTransferAction.java
@@ -520,8 +520,8 @@ public class PropertyTransferAction extends GenericWorkFlowAction {
             final Map<String, BigDecimal> propertyTaxDetails = propertyService
                     .getCurrentPropertyTaxDetails(basicproperty.getActiveProperty());
             currentPropertyTax = propertyTaxDetails.get(CURR_DMD_STR);
-            currentPropertyTaxDue = propertyTaxDetails.get(CURR_DMD_STR)
-                    .subtract(propertyTaxDetails.get(CURR_COLL_STR));
+            currentPropertyTaxDue = propertyTaxDetails.get(PropertyTaxConstants.CURR_FIRSTHALF_DMD_STR)
+                    .subtract(propertyTaxDetails.get(PropertyTaxConstants.CURR_FIRSTHALF_COLL_STR));
             arrearPropertyTaxDue = propertyTaxDetails.get(ARR_DMD_STR).subtract(propertyTaxDetails.get(ARR_COLL_STR));
             documentTypes = transferOwnerService.getPropertyTransferDocumentTypes();
             addDropdownData("MutationReason", transferOwnerService.getPropertyTransferReasons());
