7057fddfac36b4075d63d734db17b6fa3464a0e7
subhash.bose@egovernments.org <subhash.bose@egovernments.org>
2016-04-11 19:48:42 +0530

    PHOENIX-3497: Rest api rebate issue fixes

--- egov/egov-ptis/src/main/java/org/egov/ptis/client/integration/utils/CollectionHelper.java
+++ egov/egov-ptis/src/main/java/org/egov/ptis/client/integration/utils/CollectionHelper.java
@@ -186,7 +186,7 @@ public class CollectionHelper {
 
 		for (EgBillDetails billDet : billDetails) {
 			receiptDetails.add(initReceiptDetail(billDet.getGlcode(), BigDecimal.ZERO, // billDet.getCrAmount(),
-					billDet.getCrAmount(), billDet.getDrAmount(), billDet.getDescription()));
+					billDet.getCrAmount().subtract(billDet.getDrAmount()), billDet.getDrAmount(), billDet.getDescription()));
 		}
 
 		SpringBeanUtil.getPropertyTaxCollection().apportionPaidAmount(String.valueOf(bill.getId()), amountPaid, receiptDetails);
