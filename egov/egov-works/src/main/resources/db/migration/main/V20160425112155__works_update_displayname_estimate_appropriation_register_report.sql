0f6ccaa526620b6b0318013a13d00d3301cdec7d
Ritesh <ritesh.chikatwar@egovernments.org>
2016-04-25 11:41:55 +0530

    [PHOENIX-3579] - Estimate planning Budget appropriation register renaming the menu tree name

---
+++ egov/egov-works/src/main/resources/db/migration/main/V20160425112155__works_update_displayname_estimate_appropriation_register_report.sql
@@ -0,0 +1,3 @@
+--------------Start--------------------
+update eg_action set displayname  = 'Estimate Appropriation Register Report' where name='EstimateAppropriationRegisterReport' and contextroot = 'egworks';
+--------------End----------------------
