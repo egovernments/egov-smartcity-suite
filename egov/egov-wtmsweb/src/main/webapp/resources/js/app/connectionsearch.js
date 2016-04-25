1205090e9b893d5dfbbe233745b74527d6b35b48
sushant <sushant_vs@cms.co.in>
2016-04-25 14:12:08 +0530

    PHOENIX-3679: Provide export option in Search Connection

--- egov/egov-wtmsweb/src/main/webapp/resources/js/app/connectionsearch.js
+++ egov/egov-wtmsweb/src/main/webapp/resources/js/app/connectionsearch.js
@@ -189,6 +189,7 @@ function submitButton()
 	var citizenRole = $('#citizenRole').val();
 	var billcollector=$('#billcollectionRole').val();
 	
+	
 	$('#searchResultDiv').show();
 	$.post("/wtms/search/waterSearch/",$('#waterSearchRequestForm').serialize())
 	.done(function(searchResult) {
@@ -196,9 +197,33 @@ function submitButton()
 	tableContainer.dataTable({
 	destroy : true,
 	"sPaginationType" : "bootstrap",
-	"sDom" : "<'row'<'col-xs-12 hidden col-right'f>r>t<'row'<'col-md-6 col-xs-12'i><'col-md-3 col-xs-6'l><'col-md-3 col-xs-6 text-right'p>>",
+	"sDom": "<'row'<'col-xs-12 hidden col-right'f>r>t<'row'<'col-md-3 col-xs-12'i><'col-md-3 col-xs-6 col-right'l><'col-xs-12 col-md-3 col-right'<'export-data'T>><'col-md-3 col-xs-6 text-right'p>>",
 	"aLengthMenu" : [[10,25,50,-1 ],[10,25,50,"All" ] ],
 	"autoWidth" : false,
+	"oTableTools" : {
+		"sSwfPath" : "../../../../../../egi/resources/global/swf/copy_csv_xls_pdf.swf",
+		"aButtons" : [ 
+		               {
+			             "sExtends": "pdf",
+			             "mColumns": [0,1,2,4,5,6,9],
+                         "sPdfMessage": "",
+                         "sTitle": "Water Connection Report",
+                         "sPdfOrientation": "landscape"
+		                },
+		                {
+				             "sExtends": "xls",
+				             "mColumns": [0,1,2,4,5,6,9],
+                             "sPdfMessage": "Water Connection Report",
+                             "sTitle": "Water Connection Report"
+			             },
+			             {
+				             "sExtends": "print",
+				             "mColumns": [0,1,2,4,5,6,9],
+                             "sPdfMessage": "",
+                             "sTitle": "Water Connection Report"
+			             }],
+		
+	},
 	searchable : true,
 	data : searchResult,
 	columns : [{title : 'Applicant Name',data : 'resource.searchable.consumername'},
