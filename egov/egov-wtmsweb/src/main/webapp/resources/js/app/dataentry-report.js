64479b1e002b80b5237445f67c937e68099c4eac
sushant <sushant_vs@cms.co.in>
2016-05-06 19:11:42 +0530

    PHOENIX 3804 : generating PDF

--- egov/egov-wtmsweb/src/main/webapp/resources/js/app/dataentry-report.js
+++ egov/egov-wtmsweb/src/main/webapp/resources/js/app/dataentry-report.js
@@ -68,7 +68,7 @@ function loadingReport()
 					"aButtons" : [ 
 					               {
 						             "sExtends": "pdf",
-						             "mColumns": [ 1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
+						             "mColumns": [ 1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24],
 	                                 "sPdfMessage": "Data Entry Connection Report as on "+today+"",
 	                                 "sTitle": "Data Entry Connection Report",
 	                                 "sPdfOrientation": "landscape"
