20080bf9985eae09ac723032207550aaba1a8239
jayashree.reddy@egovernments.org <jayashree.reddy@egovernments.org>
2016-05-18 17:29:13 +0530

    PHOENIX-1526 Grievance officer should be able to reopen the completed complaint

--- egov/egov-pgrweb/src/main/webapp/resources/js/app/search-complaint.js
+++ egov/egov-pgrweb/src/main/webapp/resources/js/app/search-complaint.js
@@ -147,7 +147,7 @@ var tableContainer;
 		var currentStatus=tableContainer1.fnGetData(this,4);
 		var CurrentPosition=$('#employeeposition').val();
 		var isgorole= $('#isgorole').val();
-		if((currentStatus == 'COMPLETED' || currentStatus == 'REJECTED'|| currentStatus == 'WITHDRAWN') && CurrentPosition != "0")
+		if(((currentStatus == 'COMPLETED' && isgorole == 'false') || currentStatus == 'REJECTED'|| currentStatus == 'WITHDRAWN') && CurrentPosition != "0")
 			window.open("/pgr/complaint/view/"+crn);
 		else if (currentOwner == CurrentPosition || isgorole == 'true')
 			window.open("/pgr/complaint/update/"+crn);
