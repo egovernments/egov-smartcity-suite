ecc603ca6cee80129a642913cbbae3d910bd8866
sreekanth.nair <sreekanth.nair@egovernments.org>
2016-05-19 17:30:46 +0530

    [PHOENIX-3219] Compilation error fixed for merged sources

--- egov/egov-wtms/src/main/java/org/egov/wtms/utils/WaterTaxNumberGenerator.java
+++ egov/egov-wtms/src/main/java/org/egov/wtms/utils/WaterTaxNumberGenerator.java
@@ -44,7 +44,6 @@ import java.sql.SQLException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 
 import org.egov.infra.exception.ApplicationRuntimeException;
 import org.egov.infra.persistence.utils.DBSequenceGenerator;
@@ -54,6 +53,7 @@ import org.egov.wtms.utils.constants.WaterTaxConstants;
 import org.hibernate.exception.SQLGrammarException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class WaterTaxNumberGenerator {
