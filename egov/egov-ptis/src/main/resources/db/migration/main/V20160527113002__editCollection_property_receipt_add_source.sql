eb65d650f3c9fe188a3c23bdc23b51e230239c69
subhash.bose@egovernments.org <subhash.bose@egovernments.org>
2016-05-27 11:33:20 +0530

    AP-3313: sequence issue fix

--- egov/egov-ptis/src/main/resources/db/migration/main/V20160527113002__editCollection_property_receipt_add_source.sql
+++ egov/egov-ptis/src/main/resources/db/migration/main/V20160527113002__editCollection_property_receipt_add_source.sql
@@ -1,4 +1,2 @@
 alter table egpt_property_receipts add column source character varying(1) default 'M';
 alter table egpt_property_receipts add column remarks character varying(256);
-drop sequence if exists seq_egptproperty_receipts;
-create sequence seq_egptproperty_receipts;
