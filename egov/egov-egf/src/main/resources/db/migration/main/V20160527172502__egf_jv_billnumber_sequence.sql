e829e0770e32212ba9909e68c7f6716b9330c909
venki <venki@egovernments.org>
2016-05-27 17:25:33 +0530

    [PHOENIX-3993] - Externalize the BillNumber generation in JV Screen.

--- egov/egov-egf/src/main/resources/db/migration/main/V20160527172502__egf_jv_billnumber_sequence.sql
+++ egov/egov-egf/src/main/resources/db/migration/main/V20160527172502__egf_jv_billnumber_sequence.sql
@@ -1,7 +1,9 @@
-create sequence seq_jv_billnumber_2016_17;
+create if not exists sequence seq_jv_billnumber_2016_17;
+
+create if not exists sq_billnumber_mn;
 
 select setval('seq_jv_billnumber_2016_17', ((Select case when seq = null then 2 else case when seq=0 then 2 else seq end end from (select nextval('sq_billnumber_mn') as seq) t )-1));
 
-drop sequence sq_billnumber_mn;
+drop if exists sequence sq_billnumber_mn;
 
 delete from eg_script where name = 'autobillnumber';
