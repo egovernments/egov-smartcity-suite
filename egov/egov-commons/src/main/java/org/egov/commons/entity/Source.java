45eafe5f43e8113bb9dc1e9d46a69a22c88e25f8
Manasa Niranjan <manasa.niranjan@egovernments.org>
2016-06-22 13:18:40 +0530

    PHOENIX-4382 : CARD Integration: Validate for CARD IP address in rest API filter

--- egov/egov-commons/src/main/java/org/egov/commons/entity/Source.java
+++ egov/egov-commons/src/main/java/org/egov/commons/entity/Source.java
@@ -43,7 +43,7 @@ import org.apache.commons.lang3.StringUtils;
 
 public enum Source {
 
-    APONLINE, ESEVA, MEESEVA, SYSTEM,SOFTTECH;
+    APONLINE, ESEVA, MEESEVA, SYSTEM,SOFTTECH, CARD;
 
     @Override
     public String toString() {
