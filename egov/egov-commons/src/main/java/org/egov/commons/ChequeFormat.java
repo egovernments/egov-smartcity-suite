2f8b6d9a3e9237b487393b8ba2d0d8a2c1a250e4
kavya <kavya.ys@egovernments.org>
2016-07-18 10:19:28 +0530

    [PHOENIX-4672]-Cheque Formats mapping to Bank account codes.

--- egov/egov-commons/src/main/java/org/egov/commons/ChequeFormat.java
+++ egov/egov-commons/src/main/java/org/egov/commons/ChequeFormat.java
@@ -37,7 +37,7 @@
  *
  *   In case of any queries, you can reach eGovernments Foundation at contact@egovernments.org.
  */
-package org.egov.egf.model;
+package org.egov.commons;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
