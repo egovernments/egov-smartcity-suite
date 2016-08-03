4d3b046b8ca20c1a93f8e1e1528146ae31d4a840
manoj <manoj.kulkarni@egovernments.org>
2016-08-03 18:18:48 +0530

    [PHOENIX-4955] - Changing MB Date validation message.

--- egov/egov-works/src/main/java/org/egov/works/mb/service/MBHeaderService.java
+++ egov/egov-works/src/main/java/org/egov/works/mb/service/MBHeaderService.java
@@ -652,7 +652,7 @@ public class MBHeaderService {
         if (offlineStatus != null) {
             if (offlineStatus.getStatusDate().after(mbHeader.getMbDate())) {
                 message = messageSource.getMessage("error.mb.entry.date.commenced.date",
-                        new String[] {},
+                        new String[] { mbHeader.getMbDate().toString()},
                         null);
                 jsonObject.addProperty("errorEntryCommencedDate", message);
                 errors.reject("errorEntryCommencedDate", message);
