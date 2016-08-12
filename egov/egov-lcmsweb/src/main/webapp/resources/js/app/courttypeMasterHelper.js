93382854902f33b118695b72a9a193f5bcc42f9d
ashish <ashish.mk@egovernments.org>
2016-08-12 14:06:06 +0530

    (PHOENIX-4851)- alignment issue in search result screen

--- egov/egov-lcmsweb/src/main/webapp/resources/js/app/courttypeMasterHelper.js
+++ egov/egov-lcmsweb/src/main/webapp/resources/js/app/courttypeMasterHelper.js
@@ -71,6 +71,7 @@ function callAjaxSearch() {
 				 },
 				"sPaginationType" : "bootstrap",
 				"bDestroy" : true,
+				'bAutoWidth': false,
 				"sDom" : "<'row'<'col-xs-12 hidden col-right'f>r>t<'row'<'col-xs-3'i><'col-xs-3 col-right'l><'col-xs-3 col-right'<'export-data'T>><'col-xs-3 text-right'p>>",
 				"aLengthMenu" : [ [ 10, 25, 50, -1 ], [ 10, 25, 50, "All" ] ],
 				"oTableTools" : {
