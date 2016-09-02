2543d626ae6f606bc8a56601f17dd55171b39b37
pradeep <pradeep@egovernments.org>
2016-09-02 21:09:33 +0530

    PHOENIX-5124 - Correct database script

--- egov/egov-council/src/main/resources/db/migration/main/V20160902210501__council_MOM_ddl.sql
+++ egov/egov-council/src/main/resources/db/migration/main/V20160902210501__council_MOM_ddl.sql
@@ -2,7 +2,7 @@ INSERT into egw_status (ID,MODULETYPE,DESCRIPTION,LASTMODIFIEDDATE,CODE,ORDER_ID
 
 INSERT into egw_status (ID,MODULETYPE,DESCRIPTION,LASTMODIFIEDDATE,CODE,ORDER_ID) values (nextval('SEQ_EGW_STATUS'),'COUNCILMEETING','MOM_FINALISED',now(),'MOM FINALISED',4);
 
-INSERT into eg_action(id,name,url,parentmodule,ordernumber,displayname,enabled,contextroot,application) values(nextval('SEQ_EG_ACTION'),'generate-resolution','/councilmom/generateresolution',(select id from eg_module where name='Council Meeting' and parentmodule=(select id from eg_module where name='Council Meeting')),4,'view-resolution',false,'council',(select id from eg_module where name='Council Management' and parentmodule is null));
+INSERT into eg_action(id,name,url,parentmodule,ordernumber,displayname,enabled,contextroot,application)  values(nextval('SEQ_EG_ACTION'),'generate-resolution','/councilmom/generateresolution', (select id from eg_module where name='Council Meeting' and parentmodule=(select id from eg_module where name='Council Management Transaction')) ,4,'view-resolution',false,'council',(select id from eg_module where name='Council Management' and parentmodule is null));
 
 INSERT into eg_roleaction values((select id from eg_role where name='Super User'),(select id from eg_action where name='generate-resolution'));
 
