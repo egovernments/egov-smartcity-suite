aa68bc4c9562360d2692b4d1d039927f0b8444d5
sumanth <sumanth@egovernments.org>
2016-09-07 15:26:42 +0530

    PHOENIX-5096: Making ComplaintIndexing via AOP.

---
+++ egov/egov-pgr/src/main/java/org/egov/pgr/elasticSearch/service/advice/ComplaintIndexingAdvice.java
@@ -0,0 +1,71 @@
+package org.egov.pgr.elasticSearch.service.advice;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.egov.infra.config.properties.ApplicationProperties;
+import org.egov.pgr.elasticSearch.entity.ComplaintIndex;
+import org.egov.pgr.elasticSearch.service.ComplaintIndexService;
+import org.egov.pgr.entity.Complaint;
+import org.elasticsearch.client.transport.NoNodeAvailableException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ComplaintIndexingAdvice {
+
+	private static final Logger LOG = LoggerFactory.getLogger(ComplaintIndexingAdvice.class);
+
+	@Autowired
+	private ComplaintIndexService complaintIndexService;
+	
+	@Autowired
+    private ApplicationProperties applicationProperties;
+
+	@Pointcut("execution(*  org.egov.pgr.service.ComplaintService.createComplaint (..))")
+	private void createComplaintPointCut(){	
+	}
+
+	@Pointcut("execution(* org.egov.pgr.service.ComplaintService.update (..))")
+	private void updateComplaintPointCut(){
+
+	}
+
+	@AfterReturning(pointcut = "createComplaintPointCut()", returning = "retVal")
+	public void createComplaintIndex(final Object retVal){
+		Complaint savedComplaint = (Complaint)retVal;
+		final ComplaintIndex savedComplaintIndex = new ComplaintIndex();
+		BeanUtils.copyProperties(savedComplaint, savedComplaintIndex);
+		try {
+			// Indexing complaint here
+			complaintIndexService.createComplaintIndex(savedComplaintIndex);
+		} catch (NoNodeAvailableException exception) {
+			if (applicationProperties.devMode())
+				LOG.error("An error occurred, None of the Elastic search nodes are Available ", exception);
+			else
+				throw exception;
+		}
+	}
+
+	@AfterReturning(pointcut = "updateComplaintPointCut()", returning = "retVal")
+	public void updateComplaintIndex(JoinPoint joinPoint, final Object retVal){
+		Complaint savedComplaint = (Complaint)retVal;
+		Object[] arguments = joinPoint.getArgs();
+		final ComplaintIndex savedComplaintIndex = new ComplaintIndex();
+		BeanUtils.copyProperties(savedComplaint, savedComplaintIndex);
+		try { 
+			// Indexing complaint here
+			complaintIndexService.updateComplaintIndex(savedComplaintIndex, Long.valueOf(arguments[1].toString()),arguments[2].toString());
+		} catch (NoNodeAvailableException exception) {
+			if (applicationProperties.devMode())
+				LOG.error("An error occurred, None of the Elastic search nodes are Available ", exception);
+			else
+				throw exception;
+		}
+	}
+}
