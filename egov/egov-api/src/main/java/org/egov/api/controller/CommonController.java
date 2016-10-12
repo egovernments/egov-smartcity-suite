fe5cbb149dc36a2b94a3cd75cc2409b4492bd8d3
Dinesh S <dinesh.s@egovernments.org>
2016-10-12 12:21:10 +0530

    PHOENIX-5424 | Fixed email complaint update

--- egov/egov-api/src/main/java/org/egov/api/controller/CommonController.java
+++ egov/egov-api/src/main/java/org/egov/api/controller/CommonController.java
@@ -115,9 +115,11 @@ public class CommonController extends ApiController {
             citizenCreate.setMobileNumber(citizen.get("mobileNumber").toString());
             citizenCreate.setName(citizen.get("name").toString());
             
-            if(!citizen.get("emailId").toString().equals(""))
-            citizenCreate.setEmailId(citizen.get("emailId").toString());
-            
+	    if(citizen.get("emailId")!=null){
+            	if(!citizen.get("emailId").toString().equals(""))
+            	citizenCreate.setEmailId(citizen.get("emailId").toString());
+	    }
+		
             citizenCreate.setPassword(citizen.get("password").toString());
             Device device = deviceRepository.findByDeviceUId(citizen.get("deviceId").toString());
             if (device == null) {
