901ddb9ccc3f8deaedf39812778ddd727d67d974
Ritesh <ritesh.chikatwar@egovernments.org>
2016-10-18 19:23:27 +0530

    [PHOENIX-5413] - Notification to the engineer in charge when MB submitted from contractor portal

--- egov/egov-worksweb/src/main/java/org/egov/works/web/controller/contractorportal/CreateContractorMBController.java
+++ egov/egov-worksweb/src/main/java/org/egov/works/web/controller/contractorportal/CreateContractorMBController.java
@@ -1,3 +1,42 @@
+/*
+ * eGov suite of products aim to improve the internal efficiency,transparency,
+ *    accountability and the service delivery of the government  organizations.
+ *
+ *     Copyright (C) <2015>  eGovernments Foundation
+ *
+ *     The updated version of eGov suite of products as by eGovernments Foundation
+ *     is available at http://www.egovernments.org
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see http://www.gnu.org/licenses/ or
+ *     http://www.gnu.org/licenses/gpl.html .
+ *
+ *     In addition to the terms of the GPL license to be adhered to in using this
+ *     program, the following additional terms are to be complied with:
+ *
+ *         1) All versions of this program, verbatim or modified must carry this
+ *            Legal Notice.
+ *
+ *         2) Any misrepresentation of the origin of the material is prohibited. It
+ *            is required that all modified versions of this material be marked in
+ *            reasonable ways as different from the original version.
+ *
+ *         3) This license does not grant any rights to any user of the program
+ *            with regards to rights under trademark law for use of the trade names
+ *            or trademarks of eGovernments Foundation.
+ *
+ *   In case of any queries, you can reach eGovernments Foundation at contact@egovernments.org.
+ */
 package org.egov.works.web.controller.contractorportal;
 
 import java.io.IOException;
@@ -6,7 +45,9 @@ import java.math.BigDecimal;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.egov.infra.exception.ApplicationException;
+import org.egov.infra.messaging.MessagingService;
 import org.egov.works.abstractestimate.service.EstimateService;
 import org.egov.works.contractorportal.entity.ContractorMBHeader;
 import org.egov.works.contractorportal.service.ContractorMBHeaderService;
@@ -52,6 +93,9 @@ public class CreateContractorMBController {
 
     @Autowired
     private TrackMilestoneService trackMilestoneService;
+
+    @Autowired
+    private MessagingService messagingService;
 
     @RequestMapping(value = "/search-loaform", method = RequestMethod.GET)
     public String showSearchLoaForm() {
@@ -102,6 +146,17 @@ public class CreateContractorMBController {
                 workOrderEstimateService.getWorkOrderEstimateById(contractorMBHeader.getWorkOrderEstimate().getId()));
 
         final ContractorMBHeader savedContractorMBHeader = contractorMBHeaderService.create(contractorMBHeader, files);
+
+        final String smsMessage = messageSource.getMessage("msg.mb.created.sms",
+                new String[] { contractorMBHeader.getWorkOrderEstimate().getWorkOrder().getWorkOrderNumber(),
+                        contractorMBHeader.getWorkOrderEstimate().getWorkOrder().getContractor().getName(),
+                        contractorMBHeader.getMbAmount().toString() },
+                null);
+
+        final String mobileNumber = contractorMBHeader.getWorkOrderEstimate().getWorkOrder().getEngineerIncharge()
+                .getMobileNumber();
+        if (StringUtils.isNotBlank(mobileNumber))
+            messagingService.sendSMS(mobileNumber, smsMessage);
 
         return "redirect:/contractorportal/mb/contractormb-success?contractormb=" + savedContractorMBHeader.getId();
     }
