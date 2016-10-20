f7d1d3fb6dc4e817907ca287a994f88beaf13940
pratik <pratik.kumar@egovernments.org>
2016-10-20 15:50:10 +0530

    Marriage Registration unit initial data scripts.

---
+++ egov/egov-mrs/src/main/resources/db/migration/main/V20161020154407__mrs_registrationunit_initialdata.sql
@@ -0,0 +1,1 @@
+INSERT INTO egmrs_registrationunit VALUES (nextval('SEQ_EGMRS_REGISTRATIONUNIT'), 'Municipal office', 'OOBH', 'municipal office', true, 3, true, 0, 1, current_timestamp, 1, current_timestamp);
