c625ac2921a51cbd3ea16f021a09dcd4a5a40a37
Vasanth KG <vasanth@egovernments.org>
2016-10-28 16:37:00 +0530

    [EOPS-285] Deploy script added for local deployment.

--- dev-utils/deployment/deploy.sh
+++ dev-utils/deployment/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 EARFILE="../../egov/egov-ear/target/egov-ear-*.ear"
-if [ -z $WILDFLY_HOME ]
+dated=`date '+%d-%m-%y'`
+if [ -z "$WILDFLY_HOME" ]
 then
 	echo "WILDFLY_HOME is not set on environment variable."
 	echo "To set WILDFLY_HOME environment variable, do the following"
@@ -13,14 +14,27 @@ then
 	echo "	bash$ source ~/.bashrc"
 	exit 1;
 fi
+
+if [ ! -d "$WILDFLY_HOME" ]; 
+then
+	echo "${WILDFLY_HOME} doesn't exist."
+	exit 1;
+fi
+
 DEPLOY_FOLDER="${WILDFLY_HOME}/standalone/deployments"
+
+[ -d "${WILDFLY_HOME}/Archive" ] || mkdir  -p "${WILDFLY_HOME}/Archive"
+echo "Archiving OLD EAR to ${WILDFLY_HOME}/Archive"
+mv ${DEPLOY_FOLDER}/egov-ear-*.ear ${WILDFLY_HOME}/Archive/egov-ear.ear.${dated}
+
 echo "Copying local EAR to ${DEPLOY_FOLDER}"
-cp -rp ${EARFILE} ${DEPLOY_FOLDER}/.
+cp -rp ${EARFILE} ${DEPLOY_FOLDER}/egov-ear.ear
 if [ $? -eq 0 ]
 then 
-	mv ${DEPLOY_FOLDER}/egov-ear-*.ear ${DEPLOY_FOLDER}/egov-ear.ear && touch ${DEPLOY_FOLDER}/egov-ear.ear.dodeploy
+	touch ${DEPLOY_FOLDER}/egov-ear.ear.dodeploy
+	echo "Starting wildfly ...."
+	nohup ${WILDFLY_HOME}/bin/standalone.sh -Dspring.profiles.active=production -b 0.0.0.0 > /dev/null &
 else
-	echo "Unable to copy the EAR, please check the permission or disk space error" && exit 1;
+	echo "Unable to copy the EAR, please check the permission or disk space error"
+	exit 1;
 fi
-echo "Starting wildfly ...."
-nohup ${WILDFLY_HOME}/bin/standalone.sh -Dspring.profiles.active=production -b 0.0.0.0 > /dev/null &
