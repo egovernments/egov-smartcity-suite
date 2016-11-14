ccfb03e9ee90646b16fdb371c930dbab307e8a25
sreekanth.nair <sreekanth.nair@egovernments.org>
2016-11-14 11:00:49 +0530

    [PHOENIX-5401] Removed elasticsearch package from repository scanning

--- egov/egov-egi/src/main/java/org/egov/infra/config/elasticsearch/ElasticSearchConfiguration.java
+++ egov/egov-egi/src/main/java/org/egov/infra/config/elasticsearch/ElasticSearchConfiguration.java
@@ -1,3 +1,43 @@
+/*
+ * eGov suite of products aim to improve the internal efficiency,transparency,
+ *      accountability and the service delivery of the government  organizations.
+ *
+ *       Copyright (C) 2016  eGovernments Foundation
+ *
+ *       The updated version of eGov suite of products as by eGovernments Foundation
+ *       is available at http://www.egovernments.org
+ *
+ *       This program is free software: you can redistribute it and/or modify
+ *       it under the terms of the GNU General Public License as published by
+ *       the Free Software Foundation, either version 3 of the License, or
+ *       any later version.
+ *
+ *       This program is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *       GNU General Public License for more details.
+ *
+ *       You should have received a copy of the GNU General Public License
+ *       along with this program. If not, see http://www.gnu.org/licenses/ or
+ *       http://www.gnu.org/licenses/gpl.html .
+ *
+ *       In addition to the terms of the GPL license to be adhered to in using this
+ *       program, the following additional terms are to be complied with:
+ *
+ *           1) All versions of this program, verbatim or modified must carry this
+ *              Legal Notice.
+ *
+ *           2) Any misrepresentation of the origin of the material is prohibited. It
+ *              is required that all modified versions of this material be marked in
+ *              reasonable ways as different from the original version.
+ *
+ *           3) This license does not grant any rights to any user of the program
+ *              with regards to rights under trademark law for use of the trade names
+ *              or trademarks of eGovernments Foundation.
+ *
+ *     In case of any queries, you can reach eGovernments Foundation at contact@egovernments.org.
+ */
+
 package org.egov.infra.config.elasticsearch;
 
 import org.egov.infra.config.properties.ApplicationProperties;
@@ -15,24 +55,21 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 import java.net.InetSocketAddress;
 
 @Configuration
-@EnableElasticsearchRepositories(basePackages = {"org.egov.**.repository.es", "org.egov.**.repository.elasticsearch"})
+@EnableElasticsearchRepositories(basePackages = "org.egov.**.repository.es")
 public class ElasticSearchConfiguration {
 
     @Autowired
     private ApplicationProperties applicationProperties;
 
-    public Client transportClient() {
+    private Client transportClient() {
         Settings settings = Settings.settingsBuilder()
                 .put("cluster.name", applicationProperties.searchClusterName()).build();
         Client client = TransportClient.builder().settings(settings).build();
-        applicationProperties.searchHosts().stream().forEach(host ->
-                addTransportClient(client, host, applicationProperties.searchPort()));
+        applicationProperties.searchHosts().forEach(host ->
+                ((TransportClient) client).addTransportAddress(
+                        new InetSocketTransportAddress(new InetSocketAddress(host, applicationProperties.searchPort())))
+        );
         return client;
-    }
-
-    private TransportClient addTransportClient(Client client, String host, int port) {
-        return ((TransportClient) client).addTransportAddress(
-                new InetSocketTransportAddress(new InetSocketAddress(host, port)));
     }
 
     @Bean
