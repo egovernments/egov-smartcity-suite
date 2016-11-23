3447c3c69eab85d0c95c07b2c3753ac0205e0a90
vinoth rallapalli <vinoth.r@egovernments.org>
2016-11-23 16:58:18 +0530

    PHOENIX-5632 | Options in Party Name in Council management

---
+++ egov/egov-council/src/main/resources/db/migration/main/V20161123165234__council_party_cpm_bsp_master_data.sql
@@ -0,0 +1,4 @@
+
+insert into egcncl_party(id,name,isactive,createddate,createdby,lastmodifieddate,lastmodifiedby,version,code) values(nextval('seq_egcncl_party'),'CPM',true,now(),1,now(),1,0,'CPM');
+
+insert into egcncl_party(id,name,isactive,createddate,createdby,lastmodifieddate,lastmodifiedby,version,code) values(nextval('seq_egcncl_party'),'BSP',true,now(),1,now(),1,0,'BSP');
