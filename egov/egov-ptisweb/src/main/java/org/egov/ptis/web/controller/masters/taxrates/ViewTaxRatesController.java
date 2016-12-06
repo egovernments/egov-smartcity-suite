8e498dd939f086f54a5ebc80b9c91408034226b5
Rafeek <mohammad.rafeek@egovernments.org>
2016-12-06 16:45:30 +0530

    PHOENIX-5716 : Edit Tax Rates [PTIS]

--- egov/egov-ptisweb/src/main/java/org/egov/ptis/web/controller/masters/taxrates/ViewTaxRatesController.java
+++ egov/egov-ptisweb/src/main/java/org/egov/ptis/web/controller/masters/taxrates/ViewTaxRatesController.java
@@ -39,6 +39,12 @@
  */
 package org.egov.ptis.web.controller.masters.taxrates;
 
+import java.math.BigDecimal;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
 import org.egov.infra.admin.master.entity.AppConfigValues;
 import org.egov.ptis.master.service.TaxRatesService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,20 +53,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
-import static org.egov.ptis.constants.PropertyTaxConstants.TAX_RATES;
-import static org.egov.ptis.constants.PropertyTaxConstants.TAX_RATES_TEMP;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping(value = "/taxrates/view")
+@RequestMapping(value = "/taxrates")
 public class ViewTaxRatesController {
 
     private final TaxRatesService taxRatesService;
+    private static final String TAXRATE_VIEW = "taxRates-view";
+    private static final String TAXRATE_EDIT = "taxrates-edit";
+    private static final String GENERAL_TAX_RESD = "GEN_TAX_RESD";
+    private static final String GENERAL_TAX_NONRESD = "GEN_TAX_NR";
+    private static final String EDUCATIONAL_TAX = "EDU_CESS";
+    private static final String REDIRECT_URL = "redirect:/taxrates/edit";
+    private static final String REDIRECT_VIEW_URL = "redirect:/taxrates/view";
+    private static final String MESSAGE = "message";
 
     @Autowired
     public ViewTaxRatesController(final TaxRatesService taxRatesService) {
@@ -72,28 +79,70 @@ public class ViewTaxRatesController {
         return new AppConfigValues();
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String showTaxRates(Model model) {
-        List<AppConfigValues> taxRates = taxRatesService.getAllTaxRates();
-        Map<String, Double> taxRatesMap = new HashMap<String, Double>();
-        for (AppConfigValues appConfig : taxRates) {
-            String rows[] = appConfig.getValue().split("\n");
-            for (String row : rows) {
-                String value[] = row.split("=");
-                if (!TAX_RATES_TEMP.contains(value[0])) {
-                    taxRatesMap.put(TAX_RATES.get(value[0]), Double.parseDouble(value[1]));
-                }
-            }
-
-        }
-        Map<String, Double> taxRatesDisplayMap = new LinkedHashMap<String, Double>();
-        taxRatesDisplayMap.put("General Tax Residential", taxRatesMap.get("General Tax Residential"));
-        taxRatesDisplayMap.put("General Tax Non Residential", taxRatesMap.get("General Tax Non Residential"));
-        taxRatesDisplayMap.put("Education Cess", taxRatesMap.get("Education Cess"));
-        taxRatesDisplayMap.put("Vacant Land Tax", taxRatesMap.get("Vacant Land Tax"));
-        taxRatesDisplayMap.put("Library Cess", taxRatesMap.get("Library Cess"));
-        model.addAttribute("taxRates", taxRatesDisplayMap);
-        return "taxRates-view";
+    @RequestMapping(value = "/view", method = RequestMethod.GET)
+    public String showTaxRates(final Model model) {
+        final Map<String, Double> taxRatesMap = taxRatesService.getTaxDetails();
+        model.addAttribute("taxRates", taxRatesMap);
+        return TAXRATE_VIEW;
     }
 
+    @RequestMapping(value = "/edit", method = RequestMethod.GET)
+    public String viewTaxRates(final Model model) {
+        final Map<String, Double> taxRatesMap = taxRatesService.getTaxDetails();
+        model.addAttribute("taxRates", taxRatesMap);
+        return TAXRATE_EDIT;
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    public String saveTaxRates(final Model model, final HttpServletRequest request,
+            final RedirectAttributes redirectAttributes) {
+        final String resTaxValue = request.getParameter("General Tax Residential-value");
+        final String nonResTaxValue = request.getParameter("General Tax Non Residential-value");
+        final String eduTax = request.getParameter("Education Cess-value");
+        if (StringUtils.isNotBlank(resTaxValue) && StringUtils.isNotBlank(nonResTaxValue)
+                && StringUtils.isNotBlank(eduTax) && validateTaxValues(resTaxValue, nonResTaxValue, eduTax)) {
+            final StringBuilder newTaxStr = new StringBuilder();
+            final AppConfigValues appConfigValue = taxRatesService.getAllTaxRates().get(0);
+            final String[] rows = appConfigValue.getValue().split("\n");
+            for (final String row : rows) {
+                final String[] value = row.split("=");
+                if (GENERAL_TAX_RESD.equalsIgnoreCase(value[0]))
+                    newTaxStr.append("GEN_TAX_RESD=" + resTaxValue + "\n");
+                else if (GENERAL_TAX_NONRESD.equalsIgnoreCase(value[0]))
+                    newTaxStr.append("GEN_TAX_NR=" + nonResTaxValue + "\n");
+                else if (EDUCATIONAL_TAX.equalsIgnoreCase(value[0]))
+                    newTaxStr.append("EDU_CESS=" + eduTax + "\n");
+                else
+                    newTaxStr.append(rows[rows.length - 1] == row ? value[0] + "=" + value[1]
+                            : value[0] + "=" + value[1] + "\n");
+
+            }
+            if (StringUtils.isNotBlank(newTaxStr)) {
+                appConfigValue.setValue(newTaxStr.toString());
+                taxRatesService.updateTaxRateAppconfig(appConfigValue);
+                redirectAttributes.addFlashAttribute(MESSAGE, "msg.update.taxrates");
+            }
+            return REDIRECT_VIEW_URL;
+        } else {
+            redirectAttributes.addFlashAttribute(MESSAGE, "msg.taxrate.invalid");
+            return REDIRECT_URL;
+        }
+    }
+
+    private Boolean validateTaxValues(final String newRsdTax, final String newNonRsdTax, final String newEduTax) {
+        boolean isValid = true;
+        final AppConfigValues appConfigValue = taxRatesService.getAllTaxRates().get(0);
+        final String[] rows = appConfigValue.getValue().split("\n");
+        for (final String row : rows) {
+            final String[] value = row.split("=");
+            if (GENERAL_TAX_RESD.equalsIgnoreCase(value[0])
+                    && new BigDecimal(newRsdTax).compareTo(new BigDecimal(value[1])) < 0
+                    || GENERAL_TAX_NONRESD.equalsIgnoreCase(value[0])
+                            && new BigDecimal(newNonRsdTax).compareTo(new BigDecimal(value[1])) < 0
+                    || EDUCATIONAL_TAX.equalsIgnoreCase(value[0])
+                            && new BigDecimal(newEduTax).compareTo(new BigDecimal(value[1])) < 0)
+                isValid = false;
+        }
+        return isValid;
+    }
 }
