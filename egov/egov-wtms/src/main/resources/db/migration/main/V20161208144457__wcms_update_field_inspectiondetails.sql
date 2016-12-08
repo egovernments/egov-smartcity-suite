1ab8b9603ffc5bc67daedd4e9644f8b2e09d38e7
sushant <sushant_vs@cms.co.in>
2016-12-08 14:57:11 +0530

    [PHOENIX-5730] - Script to update old feildinspection details.

---
+++ egov/egov-wtms/src/main/resources/db/migration/main/V20161208144457__wcms_update_field_inspectiondetails.sql
@@ -0,0 +1,7 @@
+update egwtr_fieldinspection_details set applicationfee =0 where applicationfee is null;
+
+update egwtr_fieldinspection_details set roadcuttingcharges=0 where roadcuttingcharges is null;
+
+update egwtr_fieldinspection_details set securitydeposit=0 where securitydeposit is null;
+
+update egwtr_fieldinspection_details set supervisioncharges=0 where supervisioncharges is null;
