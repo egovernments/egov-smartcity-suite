cfb36933ac32d7edb6f2a5adb540eb952ff43604
pradeep <pradeep@egovernments.org>
2016-12-23 20:52:22 +0530

    [PHOENIX-5343] Removed wrongly commited-Registration unit initial data file.

--- egov/egov-mrs/src/main/resources/db/migration/main/V20161020154407__mrs_registrationunit_initialdata.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO egmrs_registrationunit VALUES (nextval('SEQ_EGMRS_REGISTRATIONUNIT'), 'Municipal office', 'OOBH', 'municipal office', true, 3, true, 0, 1, current_timestamp, 1, current_timestamp);
