e013ecacac7835dd4c7cb2c4a970d1950ed2092e
jayashree.reddy@egovernments.org <jayashree.reddy@egovernments.org>
2017-01-03 17:12:06 +0530

    [PHOENIX-5922] 'UoM From' field in Create fee matrix is made readonly on load of page.

--- egov/egov-tl/src/main/java/org/egov/tl/repository/FeeMatrixDetailRepositoryImpl.java
+++ egov/egov-tl/src/main/java/org/egov/tl/repository/FeeMatrixDetailRepositoryImpl.java
@@ -49,7 +49,10 @@ import org.hibernate.criterion.Restrictions;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import javax.persistence.criteria.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import java.util.ArrayList;
 
