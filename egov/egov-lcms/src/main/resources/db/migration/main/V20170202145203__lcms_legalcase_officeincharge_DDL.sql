aefbfe21b0a13289ffeab7e9e27384b743d16f0d
Deepamulimani <deepa.mulimani@egovernments.org>
2017-02-02 17:52:47 +0530

    [PHOENIX-6143] Officer Incharge should be auto complete

--- egov/egov-lcms/src/main/resources/db/migration/main/V20170202145203__lcms_legalcase_officeincharge_DDL.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE eglc_legalcase ALTER COLUMN officerincharge TYPE bigint USING officerincharge::bigint;
-
-ALTER TABLE eglc_legalcase 
-ADD CONSTRAINT fk_legalcase_position FOREIGN KEY (officerincharge) REFERENCES eg_position (id);
