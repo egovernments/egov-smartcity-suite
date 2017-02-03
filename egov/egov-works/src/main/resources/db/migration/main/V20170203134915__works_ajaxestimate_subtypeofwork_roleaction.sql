f7bd16ed8dcc2dd12ff48d9656ac927596780e1f
nagalinga <nagalinga@nagalinga>
2017-02-03 13:58:55 +0530

    [PHOENIX-6189] added subtype of work roleaction to works masters creator

---
+++ egov/egov-works/src/main/resources/db/migration/main/V20170203134915__works_ajaxestimate_subtypeofwork_roleaction.sql
@@ -0,0 +1,3 @@
+Insert into eg_roleaction (roleid, actionid) values ((select id from eg_role where name = 'Works Masters Creator'),(select id from eg_action where name ='WorksEstimateSubTypeAjax' and contextroot = 'egworks'));
+
+--rollback delete from eg_roleaction where roleid = (select id from eg_role where name = 'Works Masters Creator') and actionid in(select id from eg_action where name in('WorksEstimateSubTypeAjax') and contextroot = 'egworks');
