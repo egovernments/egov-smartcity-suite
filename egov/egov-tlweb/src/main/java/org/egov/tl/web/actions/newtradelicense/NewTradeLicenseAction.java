19f0d6c74761ed6159417ee1fb9bcb4e114b8c1b
jayashree <jayashree.reddy@egovernments.org>
2017-02-13 15:40:17 +0530

    [PHOENIX-6259]  Unable to Renew the licenses which are cancelled before collection

--- egov/egov-tlweb/src/main/java/org/egov/tl/web/actions/newtradelicense/NewTradeLicenseAction.java
+++ egov/egov-tlweb/src/main/java/org/egov/tl/web/actions/newtradelicense/NewTradeLicenseAction.java
@@ -252,7 +252,7 @@ public class NewTradeLicenseAction extends BaseLicenseAction<TradeLicense> {
     public String beforeRenew() throws IOException {
         prepareNewForm();
         setDocumentTypes(tradeLicenseService.getDocumentTypesByApplicationType(ApplicationType.NEW));
-        if (!tradeLicense.stateIsEnded()) {
+        if (tradeLicense.hasState() && !tradeLicense.stateIsEnded()) {
             ServletActionContext.getResponse().setContentType("text/html");
             ServletActionContext.getResponse().getWriter()
                     .write("<center style='color:red;font-weight:bolder'>Renewal workflow is in progress !</center>");
