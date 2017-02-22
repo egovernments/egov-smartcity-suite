9cd97c4e7a23b8d3cd5273da5d483545218d71e3
pratik <pratik.kumar@egovernments.org>
2017-02-22 13:10:23 +0530

    [PHOENIX-6215] fixed issue for date of marriage.

--- egov/egov-mrsweb/src/main/webapp/resources/js/app/registrationformvalidation.js
+++ egov/egov-mrsweb/src/main/webapp/resources/js/app/registrationformvalidation.js
@@ -81,15 +81,14 @@ $( "#select-venue" ).change(function() {
 
 
 $('#txt-dateOfMarriage').datepicker()
-    .blur('changeDate', function(e) {
+    .on('changeDate', function(e) {
     	
-    	var str=$('#txt-dateOfMarriage').val().toString(); 
-    	
+    	var str=$('#txt-dateOfMarriage').val(); 
     	if(str.match(/(^(((0[1-9]|1[0-9]|2[0-8])[\/](0[1-9]|1[012]))|((29|30|31)[\/](0[13578]|1[02]))|((29|30)[\/](0[4,6,9]|11)))[\/](19|[2-9][0-9])\d\d$)|(^29[\/]02[\/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)/g))
     	{
     		showMarriageFee();
     	}
-    	else{
+    	else if(str){
     		bootbox.alert('Invalid date!');
     		$('#txt-dateOfMarriage').val('');
     	}
