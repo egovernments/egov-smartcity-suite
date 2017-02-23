5086ac1886e2c4093ec38c96dc8a66e6dcbf697d
Sathish P <sathish.p@egovernments.org>
2017-02-23 15:26:49 +0530

    [PHOENIX-6301] - Rest API for Contractor masters - code refactored

--- egov/egov-restapi/src/main/java/org/egov/restapi/web/rest/CreateContractorController.java
+++ egov/egov-restapi/src/main/java/org/egov/restapi/web/rest/CreateContractorController.java
@@ -67,9 +67,9 @@ import org.egov.works.models.masters.Contractor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -82,8 +82,8 @@ public class CreateContractorController {
     @Autowired
     private ExternalContractorService externalContractorService;
 
-    @RequestMapping(value = "/egworks/contractor/{code}", method = GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public String getContractorByCode(@PathVariable("code") final String code) {
+    @RequestMapping(value = "/egworks/contractor", method = GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getContractorByCode(@RequestParam("code") final String code) {
         final ErrorDetails errorDetails = new ErrorDetails();
         if (StringUtils.isBlank(code)) {
             errorDetails.setErrorCode(RestApiConstants.THIRD_PARTY_ERR_CODE_NO_JSON_REQUEST);
