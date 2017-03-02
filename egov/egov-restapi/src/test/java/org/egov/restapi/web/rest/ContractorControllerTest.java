227e6bb94d2df99f7c2686dd62a2d6be5540b437
riteshchikatwar <ritesh.chikatwar@egovernments.org>
2017-03-02 16:24:35 +0530

    [PHOENIX-6301] - fix for codacy issue

--- egov/egov-restapi/src/test/java/org/egov/restapi/web/rest/ContractorControllerTest.java
+++ egov/egov-restapi/src/test/java/org/egov/restapi/web/rest/ContractorControllerTest.java
@@ -1,3 +1,42 @@
+/*
+ * eGov suite of products aim to improve the internal efficiency,transparency,
+ *    accountability and the service delivery of the government  organizations.
+ *
+ *     Copyright (C) <2015>  eGovernments Foundation
+ *
+ *     The updated version of eGov suite of products as by eGovernments Foundation
+ *     is available at http://www.egovernments.org
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see http://www.gnu.org/licenses/ or
+ *     http://www.gnu.org/licenses/gpl.html .
+ *
+ *     In addition to the terms of the GPL license to be adhered to in using this
+ *     program, the following additional terms are to be complied with:
+ *
+ *         1) All versions of this program, verbatim or modified must carry this
+ *            Legal Notice.
+ *
+ *         2) Any misrepresentation of the origin of the material is prohibited. It
+ *            is required that all modified versions of this material be marked in
+ *            reasonable ways as different from the original version.
+ *
+ *         3) This license does not grant any rights to any user of the program
+ *            with regards to rights under trademark law for use of the trade names
+ *            or trademarks of eGovernments Foundation.
+ *
+ *   In case of any queries, you can reach eGovernments Foundation at contact@egovernments.org.
+ */
 package org.egov.restapi.web.rest;
 
 import static org.junit.Assert.assertEquals;
@@ -10,6 +49,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.io.IOUtils;
+import org.egov.infra.utils.ApplicationConstant;
 import org.egov.restapi.constants.RestApiConstants;
 import org.egov.restapi.model.ContractorHelper;
 import org.egov.restapi.model.RestErrors;
@@ -31,8 +71,6 @@ public class ContractorControllerTest extends AbstractContextControllerTest<Cont
     private ContractorController contractorController;
 
     private Contractor contractor;
-
-    private ContractorHelper contractorHelper;
 
     private List<RestErrors> errors;
 
@@ -67,7 +105,8 @@ public class ContractorControllerTest extends AbstractContextControllerTest<Cont
     @Test
     public void shouldCreateContractor() throws IOException {
         requestJson = IOUtils.toString(
-                Thread.currentThread().getContextClassLoader().getResourceAsStream("createcontractor-Json.json"), "UTF-8");
+                Thread.currentThread().getContextClassLoader().getResourceAsStream("createcontractor-Json.json"),
+                ApplicationConstant.DEFAULT_CHARACTER_ENCODING);
         when(externalContractorService.validateContactorToCreate(Matchers.anyObject())).thenReturn(errors);
         when(externalContractorService.populateContractorToCreate(Matchers.anyObject())).thenReturn(contractor);
         when(externalContractorService.saveContractor(Matchers.anyObject())).thenReturn(contractor);
@@ -80,7 +119,8 @@ public class ContractorControllerTest extends AbstractContextControllerTest<Cont
     @Test
     public void shouldValidateIsCodeAndNameEmpty() throws IOException {
         requestJson = IOUtils.toString(
-                Thread.currentThread().getContextClassLoader().getResourceAsStream("createcontractorempty-Json.json"), "UTF-8");
+                Thread.currentThread().getContextClassLoader().getResourceAsStream("createcontractorempty-Json.json"),
+                ApplicationConstant.DEFAULT_CHARACTER_ENCODING);
         when(externalContractorService.validateContactorToCreate(Matchers.anyObject())).thenReturn(errors);
         when(externalContractorService.populateContractorToCreate(Matchers.anyObject())).thenReturn(contractor);
         when(externalContractorService.saveContractor(Matchers.anyObject())).thenReturn(contractor);
@@ -94,7 +134,8 @@ public class ContractorControllerTest extends AbstractContextControllerTest<Cont
     @Test
     public void shouldValidateGetContractor() throws IOException {
         requestJson = IOUtils.toString(
-                Thread.currentThread().getContextClassLoader().getResourceAsStream("createcontractor-Json.json"), "UTF-8");
+                Thread.currentThread().getContextClassLoader().getResourceAsStream("createcontractor-Json.json"),
+                ApplicationConstant.DEFAULT_CHARACTER_ENCODING);
         when(contractorService.getContractorByCode(Matchers.anyString())).thenReturn(null);
         responseJson = contractorController.getContractors(requestJson);
         final JSONObject jsonObject = new JSONObject(responseJson);
@@ -104,11 +145,12 @@ public class ContractorControllerTest extends AbstractContextControllerTest<Cont
 
     @Test
     public void shouldValidateGetContractorForCode() throws IOException {
-        contractorHelper = new ContractorHelper();
+        final ContractorHelper contractorHelper = new ContractorHelper();
         contractorHelper.setCode("JUNITCODE");
         contractorHelper.setName("JUNITNAME");
         requestJson = IOUtils.toString(
-                Thread.currentThread().getContextClassLoader().getResourceAsStream("createcontractor-Json.json"), "UTF-8");
+                Thread.currentThread().getContextClassLoader().getResourceAsStream("createcontractor-Json.json"),
+                ApplicationConstant.DEFAULT_CHARACTER_ENCODING);
         when(externalContractorService.populateContractorData(Matchers.anyObject())).thenReturn(contractorHelper);
         when(contractorService.getContractorByCode(Matchers.anyString())).thenReturn(contractor);
         responseJson = contractorController.getContractors(requestJson);
@@ -119,7 +161,8 @@ public class ContractorControllerTest extends AbstractContextControllerTest<Cont
     @Test
     public void shouldModifyContractor() throws IOException {
         requestJson = IOUtils.toString(
-                Thread.currentThread().getContextClassLoader().getResourceAsStream("createcontractor-Json.json"), "UTF-8");
+                Thread.currentThread().getContextClassLoader().getResourceAsStream("createcontractor-Json.json"),
+                ApplicationConstant.DEFAULT_CHARACTER_ENCODING);
         when(externalContractorService.validateContactorToUpdate(Matchers.anyObject())).thenReturn(errors);
         when(externalContractorService.populateContractorToUpdate(Matchers.anyObject())).thenReturn(contractor);
         when(externalContractorService.updateContractor(Matchers.anyObject())).thenReturn(contractor);
@@ -132,7 +175,8 @@ public class ContractorControllerTest extends AbstractContextControllerTest<Cont
     @Test
     public void shouldValidateModifyContractorIsCodeAndNameEmpty() throws IOException {
         requestJson = IOUtils.toString(
-                Thread.currentThread().getContextClassLoader().getResourceAsStream("createcontractorempty-Json.json"), "UTF-8");
+                Thread.currentThread().getContextClassLoader().getResourceAsStream("createcontractorempty-Json.json"),
+                ApplicationConstant.DEFAULT_CHARACTER_ENCODING);
         when(externalContractorService.validateContactorToUpdate(Matchers.anyObject())).thenReturn(errors);
         when(externalContractorService.populateContractorToUpdate(Matchers.anyObject())).thenReturn(contractor);
         when(externalContractorService.updateContractor(Matchers.anyObject())).thenReturn(contractor);
