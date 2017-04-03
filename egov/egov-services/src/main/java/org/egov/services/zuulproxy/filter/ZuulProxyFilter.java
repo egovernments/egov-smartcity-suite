ac65579ccbb3a1763f6f8ecf361358eed840a51b
Sathish P <sathish.p@egovernments.org>
2017-04-03 17:47:40 +0530

    [PHOENIX-6463] Moving UserInfo logic from header to RequestInfo for POST methods

--- egov/egov-services/src/main/java/org/egov/services/zuulproxy/filter/ZuulProxyFilter.java
+++ egov/egov-services/src/main/java/org/egov/services/zuulproxy/filter/ZuulProxyFilter.java
@@ -39,16 +39,19 @@
  */
 package org.egov.services.zuulproxy.filter;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.Objects;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.egov.infra.admin.master.entity.User;
 import org.egov.infra.admin.master.service.UserService;
@@ -56,6 +59,7 @@ import org.egov.infra.config.core.ApplicationThreadLocals;
 import org.egov.infra.config.security.authentication.SecureUser;
 import org.egov.infra.exception.ApplicationRuntimeException;
 import org.egov.services.config.properties.ServicesApplicationProperties;
+import org.egov.services.wrapper.CustomRequestWrapper;
 import org.egov.services.zuulproxy.models.Role;
 import org.egov.services.zuulproxy.models.UserInfo;
 import org.slf4j.Logger;
@@ -67,6 +71,7 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
@@ -74,8 +79,13 @@ import com.netflix.zuul.context.RequestContext;
 public class ZuulProxyFilter extends ZuulFilter {
 
     private static Logger log = LoggerFactory.getLogger(ZuulProxyFilter.class);
-    private static final String USER_INFO = "x-user-info";
+    // private static final String USER_INFO = "x-user-info";
     private static final String CLIENT_ID = "client.id";
+    private static final String GET = "GET";
+    private static final String FILESTORE_REGEX = "^/filestore/.*";
+    private static final String REQUEST_INFO_FIELD_NAME = "RequestInfo";
+    private static final String USER_INFO_FIELD_NAME = "userInfo";
+    private ObjectMapper mapper = null;
 
     @Override
     public String filterType() {
@@ -97,6 +107,7 @@ public class ZuulProxyFilter extends ZuulFilter {
     @Override
     public Object run() {
 
+        mapper = new ObjectMapper();
         final RequestContext ctx = RequestContext.getCurrentContext();
         final HttpServletRequest request = ctx.getRequest();
 
@@ -140,24 +151,52 @@ public class ZuulProxyFilter extends ZuulFilter {
             final URL routedHost = new URL(mappingURL + endPointURI);
             ctx.setRouteHost(routedHost);
             ctx.set("requestURI", routedHost.getPath());
-            ctx.addZuulRequestHeader(USER_INFO, getUserInfo(request, springContext));
+            final String userInfo = getUserInfo(request, springContext);
+            if (shouldPutUserInfoOnHeaders(ctx))
+                ctx.addZuulRequestHeader(USER_INFO_FIELD_NAME, userInfo);
+            else
+                appendUserInfoToRequestBody(ctx, userInfo);
         } catch (final MalformedURLException e) {
             throw new ApplicationRuntimeException("Could not form valid URL", e);
+        } catch (final IOException ex) {
+            ctx.setSendZuulResponse(false);
         }
         return null;
     }
 
+    private boolean shouldPutUserInfoOnHeaders(final RequestContext ctx) {
+        return Objects.equals(ctx.getRequest().getMethod().toUpperCase(), GET) ||
+                ctx.getRequest().getRequestURI().matches(FILESTORE_REGEX);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void appendUserInfoToRequestBody(final RequestContext ctx, final String userInfo) throws IOException {
+        final HashMap<String, Object> requestBody = getRequestBody(ctx);
+        final HashMap<String, Object> requestInfo = (HashMap<String, Object>) requestBody.get(REQUEST_INFO_FIELD_NAME);
+        requestInfo.put(USER_INFO_FIELD_NAME, userInfo);
+        requestBody.put(REQUEST_INFO_FIELD_NAME, requestInfo);
+        final CustomRequestWrapper requestWrapper = new CustomRequestWrapper(ctx.getRequest());
+        requestWrapper.setPayload(mapper.writeValueAsString(requestBody));
+        ctx.setRequest(requestWrapper);
+    }
+
+    private HashMap<String, Object> getRequestBody(final RequestContext ctx) throws IOException {
+        final String payload = IOUtils.toString(ctx.getRequest().getInputStream());
+        return mapper.readValue(payload, new TypeReference<HashMap<String, Object>>() {
+        });
+    }
+
     private String getUserInfo(final HttpServletRequest request, final WebApplicationContext springContext) {
         final HttpSession session = request.getSession();
-        String userInfoJson = "";
+        String userInfoJson = null;
 
-        if (session.getAttribute(USER_INFO) != null)
-            userInfoJson = session.getAttribute(USER_INFO).toString();
+        if (session.getAttribute(USER_INFO_FIELD_NAME) != null)
+            userInfoJson = session.getAttribute(USER_INFO_FIELD_NAME).toString();
 
         if (log.isInfoEnabled())
-            log.info("x-user-info is from the session... " + userInfoJson);
+            log.info("userInfo is from the session... " + userInfoJson);
 
-        if (org.apache.commons.lang.StringUtils.isBlank(userInfoJson)) {
+        if (StringUtils.isBlank(userInfoJson)) {
             final UserService userService = (UserService) springContext.getBean("userService");
             final SecureUser userDetails = new SecureUser(
                     userService.getUserByUsername(request.getRemoteUser()));
@@ -184,8 +223,8 @@ public class ZuulProxyFilter extends ZuulFilter {
                 throw new ApplicationRuntimeException("Could not convert object to json string", e);
             }
             if (log.isInfoEnabled())
-                log.info("Read x-user-info from the DB and set it to the session... " + userInfoJson);
-            session.setAttribute(USER_INFO, userInfoJson);
+                log.info("Read userInfo from the DB and set it to the session... " + userInfoJson);
+            session.setAttribute(USER_INFO_FIELD_NAME, userInfoJson);
         }
 
         return userInfoJson;
