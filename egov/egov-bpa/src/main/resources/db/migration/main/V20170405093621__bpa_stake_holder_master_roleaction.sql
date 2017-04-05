d098ce8ba01d3b36e9e84b6b2be981e156e06fc8
pradeep <pradeep@egovernments.org>
2017-04-05 13:44:26 +0530

    [PHOENIX-6461] Master data and configuration values added.

--- egov/egov-bpa/src/main/resources/db/migration/main/V20170405093621__bpa_stake_holder_master_roleaction.sql
+++ egov/egov-bpa/src/main/resources/db/migration/main/V20170405093621__bpa_stake_holder_master_roleaction.sql
@@ -1,7 +1,7 @@
 
 INSERT INTO eg_module(
             id, name, enabled, contextroot, parentmodule, displayname, ordernumber)
-    VALUES (nextval('SEQ_EG_MODULE'), 'BPA Master', true, 'bpa', (select id from eg_module where name='BPA'), 'Masters', 1);
+    VALUES (nextval('SEQ_EG_MODULE'), 'BPA Master', true, 'bpa', (select id from eg_module where name='BPA'  and parentmodule is null), 'Masters', 1);
 
 INSERT INTO eg_module(
             id, name, enabled, contextroot, parentmodule, displayname, ordernumber)
@@ -9,22 +9,22 @@ INSERT INTO eg_module(
 
     -----------------------------role action mapping start----------
     
-Insert into eg_action(id,name,url,parentmodule,ordernumber,displayname,enabled,contextroot,application) values(nextval('SEQ_EG_ACTION'),'Create StakeHolder','/stakeholder/create',(select id from eg_module where name='Stake Holder Master' and parentmodule=(select id from eg_module where name='BPA' and parentmodule is null)),1,'Create StakeHolder',true,'bpa',(select id from eg_module where name='BPA' and parentmodule is null));
+Insert into eg_action(id,name,url,parentmodule,ordernumber,displayname,enabled,contextroot,application) values(nextval('SEQ_EG_ACTION'),'Create StakeHolder','/stakeholder/create',(select id from eg_module where name='Stake Holder Master' and parentmodule=(select id from eg_module where name='BPA Master')),1,'Create StakeHolder',true,'bpa',(select id from eg_module where name='BPA' and parentmodule is null));
 Insert into eg_roleaction values((select id from eg_role where name='Super User'),(select id from eg_action where name='Create StakeHolder'));
 
-Insert into eg_action(id,name,url,parentmodule,ordernumber,displayname,enabled,contextroot,application) values(nextval('SEQ_EG_ACTION'),'search and show update StakeHolder result','/stakeholder/search/update',(select id from eg_module where name='Stake Holder Master' and parentmodule=(select id from eg_module where name='BPA' and parentmodule is null)),2,'Update StakeHolder',true,'bpa',(select id from eg_module where name='BPA' and parentmodule is null));
+Insert into eg_action(id,name,url,parentmodule,ordernumber,displayname,enabled,contextroot,application) values(nextval('SEQ_EG_ACTION'),'search and show update StakeHolder result','/stakeholder/search/update',(select id from eg_module where name='Stake Holder Master' and parentmodule=(select id from eg_module where name='BPA Master')),2,'Update StakeHolder',true,'bpa',(select id from eg_module where name='BPA' and parentmodule is null));
 Insert into eg_roleaction values((select id from eg_role where name='Super User'),(select id from eg_action where name='search and show update StakeHolder result'));
 
-Insert into eg_action(id,name,url,parentmodule,ordernumber,displayname,enabled,contextroot,application) values(nextval('SEQ_EG_ACTION'),'edit and update StakeHolder','/stakeholder/update',(select id from eg_module where name='Stake Holder Master' and parentmodule=(select id from eg_module where name='BPA' and parentmodule is null)),2,'edit and update StakeHolder',false,'bpa',(select id from eg_module where name='BPA' and parentmodule is null));
+Insert into eg_action(id,name,url,parentmodule,ordernumber,displayname,enabled,contextroot,application) values(nextval('SEQ_EG_ACTION'),'edit and update StakeHolder','/stakeholder/update',(select id from eg_module where name='Stake Holder Master' and parentmodule=(select id from eg_module where name='BPA Master')),2,'edit and update StakeHolder',false,'bpa',(select id from eg_module where name='BPA' and parentmodule is null));
 Insert into eg_roleaction values((select id from eg_role where name='Super User'),(select id from eg_action where name='edit and update StakeHolder'));
 
-Insert into eg_action(id,name,url,parentmodule,ordernumber,displayname,enabled,contextroot,application) values(nextval('SEQ_EG_ACTION'),'search and show view StakeHolder result','/stakeholder/search/view',(select id from eg_module where name='Stake Holder Master' and parentmodule=(select id from eg_module where name='BPA' and parentmodule is null)),3,'View StakeHolder',true,'bpa',(select id from eg_module where name='BPA' and parentmodule is null));
+Insert into eg_action(id,name,url,parentmodule,ordernumber,displayname,enabled,contextroot,application) values(nextval('SEQ_EG_ACTION'),'search and show view StakeHolder result','/stakeholder/search/view',(select id from eg_module where name='Stake Holder Master' and parentmodule=(select id from eg_module where name='BPA Master')),3,'View StakeHolder',true,'bpa',(select id from eg_module where name='BPA' and parentmodule is null));
 Insert into eg_roleaction values((select id from eg_role where name='Super User'),(select id from eg_action where name='search and show view StakeHolder result'));
 
-Insert into eg_action(id,name,url,parentmodule,ordernumber,displayname,enabled,contextroot,application) values(nextval('SEQ_EG_ACTION'),'view StakeHolder details','/stakeholder/view',(select id from eg_module where name='Stake Holder Master' and parentmodule=(select id from eg_module where name='BPA' and parentmodule is null)),3,'view StakeHolder details',false,'bpa',(select id from eg_module where name='BPA' and parentmodule is null));
+Insert into eg_action(id,name,url,parentmodule,ordernumber,displayname,enabled,contextroot,application) values(nextval('SEQ_EG_ACTION'),'view StakeHolder details','/stakeholder/view',(select id from eg_module where name='Stake Holder Master' and parentmodule=(select id from eg_module where name='BPA Master')),3,'view StakeHolder details',false,'bpa',(select id from eg_module where name='BPA' and parentmodule is null));
 Insert into eg_roleaction values((select id from eg_role where name='Super User'),(select id from eg_action where name='view StakeHolder details'));
 
-Insert into eg_action(id,name,url,parentmodule,ordernumber,displayname,enabled,contextroot,application) values(nextval('SEQ_EG_ACTION'),'StakeHolder success details','/stakeholder/result',(select id from eg_module where name='Stake Holder Master' and parentmodule=(select id from eg_module where name='BPA' and parentmodule is null)),3,'StakeHolder success details',false,'bpa',(select id from eg_module where name='BPA' and parentmodule is null));
+Insert into eg_action(id,name,url,parentmodule,ordernumber,displayname,enabled,contextroot,application) values(nextval('SEQ_EG_ACTION'),'StakeHolder success details','/stakeholder/result',(select id from eg_module where name='Stake Holder Master' and parentmodule=(select id from eg_module where name='BPA Master')),3,'StakeHolder success details',false,'bpa',(select id from eg_module where name='BPA' and parentmodule is null));
 Insert into eg_roleaction values((select id from eg_role where name='Super User'),(select id from eg_action where name='StakeHolder success details'));
 
 -----------------------------role action mapping end-------------
