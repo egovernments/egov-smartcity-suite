578b875f7f4ee5549f51ea8b33c0c02c11236107
dushyant <dushyant.pareek@egovernments.org>
2017-04-07 16:41:34 +0530

    [PHOENIX-6532] Corrected logic

--- egov/egov-ptisweb/src/main/java/org/egov/ptis/web/controller/transactions/exemption/TaxExemptionController.java
+++ egov/egov-ptisweb/src/main/java/org/egov/ptis/web/controller/transactions/exemption/TaxExemptionController.java
@@ -325,11 +325,11 @@ public class TaxExemptionController extends GenericWorkFlowController {
                 workFlowAction = request.getParameter("workFlowAction");
             if (request.getParameter("approvalPosition") != null && !request.getParameter("approvalPosition").isEmpty())
                 approvalPosition = Long.valueOf(request.getParameter("approvalPosition"));
-            if (checkCommercialProperty((PropertyImpl)property) && property.getTaxExemptedReason() != null) {
+            if (property.getTaxExemptedReason() != null && checkCommercialProperty((PropertyImpl)property)) {
                 model.addAttribute(ERROR_MSG, "error.commercial.prop.notallowed");
                 return PROPERTY_VALIDATION;
             }
-            if (hasTenant((PropertyImpl)property) && property.getTaxExemptedReason() != null) {
+            if (property.getTaxExemptedReason() != null && hasTenant((PropertyImpl)property)) {
                 model.addAttribute(ERROR_MSG, "error.tenant.exists");
                 return PROPERTY_VALIDATION;
             }
