a3eb8ef641cbe9d969fe1bacac20b0f6554d39c8
dushyant <dushyant.pareek@egovernments.org>
2017-04-12 18:52:48 +0530

    [PHOENIX-6459] Deleting the client specific script.

--- egov/egov-ptis/src/main/resources/db/migration/main/V20170412182653__ptis_thirdparty_roleaction_for_doctype.sql
+++
@@ -1,5 +0,0 @@
-insert into eg_roleaction (actionid, roleid) select (select id from eg_action where name = 'Populate Default User For Doctype'),
-id from eg_role where name in ('CSC Operator');
-
-insert into eg_roleaction (actionid, roleid) select (select id from eg_action where name = 'Populate Default User For Doctype'),
-id from eg_role where name in ('MeeSeva Operator');
