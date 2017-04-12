60e6cb1fe03deefccab9b37402475ac6cd784266
Iffath <iffath@egovernments.org>
2017-04-12 18:53:29 +0530

    [PHOENIX-6527] : Adding logger and license text

--- egov/egov-collection/src/main/java/org/egov/collection/utils/CollectionsUtil.java
+++ egov/egov-collection/src/main/java/org/egov/collection/utils/CollectionsUtil.java
@@ -777,6 +777,7 @@ public class CollectionsUtil {
         billReceiptInfoReq.setBillReceiptInfo(billReceipt);
         billReceiptInfoReq.setRequestInfo(requestInfo);
         LOGGER.info("updateReceiptDetailsAndGetReceiptAmountInfo - billReceipt" + billReceipt);
+        LOGGER.info("updateReceiptDetailsAndGetReceiptAmountInfo - billReceipt" + billReceipt.getAccountDetails());
         LOGGER.info("updateReceiptDetailsAndGetReceiptAmountInfo - before calling LAMS update");
         String url = collectionApplicationProperties.getLamsServiceUrl().concat(
                 collectionApplicationProperties.getUpdateDemandUrl(serviceCode.toLowerCase()));
