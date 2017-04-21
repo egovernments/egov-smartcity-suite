9ba664e64c76edec134d2800d04c70a0bc307dc6
venki <venki@egovernments.org>
2017-04-21 18:34:41 +0530

    Fix for issue while calling lams service.

--- egov/egov-collection/src/main/java/org/egov/collection/integration/models/RequestInfo.java
+++ egov/egov-collection/src/main/java/org/egov/collection/integration/models/RequestInfo.java
@@ -39,96 +39,102 @@
  */
 package org.egov.collection.integration.models;
 
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class RequestInfo {
 
-    private String apiId;
-    private String ver;
-    private String ts;
-    private String action;
-    private String did;
-    private String key;
-    private String msgId;
-    private String requesterId;
-    private String authToken;
-    private User userInfo;
+	private String apiId;
+	private String ver;
+	@JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss", timezone = "IST")
+	private Date ts;
+	private String action;
+	private String did;
+	private String key;
+	private String msgId;
+	private String requesterId;
+	private String authToken;
+	private User userInfo;
 
-    public String getApiId() {
-        return apiId;
-    }
+	public String getApiId() {
+		return apiId;
+	}
 
-    public void setApiId(final String apiId) {
-        this.apiId = apiId;
-    }
+	public void setApiId(final String apiId) {
+		this.apiId = apiId;
+	}
 
-    public String getVer() {
-        return ver;
-    }
+	public String getVer() {
+		return ver;
+	}
 
-    public void setVer(final String ver) {
-        this.ver = ver;
-    }
+	public void setVer(final String ver) {
+		this.ver = ver;
+	}
 
-    public String getTs() {
-        return ts;
-    }
 
-    public void setTs(final String ts) {
-        this.ts = ts;
-    }
+	public Date getTs() {
+		return ts;
+	}
 
-    public String getAction() {
-        return action;
-    }
+	public void setTs(Date ts) {
+		this.ts = ts;
+	}
 
-    public void setAction(final String action) {
-        this.action = action;
-    }
+	public String getAction() {
+		return action;
+	}
 
-    public String getDid() {
-        return did;
-    }
+	public void setAction(final String action) {
+		this.action = action;
+	}
 
-    public void setDid(final String did) {
-        this.did = did;
-    }
+	public String getDid() {
+		return did;
+	}
 
-    public String getKey() {
-        return key;
-    }
+	public void setDid(final String did) {
+		this.did = did;
+	}
 
-    public void setKey(final String key) {
-        this.key = key;
-    }
+	public String getKey() {
+		return key;
+	}
 
-    public String getMsgId() {
-        return msgId;
-    }
+	public void setKey(final String key) {
+		this.key = key;
+	}
 
-    public void setMsgId(final String msgId) {
-        this.msgId = msgId;
-    }
+	public String getMsgId() {
+		return msgId;
+	}
 
-    public String getRequesterId() {
-        return requesterId;
-    }
+	public void setMsgId(final String msgId) {
+		this.msgId = msgId;
+	}
 
-    public void setRequesterId(final String requesterId) {
-        this.requesterId = requesterId;
-    }
+	public String getRequesterId() {
+		return requesterId;
+	}
 
-    public String getAuthToken() {
-        return authToken;
-    }
+	public void setRequesterId(final String requesterId) {
+		this.requesterId = requesterId;
+	}
 
-    public void setAuthToken(final String authToken) {
-        this.authToken = authToken;
-    }
+	public String getAuthToken() {
+		return authToken;
+	}
 
-    public User getUserInfo() {
-        return userInfo;
-    }
+	public void setAuthToken(final String authToken) {
+		this.authToken = authToken;
+	}
 
-    public void setUserInfo(User userInfo) {
-        this.userInfo = userInfo;
-    }
+	public User getUserInfo() {
+		return userInfo;
+	}
+
+	public void setUserInfo(User userInfo) {
+		this.userInfo = userInfo;
+	}
 }
