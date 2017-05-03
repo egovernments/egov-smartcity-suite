99d397943b3e8aaf47ca14a2c7b85d109875392e
Kamlesh Pratap <kamlesh.pratap@egovernments.org>
2017-05-03 12:46:36 +0530

    [PHOENIX-6695]- Checking all assignments instead of primary for wfInitiator

--- egov/egov-ptis/src/main/java/org/egov/ptis/domain/service/revisionPetition/RevisionPetitionService.java
+++ egov/egov-ptis/src/main/java/org/egov/ptis/domain/service/revisionPetition/RevisionPetitionService.java
@@ -360,14 +360,14 @@ public class RevisionPetitionService extends PersistenceService<RevisionPetition
                     wfInitiator = propertyTaxCommonUtils.getUserAssignmentByPassingPositionAndUser(objection
                     .getCreatedBy(),objection.getState().getInitiatorPosition());
                 else 
-                    wfInitiator = assignmentService.getPrimaryAssignmentForUser(objection.getCreatedBy().getId());
+                	wfInitiator = assignmentService.getAllActiveEmployeeAssignmentsByEmpId(objection.getCreatedBy().getId()).get(0);
         }
         else if (!objection.getStateHistory().isEmpty())
-            wfInitiator = assignmentService.getPrimaryAssignmentForPositon(objection.getStateHistory().get(0)
-                    .getOwnerPosition().getId());
+            wfInitiator = assignmentService.getAssignmentsForPosition(objection.getStateHistory().get(0)
+                    .getOwnerPosition().getId(), new Date()).get(0);
         else{
-            wfInitiator = assignmentService.getPrimaryAssignmentForPositon(objection.getState().getOwnerPosition()
-                    .getId());
+            wfInitiator = assignmentService.getAssignmentsForPosition(objection.getState().getOwnerPosition()
+                    .getId(), new Date()).get(0);
         }
         return wfInitiator;
     }
