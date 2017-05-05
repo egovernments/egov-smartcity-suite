10e20387edafbd26888c2c0fea5d196e32b5cceb
Sathish P <sathish.p@egovernments.org>
2017-05-06 05:22:14 +0530

    [PHOENIX-6664]  adding query params in request context

--- egov/egov-services/src/main/java/org/egov/services/wrapper/CustomHttpServletRequestWrapper.java
+++ egov/egov-services/src/main/java/org/egov/services/wrapper/CustomHttpServletRequestWrapper.java
@@ -39,54 +39,81 @@
  */
 package org.egov.services.wrapper;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.io.IOUtils;
-
 import com.netflix.zuul.http.HttpServletRequestWrapper;
-import com.netflix.zuul.http.ServletInputStreamWrapper;
 
-public class CustomRequestWrapper extends HttpServletRequestWrapper {
+public class CustomHttpServletRequestWrapper extends HttpServletRequestWrapper {
+    
+    private HttpServletRequest request;
 
-    private String payload;
+    private Map<String, String[]> parameterMap;
 
-    public CustomRequestWrapper(final HttpServletRequest request) {
+
+    public CustomHttpServletRequestWrapper(final HttpServletRequest request) {
         super(request);
-        convertInputStreamToString(request);
+        this.request = request;
     }
 
-    private void convertInputStreamToString(final HttpServletRequest request) {
-        try {
-            payload = IOUtils.toString(request.getInputStream());
-        } catch (final IOException e) {
-            throw new RuntimeException(e);
+    public void addParameter(String name, String value) {
+        if (parameterMap == null) {
+                parameterMap = new HashMap<String, String[]>();
+                parameterMap.putAll(request.getParameterMap());
         }
+        String[] values = parameterMap.get(name);
+        if (values == null) {
+                values = new String[0];
+        }
+        List<String> list = new ArrayList<String>(values.length + 1);
+        //list.addAll(Arrays.asList(values));
+        list.add(value);
+        parameterMap.put(name, list.toArray(new String[0]));
     }
+    
+    @Override
+    public String getParameter(String name) {
+            if (parameterMap == null) {
+                    return request.getParameter(name);
+            }
 
-    public String getPayload() {
-        return payload;
-    }
-
-    public void setPayload(final String payload) {
-        this.payload = payload;
+            String[] strings = parameterMap.get(name);
+            if (strings != null) {
+                    return strings[0];
+            }
+            return null;
     }
 
     @Override
-    public int getContentLength() {
-        return payload.length();
+    public Map<String, String[]> getParameterMap() {
+            if (parameterMap == null) {
+                    return request.getParameterMap();
+            }
+
+            return Collections.unmodifiableMap(parameterMap);
     }
 
     @Override
-    public long getContentLengthLong() {
-        return payload.length();
+    public Enumeration<String> getParameterNames() {
+            if (parameterMap == null) {
+                    return request.getParameterNames();
+            }
+
+            return Collections.enumeration(parameterMap.keySet());
     }
 
     @Override
-    public ServletInputStream getInputStream() {
-        return new ServletInputStreamWrapper(payload.getBytes());
+    public String[] getParameterValues(String name) {
+            if (parameterMap == null) {
+                    return request.getParameterValues(name);
+            }
+            return parameterMap.get(name);
     }
 
 }
