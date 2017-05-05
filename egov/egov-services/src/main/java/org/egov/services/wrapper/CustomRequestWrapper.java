6902f241a60f5c4ceeb8def0daccd22e119cfa26
Sathish P <sathish.p@egovernments.org>
2017-05-06 04:38:58 +0530

    [PHOENIX-6664]  adding query params in request context

--- egov/egov-services/src/main/java/org/egov/services/wrapper/CustomRequestWrapper.java
+++ egov/egov-services/src/main/java/org/egov/services/wrapper/CustomRequestWrapper.java
@@ -40,6 +40,13 @@
 package org.egov.services.wrapper;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
@@ -52,10 +59,16 @@ import com.netflix.zuul.http.ServletInputStreamWrapper;
 public class CustomRequestWrapper extends HttpServletRequestWrapper {
 
     private String payload;
+    
+    private HttpServletRequest request;
+
+    private Map<String, String[]> parameterMap;
+
 
     public CustomRequestWrapper(final HttpServletRequest request) {
         super(request);
         convertInputStreamToString(request);
+        this.request = request;
     }
 
     private void convertInputStreamToString(final HttpServletRequest request) {
@@ -88,4 +101,59 @@ public class CustomRequestWrapper extends HttpServletRequestWrapper {
     public ServletInputStream getInputStream() {
         return new ServletInputStreamWrapper(payload.getBytes());
     }
+    
+    public void addParameter(String name, String value) {
+        if (parameterMap == null) {
+                parameterMap = new HashMap<String, String[]>();
+                parameterMap.putAll(request.getParameterMap());
+        }
+        String[] values = parameterMap.get(name);
+        if (values == null) {
+                values = new String[0];
+        }
+        List<String> list = new ArrayList<String>(values.length + 1);
+        //list.addAll(Arrays.asList(values));
+        list.add(value);
+        parameterMap.put(name, list.toArray(new String[0]));
+    }
+    
+    @Override
+    public String getParameter(String name) {
+            if (parameterMap == null) {
+                    return request.getParameter(name);
+            }
+
+            String[] strings = parameterMap.get(name);
+            if (strings != null) {
+                    return strings[0];
+            }
+            return null;
+    }
+
+    @Override
+    public Map<String, String[]> getParameterMap() {
+            if (parameterMap == null) {
+                    return request.getParameterMap();
+            }
+
+            return Collections.unmodifiableMap(parameterMap);
+    }
+
+    @Override
+    public Enumeration<String> getParameterNames() {
+            if (parameterMap == null) {
+                    return request.getParameterNames();
+            }
+
+            return Collections.enumeration(parameterMap.keySet());
+    }
+
+    @Override
+    public String[] getParameterValues(String name) {
+            if (parameterMap == null) {
+                    return request.getParameterValues(name);
+            }
+            return parameterMap.get(name);
+    }
+
 }
