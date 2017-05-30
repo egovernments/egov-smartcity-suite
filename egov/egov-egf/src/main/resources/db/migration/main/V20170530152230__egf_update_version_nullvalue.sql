c4c8e53e5d11a1fbed7110c0c9656c3727316024
kavya <kavya.ys@egovernments.org>
2017-05-30 17:32:47 +0530

    [PHOENIX-6638] Restricted parent function modification and code cleanup.

---
+++ egov/egov-egf/src/main/resources/db/migration/main/V20170530152230__egf_update_version_nullvalue.sql
@@ -0,0 +1,3 @@
+update accountdetailtype set version=0 where version is null;
+update function set version=0 where version is null;
+update Chartofaccounts set version=0 where version is null;
--- egov/egov-egfweb/src/main/java/org/egov/egf/web/controller/FundController.java
+++ egov/egov-egfweb/src/main/java/org/egov/egf/web/controller/FundController.java
@@ -40,15 +40,14 @@
 
 package org.egov.egf.web.controller;
 
-import java.util.Date;
 import java.util.List;
-
 import javax.validation.Valid;
 
 import org.egov.commons.Fund;
 import org.egov.commons.service.FundService;
 import org.egov.egf.web.adaptor.FundJsonAdaptor;
-import org.egov.infra.admin.master.service.UserService;
+import org.egov.infra.security.utils.SecurityUtils;
+import org.egov.infra.utils.DateUtils;
 import org.egov.infstr.utils.EgovMasterDataCaching;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
@@ -69,108 +68,109 @@ import com.google.gson.GsonBuilder;
 @Controller
 @RequestMapping("/fund")
 public class FundController {
-	private final static String FUND_NEW = "fund-new";
-	private final static String FUND_RESULT = "fund-result";
-	private final static String FUND_EDIT = "fund-edit";
-	private final static String FUND_VIEW = "fund-view";
-	private final static String FUND_SEARCH = "fund-search";
-	@Autowired
-	private FundService fundService;
-	@Autowired
-	private MessageSource messageSource;
-	@Autowired
-	private UserService userService;
-	@Autowired
-	private EgovMasterDataCaching egovMasterDataCaching;
+    private static final String FUND_NEW = "fund-new";
+    private static final String FUND_RESULT = "fund-result";
+    private static final String FUND_EDIT = "fund-edit";
+    private static final String FUND_VIEW = "fund-view";
+    private static final String FUND_SEARCH = "fund-search";
+    @Autowired
+    SecurityUtils securityUtils;
+    @Autowired
+    private FundService fundService;
+    @Autowired
+    private MessageSource messageSource;
 
-	private void prepareNewForm(Model model) {
-		model.addAttribute("funds", fundService.findByIsnotleaf());
-	}
+    private void prepareNewForm(final Model model) {
+        model.addAttribute("funds", fundService.findByIsnotleaf());
+    }
 
-	@RequestMapping(value = "/new", method = RequestMethod.GET)
-	public String newForm(final Model model) {
-		prepareNewForm(model);
-		model.addAttribute("fund", new Fund());
-		return FUND_NEW;
-	}
+    @RequestMapping(value = "/new", method = RequestMethod.GET)
+    public String newForm(final Model model) {
+        prepareNewForm(model);
+        model.addAttribute("fund", new Fund());
+        return FUND_NEW;
+    }
 
-	@RequestMapping(value = "/create", method = RequestMethod.POST)
-	public String create(@Valid @ModelAttribute final Fund fund,final BindingResult errors, final Model model,final RedirectAttributes redirectAttrs) {
-		if (errors.hasErrors()) {
-			prepareNewForm(model);
-			return FUND_NEW; 
-		}
-		fund.setCreatedDate(new Date());
-		fundService.create(fund);
-		redirectAttrs.addFlashAttribute("message",
-				messageSource.getMessage("msg.fund.success", null, null));
-		egovMasterDataCaching.removeFromCache("egi-fund");
-		return "redirect:/fund/result/" + fund.getId();
-	}
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public String create(@Valid @ModelAttribute final Fund fund, final BindingResult errors, final Model model,
+                         final RedirectAttributes redirectAttrs) {
+        if (errors.hasErrors()) {
+            prepareNewForm(model);
+            return FUND_NEW;
+        }
+        fund.setCreatedDate(DateUtils.now());
+        fund.setCreatedby(securityUtils.getCurrentUser());
+        fundService.create(fund);
+        redirectAttrs.addFlashAttribute("message",
+                messageSource.getMessage("msg.fund.success", null, null));
+        EgovMasterDataCaching.removeFromCache("egi-fund");
+        return "redirect:/fund/result/" + fund.getId();
+    }
 
-	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
-	public String edit(@PathVariable("id") final Integer id, Model model) {
-		Fund fund = fundService.findOne(id);
-		prepareNewForm(model);
-		model.addAttribute("fund", fund);
-		return FUND_EDIT;  
-	}
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String edit(@PathVariable("id") final Integer id, final Model model) {
+        final Fund fund = fundService.findOne(id);
+        prepareNewForm(model);
+        model.addAttribute("fund", fund);
+        return FUND_EDIT;
+    }
 
-	@RequestMapping(value = "/update", method = RequestMethod.POST)
-	public String update(@Valid @ModelAttribute final Fund fund,
-			final BindingResult errors, final Model model,
-			final RedirectAttributes redirectAttrs) {
-		if (errors.hasErrors()) {
-			prepareNewForm(model);
-			return FUND_EDIT;
-		}
-		fundService.update(fund);
-		redirectAttrs.addFlashAttribute("message",
-				messageSource.getMessage("msg.fund.success", null, null));
-		egovMasterDataCaching.removeFromCache("egi-fund");
-		return "redirect:/fund/result/" + fund.getId();
-	}
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String update(@Valid @ModelAttribute final Fund fund,
+                         final BindingResult errors, final Model model,
+                         final RedirectAttributes redirectAttrs) {
+        if (errors.hasErrors()) {
+            prepareNewForm(model);
+            return FUND_EDIT;
+        }
+        fund.setLastModifiedDate(DateUtils.now());
+        fund.setLastModifiedBy(securityUtils.getCurrentUser());
+        fundService.update(fund);
+        redirectAttrs.addFlashAttribute("message",
+                messageSource.getMessage("msg.fund.success", null, null));
+        EgovMasterDataCaching.removeFromCache("egi-fund");
+        return "redirect:/fund/result/" + fund.getId();
+    }
 
-	@RequestMapping(value = "/view/{id}", method = RequestMethod.GET)
-	public String view(@PathVariable("id") final Integer id, Model model) {
-		Fund fund = fundService.findOne(id);
-		prepareNewForm(model);
-		model.addAttribute("fund", fund);
-		return FUND_VIEW;
-	}
+    @RequestMapping(value = "/view/{id}", method = RequestMethod.GET)
+    public String view(@PathVariable("id") final Integer id, final Model model) {
+        final Fund fund = fundService.findOne(id);
+        prepareNewForm(model);
+        model.addAttribute("fund", fund);
+        return FUND_VIEW;
+    }
 
-	@RequestMapping(value = "/result/{id}", method = RequestMethod.GET)
-	public String result(@PathVariable("id") final Integer id, Model model) {
-		Fund fund = fundService.findOne(id);
-		model.addAttribute("fund", fund);
-		return FUND_RESULT;
-	}
+    @RequestMapping(value = "/result/{id}", method = RequestMethod.GET)
+    public String result(@PathVariable("id") final Integer id, final Model model) {
+        final Fund fund = fundService.findOne(id);
+        model.addAttribute("fund", fund);
+        return FUND_RESULT;
+    }
 
-	@RequestMapping(value = "/search/{mode}", method = RequestMethod.GET)
-	public String search(@PathVariable("mode") final String mode, Model model) {
-		Fund fund = new Fund();
-		prepareNewForm(model);
-		model.addAttribute("fund", fund);
-		return FUND_SEARCH;  
+    @RequestMapping(value = "/search/{mode}", method = RequestMethod.GET)
+    public String search(@PathVariable("mode") final String mode, final Model model) {
+        final Fund fund = new Fund();
+        prepareNewForm(model);
+        model.addAttribute("fund", fund);
+        return FUND_SEARCH;
 
-	}
+    }
 
-	@RequestMapping(value = "/ajaxsearch/{mode}", method = RequestMethod.POST, produces = MediaType.TEXT_PLAIN_VALUE)
-	public @ResponseBody String ajaxsearch(
-			@PathVariable("mode") final String mode, Model model,
-			@ModelAttribute final Fund fund) {
-		List<Fund> searchResultList = fundService.search(fund);
-		String result = new StringBuilder("{ \"data\":")
-				.append(toSearchResultJson(searchResultList)).append("}")
-				.toString();
-		return result;
-	}
+    @RequestMapping(value = "/ajaxsearch/{mode}", method = RequestMethod.POST, produces = MediaType.TEXT_PLAIN_VALUE)
+    @ResponseBody
+    public String ajaxsearch(
+            @PathVariable("mode") final String mode, final Model model,
+            @ModelAttribute final Fund fund) {
+        final List<Fund> searchResultList = fundService.search(fund);
+        return new StringBuilder("{ \"data\":")
+                .append(toSearchResultJson(searchResultList)).append("}")
+                .toString();
+    }
 
-	public Object toSearchResultJson(final Object object) {
-		final GsonBuilder gsonBuilder = new GsonBuilder();
-		final Gson gson = gsonBuilder.registerTypeAdapter(Fund.class,
-				new FundJsonAdaptor()).create();
-		final String json = gson.toJson(object);
-		return json;
-	}
+    public Object toSearchResultJson(final Object object) {
+        final GsonBuilder gsonBuilder = new GsonBuilder();
+        final Gson gson = gsonBuilder.registerTypeAdapter(Fund.class,
+                new FundJsonAdaptor()).create();
+        return gson.toJson(object);
+    }
 }
