69c3c6b5410b241638f93583735f081dd7067104
Kamlesh Pratap <kamlesh.pratap@egovernments.org>
2017-06-02 12:08:44 +0530

    [PHOENIX-6857]- Checking Demand Bill status while generating Notices.

--- egov/egov-ptis/src/main/java/org/egov/ptis/domain/service/notice/RecoveryNoticeService.java
+++ egov/egov-ptis/src/main/java/org/egov/ptis/domain/service/notice/RecoveryNoticeService.java
@@ -228,8 +228,10 @@ public class RecoveryNoticeService {
             validateDistressNotice(errors, basicProperty);
         else if (NOTICE_TYPE_INVENTORY.equals(noticeType))
             validateInventoryNotice(errors, basicProperty);
-        else if (VALUATION_CERTIFICATE.equals(noticeType) || NOTICE_TYPE_OC.equals(noticeType))
+        else if (VALUATION_CERTIFICATE.equals(noticeType))
             validateCertificate(errors, basicProperty);
+        else if (NOTICE_TYPE_OC.equals(noticeType))
+        	validateOwnerCertificate(errors, basicProperty);
         else
             validateOccupierNotice(errors, basicProperty);
         return errors;
@@ -285,12 +287,12 @@ public class RecoveryNoticeService {
             reportInput = generateEsdNotice(basicProperty, reportParams, city, noticeNo, formatter);
         else if (NOTICE_TYPE_INVENTORY.equals(noticeType))
             reportInput = generateInventoryNotice(basicProperty, reportParams, city, formatter);
-        else if (VALUATION_CERTIFICATE.equals(noticeType))
-            reportInput = generateValuationCertificate(basicProperty, reportParams, city, noticeNo);
-        else if (NOTICE_TYPE_OC.equals(noticeType))
-            reportInput = generateOwnershipCertificate(basicProperty, reportParams, city, noticeNo);
-        else if (NOTICE_TYPE_OCCUPIER.equals(noticeType))
-            reportOutput = prepareOccupierNotice(basicProperty, reportParams, city, noticeNo);
+		else if (VALUATION_CERTIFICATE.equals(noticeType))
+			reportInput = generateValuationCertificate(basicProperty, reportParams, city, noticeNo);
+		else if (NOTICE_TYPE_OC.equals(noticeType))
+			reportInput = generateOwnershipCertificate(basicProperty, reportParams, city, noticeNo);
+		else if (NOTICE_TYPE_OCCUPIER.equals(noticeType))
+			reportOutput = prepareOccupierNotice(basicProperty, reportParams, city, noticeNo);
         else
             reportInput = generateDistressNotice(basicProperty, reportParams, city, noticeNo);
 
@@ -420,30 +422,36 @@ public class RecoveryNoticeService {
             errors.add("assessment.has.pt.due");
     }
 
-    private List<String> validateDemandBill(final BasicProperty basicProperty, final List<String> errors) {
-        final BigDecimal totalDue = getTotalPropertyTaxDue(basicProperty);
-        if (totalDue.compareTo(BigDecimal.ZERO) == 0)
-            errors.add("common.no.property.due");
-        else {
-            final boolean billExists = getDemandBillByAssessmentNo(basicProperty);
-            if (!billExists)
-                errors.add("common.demandbill.not.exists");
-        }
-        return errors;
-    }
+	private void validateOwnerCertificate(final List<String> errors, final BasicProperty basicProperty) {
+		validateCertificate(errors, basicProperty);
+		validateDemandBillExistance(errors, basicProperty);
+	}
+    
+	private List<String> validateDemandBill(final BasicProperty basicProperty, final List<String> errors) {
+		final BigDecimal totalDue = getTotalPropertyTaxDue(basicProperty);
+		if (totalDue.compareTo(BigDecimal.ZERO) == 0)
+			errors.add("common.no.property.due");
+		validateDemandBillExistance(errors, basicProperty);
+		return errors;
+	}
 
-    private List<String> validateOccupierNotice(final List<String> errors, final BasicProperty basicProperty) {  
-        Boolean hasTenant = Boolean.FALSE;
-        for (final Floor floor : basicProperty.getProperty().getPropertyDetail().getFloorDetails())
-            if (OCC_TENANT.equalsIgnoreCase(floor.getPropertyOccupation().getOccupancyCode()))
-                hasTenant = Boolean.TRUE;
-        if (!hasTenant)
-            errors.add("error.tenant.not.exist");
-        final boolean billExists = getDemandBillByAssessmentNo(basicProperty);
-        if (!billExists)
-            errors.add("common.demandbill.not.exists");
-        return errors;
-    }
+	private List<String> validateOccupierNotice(final List<String> errors, final BasicProperty basicProperty) {
+		Boolean hasTenant = Boolean.FALSE;
+		for (final Floor floor : basicProperty.getProperty().getPropertyDetail().getFloorDetails())
+			if (OCC_TENANT.equalsIgnoreCase(floor.getPropertyOccupation().getOccupancyCode()))
+				hasTenant = Boolean.TRUE;
+		if (!hasTenant)
+			errors.add("error.tenant.not.exist");
+		validateDemandBillExistance(errors, basicProperty);
+		return errors;
+	}
+    
+	private List<String> validateDemandBillExistance(final List<String> errors, final BasicProperty basicProperty) {
+		final boolean billExists = getDemandBillByAssessmentNo(basicProperty);
+		if (!billExists)
+			errors.add("common.demandbill.not.exists");
+		return errors;
+	}
 
     private ReportRequest generateDistressNotice(final BasicProperty basicProperty, final Map<String, Object> reportParams,
                                                  final City city, final String noticeNo) {
