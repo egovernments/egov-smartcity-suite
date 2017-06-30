c362035f53059e263c1fde113469635ecbcc0878
Neelam Kumari <neelam_kumari@cms.co.in>
2017-06-30 11:45:03 +0530

    [PHOENIX-6900] Tax exemption :Spelling is incorrect in rejection acknowledge screen

--- egov/egov-ptisweb/src/main/java/org/egov/ptis/web/controller/transactions/exemption/UpdateTaxExemptionController.java
+++ egov/egov-ptisweb/src/main/java/org/egov/ptis/web/controller/transactions/exemption/UpdateTaxExemptionController.java
@@ -334,7 +334,7 @@ public class UpdateTaxExemptionController extends GenericWorkFlowController {
                     approvalPosition, taxExemptedReason, propertyByEmployee, EXEMPTION);
             }
 
-            successMessage = "Property Exemption rejected successfully and forwared to "
+            successMessage = "Property Exemption rejected successfully and forwarded to "
                     + assignment.getEmployee().getName().concat("~").concat(assignment.getPosition().getName())
                     + " with application number "
                     + property.getApplicationNo();
