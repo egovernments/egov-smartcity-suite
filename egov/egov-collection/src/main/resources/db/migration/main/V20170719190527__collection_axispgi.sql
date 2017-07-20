0380df1efa9db5ad98a58a3fd471fd1688ca8eed
Deepamulimani <deepa.mulimani@egovernments.org>
2017-07-20 09:27:17 +0530

    [PHOENIX-6987] Renaming Axis payment gateway name

---
+++ egov/egov-collection/src/main/resources/db/migration/main/V20170719190527__collection_axispgi.sql
@@ -0,0 +1,2 @@
+update egcl_servicedetails set name='AXIS Bank Payment Gateway (Visa/Master(Debit/Credit)cards)' where code='AXIS';
+update egcl_servicedetails set callbackurl ='http://dev4.governation.com/collection/citizen/onlineReceipt-acceptMessageFromPaymentGateway.action' where code='ATOM';
