ee0c9b0ab1f3686596ab1cb9bf701b73135fedaf
Neelam Kumari <neelam_kumari@cms.co.in>
2017-08-10 15:41:51 +0530

    Build Fixed

--- egov/egov-ptis/src/main/resources/db/migration/main/V20170810150715__ptis_bulkBoundary_roleActionMapping.sql
+++
@@ -1,5 +0,0 @@
-UPDATE eg_action SET enabled=false WHERE name='bulkBoundaryUpdation-Search';
-
-INSERT into EGPT_STATUS (id,status_name,created_date,is_active,code)
-VALUES
-(nextval('SEQ_EGPT_STATUS'),"Bulk Boundary Updation",now(),'Y',"BULK BOUNDARY UPDATION");
