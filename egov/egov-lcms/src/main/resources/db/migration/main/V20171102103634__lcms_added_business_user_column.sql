5f532ce284ac030bc2d8a7711562481442b42054
shwetha <shwetha.dc@egovernments.org>
2017-11-03 12:07:58 +0530

    [PHOENIX-7412] build fix

--- egov/egov-lcms/src/main/resources/db/migration/main/V20171102103634__lcms_added_business_user_column.sql
+++ egov/egov-lcms/src/main/resources/db/migration/main/V20171102103634__lcms_added_business_user_column.sql
@@ -1,6 +1,7 @@
 -----adding user column in advaocate master table 
-
+ALTER TABLE eglc_advocate_master  DROP column if exists advocateuser;
 ALTER TABLE eglc_advocate_master  ADD column advocateuser bigint ;
-alter table eglc_advocate_master add CONSTRAINT fk_advomaster_advocateuser FOREIGN KEY (advocateuser) REFERENCES eg_user (id);
-
+ALTER TABLE eglc_advocate_master DROP CONSTRAINT if exists fk_advomaster_advocateuser;
+ALTER TABLE eglc_advocate_master_aud  DROP column if exists advocateuser;
 ALTER TABLE eglc_advocate_master_aud ADD column advocateuser bigint ; 
+
