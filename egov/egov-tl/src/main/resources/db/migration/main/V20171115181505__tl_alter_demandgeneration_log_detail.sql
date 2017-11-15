ea60c4bdb6f797e494e78e6d030bd5ed48ece07c
sreekanth.nair <sreekanth.nair@egovernments.org>
2017-11-15 20:02:20 +0530

    [PHOENIX-6338] Updated licenseNumber column type length matching with license table

--- egov/egov-tl/src/main/resources/db/migration/main/V20171115181505__tl_alter_demandgeneration_log_detail.sql
+++ egov/egov-tl/src/main/resources/db/migration/main/V20171115181505__tl_alter_demandgeneration_log_detail.sql
@@ -1,4 +1,4 @@
-﻿alter table egtl_demandgenerationlogdetail  add column licenseNumber varchar(25);
+﻿alter table egtl_demandgenerationlogdetail  add column licenseNumber varchar(50);
 alter table egtl_demandgenerationlogdetail  add column licenseId bigint;
 update egtl_demandgenerationlogdetail set licenseid=license, licensenumber=(select license_number from egtl_license where id=license);
 alter table egtl_demandgenerationlogdetail  drop column license;
