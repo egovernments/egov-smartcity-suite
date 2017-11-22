4bdf848afe72f62c4ad094e421c3001fcd968ac9
shwetha <shwetha.dc@egovernments.org>
2017-11-22 16:23:24 +0530

    [PHOENIX-7439] removed employee role

--- egov/egov-lcms/src/main/resources/db/migration/main/V20171122122542__lcms_appconfig_values_standing_roles.sql
+++ egov/egov-lcms/src/main/resources/db/migration/main/V20171122122542__lcms_appconfig_values_standing_roles.sql
@@ -5,6 +5,6 @@ update eg_role set name='STANDING_COUNSEL' where name='TP Standing Counsel';
 ----App config values for business roles
 INSERT INTO eg_appconfig ( ID, KEY_NAME, DESCRIPTION, VERSION, MODULE ) VALUES (nextval('SEQ_EG_APPCONFIG'), 'STANDINGCOUNSEL_ROLES', 'For these Role create business users as standing counsel  ',0, (select id from eg_module where name='LCMS'));
 
-INSERT INTO eg_appconfig_values ( ID, KEY_ID, EFFECTIVE_FROM, VALUE, VERSION ) VALUES (nextval('SEQ_EG_APPCONFIG_VALUES'), (SELECT id FROM EG_APPCONFIG WHERE KEY_NAME='STANDINGCOUNSEL_ROLES'), current_date, 'EMPLOYEE',0);
+--INSERT INTO eg_appconfig_values ( ID, KEY_ID, EFFECTIVE_FROM, VALUE, VERSION ) VALUES (nextval('SEQ_EG_APPCONFIG_VALUES'), (SELECT id FROM EG_APPCONFIG WHERE KEY_NAME='STANDINGCOUNSEL_ROLES'), current_date, 'EMPLOYEE',0);
 
 INSERT INTO eg_appconfig_values ( ID, KEY_ID, EFFECTIVE_FROM, VALUE, VERSION ) VALUES (nextval('SEQ_EG_APPCONFIG_VALUES'), (SELECT id FROM EG_APPCONFIG WHERE KEY_NAME='STANDINGCOUNSEL_ROLES'), current_date, 'STANDING_COUNSEL',0);
