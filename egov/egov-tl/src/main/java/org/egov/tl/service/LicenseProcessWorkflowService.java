36aa236879f086b9b07a8491b242a1267afc5fda
jayashree reddy <jayashree.reddy@egovernments.org>
2017-11-23 13:21:50 +0530

    [PHOENIX-7473] Issue with Renew Trade License.

--- egov/egov-tl/src/main/java/org/egov/tl/service/LicenseProcessWorkflowService.java
+++ egov/egov-tl/src/main/java/org/egov/tl/service/LicenseProcessWorkflowService.java
@@ -143,6 +143,7 @@ public class LicenseProcessWorkflowService {
                     .withComments(workflowBean.getApproverComments())
                     .withDateInfo(currentDate.toDate());
             tradeLicense.setStatus(licenseStatusService.getLicenseStatusByCode(STATUS_CANCELLED));
+            tradeLicense.setCollectionPending(false);
             if (tradeLicense.isNewApplication())
                 tradeLicense.setActive(false);
             tradeLicense.setNewWorkflow(false);
