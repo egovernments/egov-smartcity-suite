df138a611c2abe346c50e00dfa3c7542c326ab38
amit-panwar <amit.pawar@egovernments.org>
2018-02-19 12:59:27 +0530

    [PHOENIX-7652,PHOENIX-7650] Added pattern validation for category and subcategory name , In edit document type if we make document is disabled so it would not we mandatory.

--- egov/egov-tlweb/src/main/webapp/WEB-INF/views/masters/subcategory/subcategory-create.jsp
+++ egov/egov-tlweb/src/main/webapp/WEB-INF/views/masters/subcategory/subcategory-create.jsp
@@ -2,7 +2,7 @@
   ~    eGov  SmartCity eGovernance suite aims to improve the internal efficiency,transparency,
   ~    accountability and the service delivery of the government  organizations.
   ~
-  ~     Copyright (C) 2017  eGovernments Foundation
+  ~     Copyright (C) 2018  eGovernments Foundation
   ~
   ~     The updated version of eGov suite of products as by eGovernments Foundation
   ~     is available at http://www.egovernments.org
@@ -80,7 +80,9 @@
                         <spring:message code="lbl.name"/><span id="mandatory" class="mandatory"></span>
                     </label>
                     <div class="col-sm-3 add-margin">
-                        <form:input path="name" id="name" cssClass="form-control" cssErrorClass="form-control error" required="required" maxLength="256"/>
+                        <form:input path="name" id="name" cssClass="form-control patternvalidation"
+                                    data-pattern="alphabetwithspacehyphenunderscore"
+                                    cssErrorClass="form-control error" required="required" maxLength="256"/>
                         <form:errors path="name" cssClass="error-msg"/>
                     </div>
                     <label class="col-sm-2 control-label text-right">
--- egov/egov-tlweb/src/main/webapp/resources/js/app/license-document-type.js
+++ egov/egov-tlweb/src/main/webapp/resources/js/app/license-document-type.js
@@ -2,7 +2,7 @@
  *    eGov  SmartCity eGovernance suite aims to improve the internal efficiency,transparency,
  *    accountability and the service delivery of the government  organizations.
  *
- *     Copyright (C) 2017  eGovernments Foundation
+ *     Copyright (C) 2018  eGovernments Foundation
  *
  *     The updated version of eGov suite of products as by eGovernments Foundation
  *     is available at http://www.egovernments.org
@@ -46,42 +46,59 @@
  *
  */
 
-$(document).ready(function(){
-	tableContainer1 = $("#document-Table"); 
-	$("#searchbtn").click(function (){
-		tableContainer1.dataTable({
-			"sDom": "<'row'<'col-xs-12 hidden col-right'f>r>t<'row'<'col-md-6 col-xs-12'i><'col-md-3 col-xs-6'l><'col-md-3 col-xs-6 text-right'p>>",
-			"aLengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
-			"bDestroy": true,
-			"autoWidth": false,
-			"ajax": {
-				type:"POST",
-				url:"search?"+$("#searchdocumentform").serialize(),
-				dataSrc: function ( json ) {
-	                //Make your callback here.
-	                $('.report-section').removeClass('display-hide');
-	                return json.data;
-	            }       
-			},
-			"columns": [
-						{ "data": "name"},
-						{ "data": "applicationType"},
-						{ "data": "mandatory"},
-						{ "data": "enabled"},
-						{ "data" : null, 'sClass':"text-center","target":-1,"defaultContent": 
-							
-		                   '<button type="button" class="btn btn-xs btn-secondary edit add-margin"><span class="glyphicon glyphicon-edit"></span>&nbsp;Edit</button>'}
-									,
-									{ "data": "id", "visible":false }
-									]
-								});
-	                     });
+$(document).ready(function () {
+    var tableContainer1 = $("#document-Table");
+    $("#searchbtn").click(function () {
+        tableContainer1.dataTable({
+            "sDom": "<'row'<'col-xs-12 hidden col-right'f>r>t<'row'<'col-md-6 col-xs-12'i><'col-md-3 col-xs-6'l><'col-md-3 col-xs-6 text-right'p>>",
+            "aLengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+            "bDestroy": true,
+            "autoWidth": false,
+            "ajax": {
+                type: "POST",
+                url: "search?" + $("#searchdocumentform").serialize(),
+                dataSrc: function (json) {
+                    //Make your callback here.
+                    $('.report-section').removeClass('display-hide');
+                    return json.data;
+                }
+            },
+            "columns": [
+                {"data": "name"},
+                {"data": "applicationType"},
+                {"data": "mandatory"},
+                {"data": "enabled"},
+                {
+                    "data": null, 'sClass': "text-center", "target": -1, "defaultContent":
 
-	$("#document-Table").on('click','tbody tr td .edit',function(event) {
-		var id = tableContainer1.fnGetData($(this).parent().parent(),5);
-		var url = '/tl/documenttype/edit/'+id ;
-		window.open(url,id,'width=900, height=700, top=300, left=260,scrollbars=yes');
-		
-	});
+                        '<button type="button" class="btn btn-xs btn-secondary edit add-margin"><span class="glyphicon glyphicon-edit"></span>&nbsp;Edit</button>'
+                }
+                ,
+                {"data": "id", "visible": false}
+            ]
+        });
+    });
+
+    $("#document-Table").on('click', 'tbody tr td .edit', function (event) {
+        var id = tableContainer1.fnGetData($(this).parent().parent(), 5);
+        var url = '/tl/documenttype/edit/' + id;
+        window.open(url, id, 'width=900, height=700, top=300, left=260,scrollbars=yes');
+
+    });
 
 });
+
+$("#enabled").click(function () {
+    if ($("#enabled").prop('checked') == false) {
+        $('input:checkbox[name=mandatory]').attr('checked', false);
+    }
+})
+
+$('input:checkbox[name=mandatory]').click(function () {
+    if ($("#enabled").prop('checked') == false) {
+        bootbox.alert("Please first make the document enabled");
+        return false;
+    }
+    else
+        $('input:checkbox[name=mandatory]').attr('checked', true);
+})
