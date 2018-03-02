108b88404c98e40768f139a65180915aa71e2b9d
dushyant <dushyant.pareek@egovernments.org>
2018-03-02 15:29:50 +0530

    [PHOENIX-7701] Updated mv procedure to include tax calculation changes.

---
+++ egov/egov-ptis/src/main/resources/db/migration/main/V20180302145322__ptis_new_columns_mv_inst_demandcoll.sql
@@ -0,0 +1,11 @@
+
+ALTER TABLE egpt_mv_inst_dem_coll ADD column gen_tax double precision;
+ALTER TABLE egpt_mv_inst_dem_coll ADD column gen_tax_coll double precision;
+ALTER TABLE egpt_mv_inst_dem_coll ADD column scavengetax double precision;
+ALTER TABLE egpt_mv_inst_dem_coll ADD column scavengetaxcoll double precision;
+ALTER TABLE egpt_mv_inst_dem_coll ADD column drainagetax double precision;
+ALTER TABLE egpt_mv_inst_dem_coll ADD column drainagetaxcoll double precision;
+ALTER TABLE egpt_mv_inst_dem_coll ADD column lighttax double precision;
+ALTER TABLE egpt_mv_inst_dem_coll ADD column lighttaxcoll double precision;
+ALTER TABLE egpt_mv_inst_dem_coll ADD column watertax double precision;
+ALTER TABLE egpt_mv_inst_dem_coll ADD column watertaxcoll double precision;
