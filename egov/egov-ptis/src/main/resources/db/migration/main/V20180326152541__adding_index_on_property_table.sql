61a414d18d56440174c8fa1ddf3da05223f0c123
rahul.roy <rahul.roy@egovernments.org>
2018-03-26 16:52:36 +0530

    [PHOENIX-7808]-Adding Index on tables and modified insert query for demand increase table

---
+++ egov/egov-ptis/src/main/resources/db/migration/main/V20180326152541__adding_index_on_property_table.sql
@@ -0,0 +1,4 @@
+CREATE INDEX bp_isactive_idx ON egpt_basic_property (isactive);
+CREATE INDEX bp_idpropertyid_idx ON egpt_basic_property (id_propertyid);
+CREATE INDEX pid_ward_adm_idx ON egpt_propertyid (ward_adm_id);
+
