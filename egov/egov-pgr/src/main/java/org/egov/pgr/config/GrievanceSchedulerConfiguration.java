af56fe03dd788341e162920b504fad9e18481c05
amit-panwar <amit.pawar@egovernments.org>
2018-03-28 16:22:01 +0530

    [PHOENIX-379] Changing escalation scheduler time.

--- egov/egov-pgr/src/main/java/org/egov/pgr/config/GrievanceSchedulerConfiguration.java
+++ egov/egov-pgr/src/main/java/org/egov/pgr/config/GrievanceSchedulerConfiguration.java
@@ -2,7 +2,7 @@
  *    eGov  SmartCity eGovernance suite aims to improve the internal efficiency,transparency,
  *    accountability and the service delivery of the government  organizations.
  *
- *     Copyright (C) 2017  eGovernments Foundation
+ *     Copyright (C) 2018  eGovernments Foundation
  *
  *     The updated version of eGov suite of products as by eGovernments Foundation
  *     is available at http://www.egovernments.org
@@ -107,7 +107,7 @@ public class GrievanceSchedulerConfiguration extends QuartzSchedulerConfiguratio
         escalationCron.setJobDetail(complaintEscalationJobDetail().getObject());
         escalationCron.setGroup("PGR_TRIGGER_GROUP");
         escalationCron.setName("PGR_ESCALATION_TRIGGER");
-        escalationCron.setCronExpression("0 */52 * * * ?");
+        escalationCron.setCronExpression("0 */5 * * * ?");
         return escalationCron;
     }
 
