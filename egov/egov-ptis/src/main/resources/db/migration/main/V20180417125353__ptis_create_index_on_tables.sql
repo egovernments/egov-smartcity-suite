b8d403388e32f4c195005ea4edddfbf9afcb88d0
Kamlesh Pratap <kamlesh.pratap@egovernments.org>
2018-04-17 12:56:05 +0530

    [PHOENIX-7898]- Creating Indexes on tables to speedup queries.

---
+++ egov/egov-ptis/src/main/resources/db/migration/main/V20180417125353__ptis_create_index_on_tables.sql
@@ -0,0 +1,6 @@
+create index idx_id_property on egpt_ptdemand(id_property);
+create index idx_bp_propertyid on egpt_basic_property(propertyid);  
+create index idx_demand on eg_demand_details(id_demand);    
+create index idx_basic_property on egpt_property_status_values(id_basic_property);    
+create index idx_basicproperty on egpt_property_owner_info(basicproperty);    
+
