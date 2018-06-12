43adf2ce3695902eda786dd834140f99cfc7406c
somvit <somvit.bhowmik@tarento.com>
2018-06-12 15:51:01 +0530

    [PHOENIX-7998] updated copyright

--- egov/egov-pushbox/src/main/java/org/egov/pushbox/repository/UserFcmDeviceRepository.java
+++ egov/egov-pushbox/src/main/java/org/egov/pushbox/repository/UserFcmDeviceRepository.java
@@ -2,7 +2,7 @@
  *    eGov  SmartCity eGovernance suite aims to improve the internal efficiency,transparency,
  *    accountability and the service delivery of the government  organizations.
  *
- *     Copyright (C) 2017  eGovernments Foundation
+ *     Copyright (C) 2018  eGovernments Foundation
  *
  *     The updated version of eGov suite of products as by eGovernments Foundation
  *     is available at http://www.egovernments.org
@@ -66,7 +66,7 @@ public interface UserFcmDeviceRepository extends JpaRepository<UserFcmDevice, ja
     UserFcmDevice findByUserId(Long id);
 
     UserFcmDevice findByUserIdAndDeviceId(Long userId, String deviceId);
-    
+
     List<UserFcmDevice> findByUserIdIn(List<Long> ids);
 
 }
