626005c9fe3f4f026b6a1ee1998c7e5e3c0950f9
parvati <parvati.hiremath@egovernments.org>
2018-06-22 17:37:18 +0530

    [PHOENIX-8013] Good will amount GLCODE

---
+++ egov/egov-egf/src/main/resources/db/migration/sample/V20180622172812__egf_glcode_for_lease.sql
@@ -0,0 +1,2 @@
+insert into chartofaccounts  VALUES (nextval('seq_chartofaccounts'),'1308003','Goodwill From Leases',null,true,(select id from chartofaccounts where glcode = '13080'), null, 'A', 'I',
+null, 4, false, false, (select id from schedulemapping where schedule = 'I-03'),null, null,null, null, '130', null,null, now(), 1, now(), 1, 0);
