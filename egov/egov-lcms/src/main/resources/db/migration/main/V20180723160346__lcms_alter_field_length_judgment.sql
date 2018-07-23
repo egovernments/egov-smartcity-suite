6875b12a46cc62fa8e18d65c5904519ca92bdd95
shwetha <shwetha.dc@egovernments.org>
2018-07-23 16:51:47 +0530

    [PHOENIX-8157] fixed issues for Error Page for entering maximum characters in Judgement Details & Compliance Report fields.

---
+++ egov/egov-lcms/src/main/resources/db/migration/main/V20180723160346__lcms_alter_field_length_judgment.sql
@@ -0,0 +1,21 @@
+Alter table eglc_judgment Alter column judgmentdetails TYPE character varying(1024) ;
+
+Alter table eglc_judgment_aud Alter column judgmentdetails TYPE character varying(1024) ;
+
+Alter table eglc_judgment Alter column enquirydetails TYPE character varying(1024) ;
+
+Alter table eglc_judgment_aud Alter column enquirydetails TYPE character varying(1024) ;
+
+Alter table eglc_judgment Alter column setasidepetitiondetails TYPE character varying(1024) ;
+
+Alter table eglc_judgment_aud Alter column setasidepetitiondetails TYPE character varying(1024) ;
+
+Alter table eglc_judgmentimpl Alter column compliancereport TYPE character varying(1024) ;
+
+Alter table eglc_judgmentimpl_aud  Alter column compliancereport TYPE character varying(1024) ;
+
+
+Alter table eglc_judgmentimpl Alter column implementationdetails TYPE character varying(1024) ;
+
+Alter table eglc_judgmentimpl_aud  Alter column implementationdetails TYPE character varying(1024) ;
+
