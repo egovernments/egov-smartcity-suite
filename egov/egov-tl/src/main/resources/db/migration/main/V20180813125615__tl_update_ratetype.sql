1bc6aa810402f85f45a08dace283b2ce4064a04e
sreekanth.nair <sreekanth.nair@egovernments.org>
2018-08-13 13:21:31 +0530

    [PHOENIX-379] Update old rate type enum value with updated value

---
+++ egov/egov-tl/src/main/resources/db/migration/main/V20180813125615__tl_update_ratetype.sql
@@ -0,0 +1,2 @@
+update egtl_subcategory_details set ratetype='FLAT_BY_RANGE' where ratetype='Flat_by_Range';
+update egtl_subcategory_details set ratetype='UNIT_BY_RANGE' where ratetype='Unit_by_Range';
