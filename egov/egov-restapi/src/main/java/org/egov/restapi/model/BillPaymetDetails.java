fa6462c8ed5e95d9c68a20cfa987b60d05374e89
Pabitra Sahu <pabitra.s@careator.com>
2018-08-20 17:32:38 +0530

    [PHOENIX-8240,8122] New API for giving bill payment details,New class added to return the API response,Changes in bill creation API, Commented one test case as it is nor required after the changes been made for Bill Creation.New Constants been added.

---
+++ egov/egov-restapi/src/main/java/org/egov/restapi/model/BillPaymetDetails.java
@@ -0,0 +1,89 @@
+package org.egov.restapi.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class BillPaymetDetails {
+	
+	private String cityname;
+	private String billVoucherNo;
+	private String billVoucherStatus;
+	private String paymentVoucherNo;
+	private String paymentVoucherStatus;
+	//private Double amount;
+	private BigDecimal paymentAmount;
+	private Date voucherDate;
+	private String chequRefNo;
+	
+	
+	public BillPaymetDetails() {
+		
+	}
+	
+	public String getCityname() {
+		return cityname;
+	}
+	public void setCityname(String cityname) {
+		this.cityname = cityname;
+	}
+
+	public String getBillVoucherNo() {
+		return billVoucherNo;
+	}
+	public void setBillVoucherNo(String billVoucherNo) {
+		this.billVoucherNo = billVoucherNo;
+	}
+
+	public String getBillVoucherStatus() {
+		return billVoucherStatus;
+	}
+	public void setBillVoucherStatus(String billVoucherStatus) {
+		this.billVoucherStatus = billVoucherStatus;
+	}
+
+	public String getPaymentVoucherNo() {
+		return paymentVoucherNo;
+	}
+	public void setPaymentVoucherNo(String paymentVoucherNo) {
+		this.paymentVoucherNo = paymentVoucherNo;
+	}
+
+	public String getPaymentVoucherStatus() {
+		return paymentVoucherStatus;
+	}
+
+	public void setPaymentVoucherStatus(String paymentVoucherStatus) {
+		this.paymentVoucherStatus = paymentVoucherStatus;
+	}
+
+	/*public Double getAmount() {
+		return amount;
+	}
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}*/
+
+	public BigDecimal getPaymentAmount() {
+		return paymentAmount;
+	}
+	public void setPaymentAmount(BigDecimal paymentAmount) {
+		this.paymentAmount = paymentAmount;
+	}
+
+	public Date getVoucherDate() {
+		return voucherDate;
+	}
+	public void setVoucherDate(Date voucherDate) {
+		this.voucherDate = voucherDate;
+	}
+
+	public String getChequRefNo() {
+		return chequRefNo;
+	}
+	public void setChequRefNo(String chequRefNo) {
+		this.chequRefNo = chequRefNo;
+	}
+	
+	
+
+}
