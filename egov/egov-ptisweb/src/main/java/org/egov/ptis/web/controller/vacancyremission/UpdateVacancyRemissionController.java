8f28723e9b865bb338f6dd050de24298c210ad7a
Kamlesh Pratap <kamlesh.pratap@egovernments.org>
2018-10-08 16:13:59 +0530

    [PHOENIX-8376]-Build Fix!

--- egov/egov-ptisweb/src/main/java/org/egov/ptis/web/controller/vacancyremission/UpdateVacancyRemissionController.java
+++ egov/egov-ptisweb/src/main/java/org/egov/ptis/web/controller/vacancyremission/UpdateVacancyRemissionController.java
@@ -129,9 +129,9 @@ public class UpdateVacancyRemissionController extends GenericWorkFlowController 
                     vacancyRemission.getCurrentState()));
             model.addAttribute("ownersName", vacancyRemission.getBasicProperty().getFullOwnerName());
             model.addAttribute("applicationNo", vacancyRemission.getApplicationNumber());
-            if (propertyTaxUtil.enableVRApproval(vacancyRemission)) {
+            if (propertyTaxUtil.enableVRApproval(vacancyRemission.getBasicProperty().getUpicNo())) {
                 return "redirect:/vacancyremissionapproval/create/" + vacancyRemission.getBasicProperty().getUpicNo();
-            } else if (propertyTaxUtil.enableMonthlyUpdate(vacancyRemission)) {
+            } else if (propertyTaxUtil.enableMonthlyUpdate(vacancyRemission.getBasicProperty().getUpicNo())) {
                 return "redirect:/vacancyremission/monthlyupdate/" + vacancyRemission.getBasicProperty().getUpicNo();
             }
             final BasicProperty basicProperty = vacancyRemission.getBasicProperty();
