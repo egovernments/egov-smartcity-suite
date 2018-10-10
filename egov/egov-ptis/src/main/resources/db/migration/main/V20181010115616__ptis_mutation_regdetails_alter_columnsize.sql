cd93f715be3afc48bdd2371db57c33c39dd19a2b
Iffath <iffath@egovernments.org>
2018-10-10 12:18:05 +0530

    [PHOENIX-8383] : increase the column size as per given by the registration department.

---
+++ egov/egov-ptis/src/main/resources/db/migration/main/V20181010115616__ptis_mutation_regdetails_alter_columnsize.sql
@@ -0,0 +1,5 @@
+ALTER TABLE egpt_mutation_registration_details ALTER COLUMN seller TYPE character varying(400);
+
+ALTER TABLE egpt_mutation_registration_details ALTER COLUMN buyer TYPE character varying(400);
+
+ALTER TABLE egpt_mutation_registration_details ALTER COLUMN address TYPE character varying(250);
