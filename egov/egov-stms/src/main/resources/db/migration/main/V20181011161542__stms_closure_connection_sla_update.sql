71a1584b50e6169ea657eaf27cdbc06aaa75c816
jayashree reddy <jayashree.reddy@egovernments.org>
2018-10-11 16:57:52 +0530

    [PHOENIX-8379] Script fix.

--- egov/egov-stms/src/main/resources/db/migration/main/V20181011161542__stms_closure_connection_sla_update.sql
+++ egov/egov-stms/src/main/resources/db/migration/main/V20181011161542__stms_closure_connection_sla_update.sql
@@ -2,4 +2,4 @@ INSERT INTO eg_appconfig ( ID, KEY_NAME, DESCRIPTION, VERSION, MODULE ) VALUES (
 
 INSERT INTO eg_appconfig_values ( ID, KEY_ID, EFFECTIVE_FROM, VALUE, VERSION ) VALUES (nextval('SEQ_EG_APPCONFIG_VALUES'), (SELECT id FROM EG_APPCONFIG WHERE KEY_NAME='SLAFORCLOSURE' and module= (select id from eg_module where name='Sewerage Tax Management')), current_date, '15',0);
 
-update eg_appconfig_values set value = '15' where key_id in (select id from eg_appconfig where module =(select id from eg_module where name='Sewerage Tax Management') and keyname = 'SLAFORCHANGEINCLOSET');
+update eg_appconfig_values set value = '15' where key_id in (select id from eg_appconfig where module =(select id from eg_module where name='Sewerage Tax Management') and key_name = 'SLAFORCHANGEINCLOSET');
