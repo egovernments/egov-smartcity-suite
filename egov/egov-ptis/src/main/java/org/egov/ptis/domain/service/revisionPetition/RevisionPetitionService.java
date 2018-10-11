2c57539cd0ae5939527d49c37125ca885353b532
DushyantPareek <dushyant.pareek@egovernments.org>
2018-10-11 16:05:56 +0530

    [PHOENIX-8360] Fixed a use case where collection adjustment was failing.

--- egov/egov-ptis/src/main/java/org/egov/ptis/domain/service/revisionPetition/RevisionPetitionService.java
+++ egov/egov-ptis/src/main/java/org/egov/ptis/domain/service/revisionPetition/RevisionPetitionService.java
@@ -1144,14 +1144,15 @@ public class RevisionPetitionService extends PersistenceService<RevisionPetition
 		if (oldProperty == null)
 			LOGGER.info("modifyBasicProp, Could not get the previous property. DCB for arrears will be incorrect");
 		else {
-			modProperty = propertyService.createDemandForModify(oldProperty, newProperty, propCompletionDate);
+			final PropertyImpl historyProperty = (PropertyImpl) propertyService.getHistoryPropertyByUpinNo(oldProperty.getBasicProperty());
+			modProperty = propertyService.createDemandForModify(historyProperty, newProperty, propCompletionDate);
 			modProperty = createArrearsDemand(oldProperty, propCompletionDate, newProperty);
 		}
 
 		Map<Installment, Set<EgDemandDetails>> demandDetailsSetByInstallment;
 		List<Installment> installments;
 
-		final Set<EgDemandDetails> oldEgDemandDetailsSet = propertyService.getOldDemandDetails(oldProperty,
+		final Set<EgDemandDetails> oldEgDemandDetailsSet = propertyService.getOldDemandDetails(propertyService.getHistoryPropertyByUpinNo(oldProperty.getBasicProperty()),
 				newProperty);
 		demandDetailsSetByInstallment = propertyService.getEgDemandDetailsSetByInstallment(oldEgDemandDetailsSet);
 		installments = new ArrayList<>(demandDetailsSetByInstallment.keySet());
