a0f60a093c5495cfe8ace207c7ef3715c2e844a8
Jayashree Reddy <jayashree.reddy@egovernments.org>
2018-12-10 12:18:38 +0530

    [PHOENIX-8421] Vertical and Horizontal Escalation security fixes

--- egov/egov-tl/src/main/resources/db/migration/main/V20181210103049__tl_ajax_workflow_roleaction.sql
+++ egov/egov-tl/src/main/resources/db/migration/main/V20181210103049__tl_ajax_workflow_roleaction.sql
@@ -1,1 +1,9 @@
 insert into eg_roleaction values ((select id from eg_role where name='TLApprover'),(select id from eg_action where name='findDesignations-byDepartment' and contextroot='eis'));
+
+delete from eg_feature_action where action=(select id from eg_action where name = 'AjaxDesignationDropdown') and feature in (select id from eg_feature where name in ('Closure Application Approval','Renew License','Create New License','License Approval'));
+
+delete from eg_feature_action where action=(select id from eg_action where name='AjaxDesignationsByDepartmentWithDesignation') and feature in (select id from eg_feature where name in ('Closure Application Approval'));
+
+insert into eg_feature_action values ((select id from eg_action where name='findDesignations-byDepartment'),(select id from eg_feature where name ='License Approval'));
+
+insert into eg_feature_action  values ((select id from eg_action where name = 'FindDesignationsByDepartmentWithDesignation' and contextroot='eis'),(select id from eg_feature where name ='Closure Application Approval'));
