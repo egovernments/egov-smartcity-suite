ae9605540e1ae287ca36794d0edf9be137b8977e
sreekanth.nair <sreekanth.nair@egovernments.org>
2019-01-04 13:04:24 +0530

    [PHOENIX-8447] Removed wrong scripts

--- egov/egov-tl/src/main/resources/db/migration/main/V20190104124928__tl_license_fee_collection_url_update.sql
+++ egov/egov-tl/src/main/resources/db/migration/main/V20190104124928__tl_license_fee_collection_url_update.sql
@@ -2,7 +2,3 @@ UPDATE eg_action set url='/license/fee/collect/' WHERE name='TL_LICENSE_FEE_COLL
 DELETE FROM eg_feature_action WHERE action=(SELECT id FROM eg_action WHERE name='TL_LICENSE_BILL_COLLECT');
 DELETE FROM eg_roleaction WHERE actionid=(SELECT id FROM eg_action WHERE name='TL_LICENSE_BILL_COLLECT');
 DELETE FROM eg_action WHERE name='TL_LICENSE_BILL_COLLECT';
-
-
-select * from newone.egtl_license where appl_num='00004-2019-YB';
-select * from newone.eg_demand where id=19
