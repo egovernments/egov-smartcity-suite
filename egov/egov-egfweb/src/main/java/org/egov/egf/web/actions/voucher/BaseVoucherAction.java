5cf934058e1f08e4b20529c7a8dbe47d5f2911f6
Pabitra Sahu <pabitra.s@careator.com>
2019-01-21 13:12:12 +0530

    [PHOENIX-8428] Passing Date directly.

--- egov/egov-egfweb/src/main/java/org/egov/egf/web/actions/voucher/BaseVoucherAction.java
+++ egov/egov-egfweb/src/main/java/org/egov/egf/web/actions/voucher/BaseVoucherAction.java
@@ -577,8 +577,8 @@ public class BaseVoucherAction extends GenericWorkFlowAction {
 
         final StringBuffer fyQuery = new StringBuffer();
         fyQuery.append("from CFinancialYear where isActiveForPosting=true and startingDate <= ?1 AND endingDate >= ?2");
-        final List<CFinancialYear> list = persistenceService.findAllBy(fyQuery.toString(), Constants.DDMMYYYYFORMAT1.format(voucherHeader.getVoucherDate()),
-                Constants.DDMMYYYYFORMAT1.format(voucherHeader.getVoucherDate()));
+        final List<CFinancialYear> list = persistenceService.findAllBy(fyQuery.toString(), voucherHeader.getVoucherDate(),
+                voucherHeader.getVoucherDate());
         if (list.size() == 0) {
             addActionError(getText("journalvoucher.fYear.notActive"));
             return true;
