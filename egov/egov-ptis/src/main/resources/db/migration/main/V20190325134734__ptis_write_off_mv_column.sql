f0cf08be3cf0ecb9701c4cd16117664e6e039103
DushyantPareek <dushyant.pareek@egovernments.org>
2019-03-25 14:26:12 +0530

    [PHOENIX-8826] MV changes to incorporate penalty waive off amount.

---
+++ egov/egov-ptis/src/main/resources/db/migration/main/V20190325134734__ptis_write_off_mv_column.sql
@@ -0,0 +1,4 @@
+ALTER table egpt_mv_propertyinfo add column exempted_amount double precision default 0;
+ALTER table egpt_mv_propertyinfo add column waivedoff_amount double precision default 0;
+ALTER table egpt_mv_propertyinfo add column writeoff_amount double precision default 0;
+ALTER table egpt_mv_propertyinfo add column courtcase_amount double precision default 0;
