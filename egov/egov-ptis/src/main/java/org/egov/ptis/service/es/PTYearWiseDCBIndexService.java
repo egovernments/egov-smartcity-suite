0f296fb74feb309d02d75e200271b333d462dea1
Debajyoti Bhaumik <code.debjyoti@gmail.com>
2019-04-11 18:48:34 +0530

    wip

--- egov/egov-ptis/src/main/java/org/egov/ptis/service/es/PTYearWiseDCBIndexService.java
+++ egov/egov-ptis/src/main/java/org/egov/ptis/service/es/PTYearWiseDCBIndexService.java
@@ -205,19 +205,19 @@ public class PTYearWiseDCBIndexService {
                                     .add(new BigDecimal(responseFields.get(CURRENT_INTEREST_COLLECTION).toString())))));
             serviceWiseResponse.setWaivedoffAmount(
                     new BigDecimal(
-                            Optional.ofNullable(responseFields.get(WAIVEDOFF_AMOUNT)).orElse("").toString()
+                            Optional.ofNullable(responseFields.get(WAIVEDOFF_AMOUNT)).orElse("0").toString()
                     ));
             serviceWiseResponse.setExemptedAmount(
                     new BigDecimal(
-                            Optional.ofNullable(responseFields.get(EXEMPTED_AMOUNT)).orElse("").toString()
+                            Optional.ofNullable(responseFields.get(EXEMPTED_AMOUNT)).orElse("0").toString()
                     ));
             serviceWiseResponse.setWriteoffAmount(
                     new BigDecimal(
-                            Optional.ofNullable(responseFields.get(WRITEOFF_AMOUNT)).orElse("").toString()
+                            Optional.ofNullable(responseFields.get(WRITEOFF_AMOUNT)).orElse("0").toString()
                     ));
             serviceWiseResponse.setCourtcaseAmount(
                     new BigDecimal(
-                            Optional.ofNullable(responseFields.get(COURTCASE_AMOUNT)).orElse("").toString()
+                            Optional.ofNullable(responseFields.get(COURTCASE_AMOUNT)).orElse("0").toString()
                     ));
 
             serviceWiseResponse.setDrillDownType(responseFields.get("assessmentNo").toString());
