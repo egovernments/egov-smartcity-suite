e29ae844a2a76a773c5602a2288ef456c88c7967
Deepamulimani <deepa.mulimani@egovernments.org>
2019-05-20 16:11:32 +0530

    [PHOENIX-8763]-Reading citycode from cityservice

--- egov/egov-collection/src/main/java/org/egov/collection/integration/pgi/SbimopsAdaptor.java
+++ egov/egov-collection/src/main/java/org/egov/collection/integration/pgi/SbimopsAdaptor.java
@@ -82,7 +82,7 @@ import org.egov.collection.config.properties.CollectionApplicationProperties;
 import org.egov.collection.constants.CollectionConstants;
 import org.egov.collection.entity.OnlinePayment;
 import org.egov.collection.entity.ReceiptHeader;
-import org.egov.infra.config.core.ApplicationThreadLocals;
+import org.egov.infra.admin.master.service.CityService;
 import org.egov.infra.exception.ApplicationRuntimeException;
 import org.egov.infstr.models.ServiceDetails;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -130,6 +130,8 @@ public class SbimopsAdaptor implements PaymentGatewayAdaptor {
 
     @Autowired
     private CollectionApplicationProperties collectionApplicationProperties;
+    @Autowired
+    private CityService cityService;
 
     /**
      * This method invokes APIs to frame request object for the payment service passed as parameter
@@ -155,10 +157,10 @@ public class SbimopsAdaptor implements PaymentGatewayAdaptor {
         requestParameterMap.put(SBIMOPS_RID, receiptHeader.getConsumerCode());
         requestParameterMap.put(SBIMOPS_TA, receiptHeader.getTotalAmount().toString());
         final StringBuilder chStringBuilder = new StringBuilder((String.format(SBIMOPS_HOA_FORMAT,
-                collectionApplicationProperties.sbimopsHoa(ApplicationThreadLocals.getCityCode(), billServiceCode))).replace(' ',
+                collectionApplicationProperties.sbimopsHoa(cityService.getCityCode(), billServiceCode))).replace(' ',
                         '0'));
         chStringBuilder.append(CollectionConstants.SEPARATOR_COMMA)
-                .append(collectionApplicationProperties.sbimopsDdocode(ApplicationThreadLocals.getCityCode()).toString())
+                .append(collectionApplicationProperties.sbimopsDdocode(cityService.getCityCode()))
                 .append(CollectionConstants.SEPARATOR_COMMA)
                 .append(collectionApplicationProperties.sbimopsServiceCode(billServiceCode))
                 .append(CollectionConstants.SEPARATOR_COMMA)
@@ -253,8 +255,7 @@ public class SbimopsAdaptor implements PaymentGatewayAdaptor {
                     if (LOGGER.isDebugEnabled())
                         LOGGER.debug("End SbimopsAdaptor-parsePaymentResponse");
                 }
-            }
-            else{
+            } else {
                 LOGGER.error(" DTID/Status are not found in the response.Sbimops realtime transaction response : " + response);
                 throw new ApplicationRuntimeException("Sbimops DTID/Status are not found in the response.");
             }
@@ -283,7 +284,7 @@ public class SbimopsAdaptor implements PaymentGatewayAdaptor {
             if (responseParameterMap != null && responseParameterMap.containsKey(SBIMOPS_STATUS.toUpperCase())) {
                 if (LOGGER.isInfoEnabled())
                     LOGGER.info("Sbimops reconciliation transaction response : " + responseParameterMap.toString());
-                LOGGER.error("SBIMOPSRECON Response:"+ responseParameterMap.toString());
+                LOGGER.error("SBIMOPSRECON Response:" + responseParameterMap.toString());
                 final String transactionStatus = responseParameterMap.get(SBIMOPS_STATUS.toUpperCase());
                 sbimopsResponse.setAuthStatus(getTransactionStatus(transactionStatus));
                 sbimopsResponse.setErrorDescription(transactionStatus);
@@ -351,7 +352,7 @@ public class SbimopsAdaptor implements PaymentGatewayAdaptor {
         final JsonObject requestJson = new JsonObject();
         deptCodeJson.add(SBIMOPS_ROW, transactionIdJson);
         requestJson.add(SBIMOPS_RECORDSET, deptCodeJson);
-        LOGGER.error("SBIMOPSRECON Request:"+transactionId.toString());
+        LOGGER.error("SBIMOPSRECON Request:" + transactionId.toString());
         if (LOGGER.isInfoEnabled())
             LOGGER.info("SBIMOPS reconciliation request parameters:" + requestJson.toString());
         return requestJson.toString();
