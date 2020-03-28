4dbcb91070d35d81ab2a19806ef9afb5d5de1f82
AshwiniReddy398 <ashwini.s@egovernments.org>
2020-03-28 20:13:35 +0530

    [PHOENIX-8984]:fixed partial collection issue Bill Collectors Collection
    Book

--- egov/egov-ptisweb/src/main/java/org/egov/ptis/actions/reports/BillCollectorCollectionBookAdaptor.java
+++ egov/egov-ptisweb/src/main/java/org/egov/ptis/actions/reports/BillCollectorCollectionBookAdaptor.java
@@ -1,37 +1,83 @@
+/*
+ *    eGov  SmartCity eGovernance suite aims to improve the internal efficiency,transparency,
+ *    accountability and the service delivery of the government  organizations.
+ *
+ *     Copyright (C) 2017  eGovernments Foundation
+ *
+ *     The updated version of eGov suite of products as by eGovernments Foundation
+ *     is available at http://www.egovernments.org
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see http://www.gnu.org/licenses/ or
+ *     http://www.gnu.org/licenses/gpl.html .
+ *
+ *     In addition to the terms of the GPL license to be adhered to in using this
+ *     program, the following additional terms are to be complied with:
+ *
+ *         1) All versions of this program, verbatim or modified must carry this
+ *            Legal Notice.
+ *            Further, all user interfaces, including but not limited to citizen facing interfaces,
+ *            Urban Local Bodies interfaces, dashboards, mobile applications, of the program and any
+ *            derived works should carry eGovernments Foundation logo on the top right corner.
+ *
+ *            For the logo, please refer http://egovernments.org/html/logo/egov_logo.png.
+ *            For any further queries on attribution, including queries on brand guidelines,
+ *            please contact contact@egovernments.org
+ *
+ *         2) Any misrepresentation of the origin of the material is prohibited. It
+ *            is required that all modified versions of this material be marked in
+ *            reasonable ways as different from the original version.
+ *
+ *         3) This license does not grant any rights to any user of the program
+ *            with regards to rights under trademark law for use of the trade names
+ *            or trademarks of eGovernments Foundation.
+ *
+ *   In case of any queries, you can reach eGovernments Foundation at contact@egovernments.org.
+ *
+ */
 package org.egov.ptis.actions.reports;
 
 import static org.egov.ptis.constants.PropertyTaxConstants.DATE_FORMAT_DDMMYYY;
 
 import java.lang.reflect.Type;
 import java.math.BigDecimal;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 
 import org.egov.infra.utils.DateUtils;
 import org.egov.infra.web.support.json.adapter.DataTableJsonAdapter;
 import org.egov.infra.web.support.ui.DataTable;
-import org.egov.ptis.client.util.PropertyTaxUtil;
-import org.egov.ptis.domain.entity.property.BasicProperty;
-import org.egov.ptis.domain.entity.property.CollectionSummary;
-import org.egov.ptis.domain.entity.property.CollectionSummaryDetails;
 import org.egov.ptis.domain.entity.property.view.CollectionIndexInfo;
-import org.egov.ptis.repository.reports.PropertyMVInfoRepository;
+import org.egov.ptis.domain.service.report.BillCollectorBookReportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSerializationContext;
 
+@Component
 public class BillCollectorCollectionBookAdaptor implements DataTableJsonAdapter<CollectionIndexInfo> {
 
-    PropertyMVInfoRepository propertyMVInfoRepository;
+    private static BillCollectorBookReportService billCollectorBookReportService;
+
+    public BillCollectorCollectionBookAdaptor() {
+    }
+
+    @Autowired
+    public BillCollectorCollectionBookAdaptor(final BillCollectorBookReportService billCollectorBookReportService) {
+        BillCollectorCollectionBookAdaptor.billCollectorBookReportService = billCollectorBookReportService;
+    }
 
     @Override
     public JsonElement serialize(DataTable<CollectionIndexInfo> billCollectorCollectionBookResponse, Type typeOfSrc,
@@ -46,7 +92,10 @@ public class BillCollectorCollectionBookAdaptor implements DataTableJsonAdapter<
                     DateUtils.getFormattedDate(collectionIndexInfoObj.getReceiptDate(), DATE_FORMAT_DDMMYYY));
             jsonObject.addProperty("name", collectionIndexInfoObj.getCreatedBy().getName());
             jsonObject.addProperty("ward", collectionIndexInfoObj.getWardId());
-            jsonObject.addProperty("assessmentNo", collectionIndexInfoObj.getConsumerCode());
+            if (billCollectorBookReportService.getbalanceAmount(collectionIndexInfoObj.getId()))
+                jsonObject.addProperty("assessmentNo", collectionIndexInfoObj.getConsumerCode() + "[P]");
+            else
+                jsonObject.addProperty("assessmentNo", collectionIndexInfoObj.getConsumerCode());
             jsonObject.addProperty("fromInstallment", collectionIndexInfoObj.getFromInstallment());
             jsonObject.addProperty("toInstallment", collectionIndexInfoObj.getToInstallment());
             jsonObject.addProperty("arrearPropertyTax", collectionIndexInfoObj.getArrearAmount() == null ? BigDecimal.ZERO
@@ -72,8 +121,8 @@ public class BillCollectorCollectionBookAdaptor implements DataTableJsonAdapter<
                     : collectionIndexInfoObj.getCurrentAmount())
                             .add(collectionIndexInfoObj.getCurrentEduTax() == null ? BigDecimal.ZERO
                                     : collectionIndexInfoObj.getCurrentEduTax())
-                                            .add(collectionIndexInfoObj.getCurrentLibCes() == null ? BigDecimal.ZERO
-                                                    : collectionIndexInfoObj.getCurrentLibCes());
+                            .add(collectionIndexInfoObj.getCurrentLibCes() == null ? BigDecimal.ZERO
+                                    : collectionIndexInfoObj.getCurrentLibCes());
             jsonObject.addProperty("currentTotal", currentTotal);
             bcCollectionBookDetails.add(jsonObject);
         });
