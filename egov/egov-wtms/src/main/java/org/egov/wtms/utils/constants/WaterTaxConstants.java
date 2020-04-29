66afa540827a87939156a45dd0f7dd3ee840e82b
alok <alok@Alok.eGov>
2020-04-29 18:11:16 +0530

    [PHOENIX-9176] - Removing drop down values Closing of connection and Reconnection.

--- egov/egov-wtms/src/main/java/org/egov/wtms/utils/constants/WaterTaxConstants.java
+++ egov/egov-wtms/src/main/java/org/egov/wtms/utils/constants/WaterTaxConstants.java
@@ -478,8 +478,6 @@ public class WaterTaxConstants {
             add(NEWCONNECTION);
             add(ADDNLCONNECTION);
             add(CHANGEOFUSE);
-            add(CLOSINGCONNECTION);
-            add(RECONNECTION);
         }
     };
     public static final String WARDSECRETARY_OPERATOR_ROLE = "WardSecretary Operator";
