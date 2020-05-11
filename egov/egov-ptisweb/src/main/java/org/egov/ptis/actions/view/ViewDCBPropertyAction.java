40f1e4818d4388ab70bc161213663142d9b2195e
AshwiniReddy398 <ashwini.s@egovernments.org>
2020-05-11 10:32:01 +0530

    [APQA-823]:Inactive property should throw validation message in view DCB
    of public URL

--- egov/egov-ptisweb/src/main/java/org/egov/ptis/actions/view/ViewDCBPropertyAction.java
+++ egov/egov-ptisweb/src/main/java/org/egov/ptis/actions/view/ViewDCBPropertyAction.java
@@ -161,12 +161,14 @@ import org.springframework.context.ApplicationContext;
 
     @Override
     public void prepare() {
-        setBasicProperty(basicPropertyDAO.getBasicPropertyByPropertyID(propertyId));
-        if (OWNERSHIP_TYPE_VAC_LAND.equals(basicProperty.getProperty().getPropertyDetail().getPropertyTypeMaster()!=null?
-                basicProperty.getProperty().getPropertyDetail().getPropertyTypeMaster().getCode():"")) {
-            serviceCode = SERVICE_CODE_VACANTLANDTAX;
-        } else {
-            serviceCode = SERVICE_CODE_PROPERTYTAX;
+        basicProperty = basicPropertyDAO.getBasicPropertyByPropertyID(propertyId);
+        if (basicProperty != null) {
+            if (OWNERSHIP_TYPE_VAC_LAND.equals(basicProperty.getProperty().getPropertyDetail().getPropertyTypeMaster() != null
+                    ? basicProperty.getProperty().getPropertyDetail().getPropertyTypeMaster().getCode() : "")) {
+                serviceCode = SERVICE_CODE_VACANTLANDTAX;
+            } else {
+                serviceCode = SERVICE_CODE_PROPERTYTAX;
+            }
         }
     }
 
