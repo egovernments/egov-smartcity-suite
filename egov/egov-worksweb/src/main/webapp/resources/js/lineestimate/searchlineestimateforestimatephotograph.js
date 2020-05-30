5b1e83f1d4c38b9878233a2192733f1d614d2a46
mohammadrafeek-eGov <mohammad.rafeek@egovernments.org>
2020-05-30 14:35:11 +0530

    [PHOENIX-9140]-displaying no records for invalid input data.

--- egov/egov-worksweb/src/main/webapp/resources/js/lineestimate/searchlineestimateforestimatephotograph.js
+++ egov/egov-worksweb/src/main/webapp/resources/js/lineestimate/searchlineestimateforestimatephotograph.js
@@ -84,7 +84,11 @@ function callAjaxSearch() {
 				ajax : {
 					url : "/egworks/lineestimate/searchlineestimateforestimatephotograph",
 					type: "POST",
-					"data":  getFormData(jQuery('form'))
+					"data":  getFormData(jQuery('form')),
+					"error" : function() {
+						jQuery("#resultTable tbody tr td").html(
+								"No data available.");
+					}
 				},
 				"bDestroy" : true,
 				'bAutoWidth': false,
