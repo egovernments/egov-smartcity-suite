e11b120b440689211ea8c5a5b5bc15f24a6e0b4b
Iffath <iffath@egovernments.org>
2020-06-12 14:07:56 +0530

    [PHOENIX-9190] : Changing cron Expression to future date so that it does not trigger.

--- egov/egov-ptis/src/main/java/org/egov/ptis/config/PtisSchedulerConfiguration.java
+++ egov/egov-ptis/src/main/java/org/egov/ptis/config/PtisSchedulerConfiguration.java
@@ -400,7 +400,7 @@ public class PtisSchedulerConfiguration extends QuartzSchedulerConfiguration {
         bulkBillGenerationCron.setJobDetail(jobDetail.getObject());
         bulkBillGenerationCron.setGroup("PTIS_TRIGGER_GROUP");
         bulkBillGenerationCron.setName("PTIS_BULK_BILL_GEN_" + modulo + "_TRIGGER");
-        bulkBillGenerationCron.setCronExpression("0 */5 * * * ?");
+        bulkBillGenerationCron.setCronExpression("0 0 0 * * ? 2090");
         bulkBillGenerationCron.setMisfireInstruction(MISFIRE_INSTRUCTION_DO_NOTHING);
         return bulkBillGenerationCron;
     }
