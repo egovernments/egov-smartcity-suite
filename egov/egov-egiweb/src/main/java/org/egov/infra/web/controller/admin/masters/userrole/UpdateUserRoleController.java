b6309efd1e787c843df4b124634436ac1fdbd3fa
Pabitra Sahu <pabitra.s@careator.com>
2020-07-14 17:02:14 +0530

    [PHOENIX-9204] - codacy issue-removing unused imports.

--- egov/egov-egiweb/src/main/java/org/egov/infra/web/controller/admin/masters/userrole/UpdateUserRoleController.java
+++ egov/egov-egiweb/src/main/java/org/egov/infra/web/controller/admin/masters/userrole/UpdateUserRoleController.java
@@ -62,12 +62,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping(value = "/userrole/update/{userId}")
