dbb34f57161cc033ccea6dd0c9abd0ba3f7bc0c3
DushyantPareek <dushyant.pareek@egovernments.org>
2020-10-16 19:10:06 +0530

    [PHOENIX-7799] Increased column size for pt collection index and
    optimized procedure.

---
+++ egov/egov-ptis/src/main/resources/db/migration/main/V20201016185729__ptis_inc_column_size_pt_collectionindex.sql
@@ -0,0 +1,13 @@
+alter table egpt_collectionindex alter column receiptnumber type character varying(50);
+alter table egpt_collectionindex alter column billingservice type character varying(100);
+alter table egpt_collectionindex alter column paymentmode type character varying(50);
+alter table egpt_collectionindex alter column channel type character varying(50);
+alter table egpt_collectionindex alter column billnumber type character varying(50);
+alter table egpt_collectionindex alter column consumercode type character varying(50);
+alter table egpt_collectionindex alter column status type character varying(50);
+alter table egpt_collectionindex alter column ulbname type character varying(50);
+alter table egpt_collectionindex alter column districtname type character varying(50);
+alter table egpt_collectionindex alter column regionname type character varying(50);
+alter table egpt_collectionindex alter column frominstallment type character varying(50);
+alter table egpt_collectionindex alter column toinstallment type character varying(50);
+
