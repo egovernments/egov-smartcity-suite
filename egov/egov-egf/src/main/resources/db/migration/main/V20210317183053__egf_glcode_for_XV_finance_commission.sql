7f657c4726d74a5306ec96ff47e3084c366aa231
allvy <pabitra.s@egovernments.org>
2021-03-17 18:34:56 +0530

    [AP-1363] - New Glcode for XV Finance commission.

---
+++ egov/egov-egf/src/main/resources/db/migration/main/V20210317183053__egf_glcode_for_XV_finance_commission.sql
@@ -0,0 +1,1 @@
+insert into chartofaccounts VALUES (nextval('seq_chartofaccounts'),'3201020','XV Finance Commission','XV Finance Commission',true,(select id from chartofaccounts where glcode = '32010'), null, null, 'L', null, 4, false, false, null,null, null,null, null, '320', null,null, now(), 1, now(), 1, 0);
