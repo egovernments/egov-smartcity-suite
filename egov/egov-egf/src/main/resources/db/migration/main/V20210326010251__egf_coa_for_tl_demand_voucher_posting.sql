a027334a0387cd5cb12db1356a23fdbedda21f5b
allvy <pabitra.s@egovernments.org>
2021-03-26 01:07:10 +0530

    [AP-1363] - Removed the incorrect version file and added the correct with correct version number.

--- egov/egov-egf/src/main/resources/db/migration/main/V20210326010251__egf_coa_for_tl_demand_voucher_posting.sql
+++ egov/egov-egf/src/main/resources/db/migration/main/V20210326010251__egf_coa_for_tl_demand_voucher_posting.sql
@@ -2,3 +2,4 @@ INSERT into chartofaccounts VALUES (nextval('seq_chartofaccounts'),'4313006','Ar
 
 INSERT into chartofaccounts VALUES (nextval('seq_chartofaccounts'),'4313007','Current Receivables for Trade License','Current Receivables for Trade License',true,(select id from chartofaccounts where glcode = '43130'), null, 'A', 'A', null, 4, false, false, null,null, null,null, null, '431', null,null, now(), 1, now(), 1, 0);
 
+
