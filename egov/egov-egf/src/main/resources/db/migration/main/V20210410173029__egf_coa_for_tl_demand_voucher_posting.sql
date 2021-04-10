33cca7cfd410b1882f402b67f8020f9b60a284fb
allvy <pabitra.s@egovernments.org>
2021-04-10 17:33:29 +0530

    [AP-1363] - New Glcode for TL demand voucher posting.

---
+++ egov/egov-egf/src/main/resources/db/migration/main/V20210410173029__egf_coa_for_tl_demand_voucher_posting.sql
@@ -0,0 +1,4 @@
+INSERT into chartofaccounts VALUES (nextval('seq_chartofaccounts'),'4313006','Arrear Receivables for Trade License','Arrear Receivables for Trade License',true,(select id from chartofaccounts where glcode = '43130'), null, 'A', 'A', null, 4, false, false, null,null, null,null, null, '431', null,null, now(), 1, now(), 1, 0);
+
+INSERT into chartofaccounts VALUES (nextval('seq_chartofaccounts'),'4313007','Current Receivables for Trade License','Current Receivables for Trade License',true,(select id from chartofaccounts where glcode = '43130'), null, 'A', 'A', null, 4, false, false, null,null, null,null, null, '431', null,null, now(), 1, now(), 1, 0);
+
